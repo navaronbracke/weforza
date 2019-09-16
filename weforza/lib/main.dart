bc92eb0670e2ad1226a1cfaa7e1065144931025c
navaronbracke <brackenavaron@gmail.com>
2019-09-16 21:41:07 +0200

    Added Platform Aware Widget interface. Added Landing Page scaffold for IOS/Android.

--- weforza/lib/main.dart
+++ weforza/lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:weforza/widgets/platformAwareWidgetBuilder.dart';
 import 'generated/i18n.dart';
 import 'widgets/homePage.dart';
 
 void main() => runApp(WeForzaApp());
 
 ///This class represents the application
-class WeForzaApp extends StatelessWidget {
+class WeForzaApp extends StatelessWidget implements PlatformAwareWidget {
 
   final Color _primaryColor = Colors.red;
   final Color _accentColor = Colors.redAccent;
@@ -13,14 +15,31 @@ class WeForzaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return PlatformAwareWidgetBuilder.buildPlatformAwareWidget(context, this);
+  }
+
+  @override
+  Widget buildAndroidWidget(BuildContext context) {
     return MaterialApp(
       title: _appTitle,//Calling S.of(context).string_name won't work here.
       localizationsDelegates: [S.delegate],
       supportedLocales: S.delegate.supportedLocales,
+      //We could define a ThemeData object in a different Dart file for easy customization
       theme: ThemeData(
-        primarySwatch: _primaryColor,
-        accentColor: _accentColor
+          primarySwatch: _primaryColor,
+          accentColor: _accentColor,
       ),
+      home: HomePage(),
+    );
+  }
+
+  @override
+  Widget buildIosWidget(BuildContext context) {
+    return CupertinoApp(
+      title: _appTitle,
+      localizationsDelegates: [S.delegate],
+      supportedLocales: S.delegate.supportedLocales,
+      //TODO Theme
       home: HomePage(),
     );
   }
--- weforza/lib/widgets/homePage.dart
+++ weforza/lib/widgets/homePage.dart
@@ -1,27 +1,40 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:weforza/widgets/platformAwareWidgetBuilder.dart';
 import '../generated/i18n.dart';
 
-///This [StatefulWidget] represents the app landing page.
-class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-///This class is the [State] for [HomePage].
-///It builds a [Widget] tree, which consists of an [AppBar] and a content [Widget].
-class _HomePageState extends State<HomePage> {
+///This [Widget] represents the app landing page.
+class HomePage extends StatelessWidget implements PlatformAwareWidget {
 
   @override
   Widget build(BuildContext context) {
+    return PlatformAwareWidgetBuilder.buildPlatformAwareWidget(context, this);
+  }
+
+  @override
+  Widget buildAndroidWidget(BuildContext context) {
+    // TODO: implement buildAndroidWidget
+
+    //Placeholder
     return Scaffold(
       appBar: AppBar(
-        title: Text(S.of(context).AppName),
+        title: Text("Android HomePageWidget"),
       ),
-      body: Center(
-        //TODO: add homepage widget children
-      ),
+      body: Center(),
     );
   }
+
+  @override
+  Widget buildIosWidget(BuildContext context) {
+    // TODO: implement buildIosWidget
+
+    //Placeholder
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("IOS Homepage Widget"),
+      ),
+      child: Container(),
+    );
+  }
+
 }

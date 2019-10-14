9871d353af059461829b59d8e282532d8b9311b8
navaronbracke <brackenavaron@gmail.com>
2019-10-14 02:57:54 +0200

    Added basic layout for the RideList page. Added Ride/RideRepository classes.

--- weforza/lib/blocs/rideListBloc.dart
+++ weforza/lib/blocs/rideListBloc.dart
@@ -1,8 +1,23 @@
 
 import 'package:weforza/blocs/bloc.dart';
+import 'package:weforza/model/member.dart';
+import 'package:weforza/model/ride.dart';
+import 'package:weforza/repository/memberRepository.dart';
+import 'package:weforza/repository/rideRepository.dart';
 
 ///This is the BLoC for RideListPage.
 class RideListBloc extends Bloc {
+  RideListBloc(this._memberRepository,this._rideRepository): assert(_rideRepository != null && _memberRepository != null);
+
+  final IRideRepository _rideRepository;
+  final IMemberRepository _memberRepository;
+
+  ///Get all rides.
+  Future<List<Ride>> getAllRides() => _rideRepository.getAllRides();
+  ///Get all members.
+  Future<List<Member>> getAllMembers() => _memberRepository.getAllMembers();
+
+
   @override
   void dispose() {}
 

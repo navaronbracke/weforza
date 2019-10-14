4d6f28bfd584da17b4d08c64c0fa64750886d5f4
navaronbracke <brackenavaron@gmail.com>
2019-10-14 18:25:46 +0200

    Adjusted app theme. Adjusted RideList UI to new theme. Added counter property to RideListBLoC.

--- weforza/lib/theme/appTheme.dart
+++ weforza/lib/theme/appTheme.dart
@@ -5,29 +5,22 @@ import 'package:flutter/material.dart';
 abstract class ApplicationTheme {
 
   ///Android Theme colors.
-  static final Color _androidPrimaryColor = Color.fromARGB(255, 38, 38, 38);
-  static final Color _androidAccentColor = Color.fromARGB(255, 94, 16, 16);
-
-  ///Android Theme Ink Splash colors.
-  ///Splash color for [_androidPrimaryColor].
-  static final Color _androidPrimaryDerivedSplashColor = Color.fromARGB(255, 60,60,60);
-  ///Splash color for the go to member details [IconButton].
-  static final Color goToMemberDetailSplashColor = Color.fromARGB(255, 173, 173, 173);
+  static final Color _androidPrimaryColor = Colors.blue;
+  static final Color _androidAccentColor = Colors.blue.shade300;
 
   ///Cupertino Theme colors.
-  static final Color _iosPrimaryColor = Color.fromARGB(255, 38, 38, 38);
-  static final Color _iosAccentColor = Color.fromARGB(255, 94, 16, 16);
+  static final Color _iosPrimaryColor = Colors.blue;
+  static final Color _iosAccentColor = Colors.blueAccent;
 
   static final TextStyle memberListItemFirstNameTextStyle = TextStyle(fontSize: 16,fontWeight: FontWeight.w400);
   static final TextStyle memberListItemLastNameTextStyle = TextStyle(fontSize: 12);
-
 
   ///Provide the Android theme.
   static ThemeData androidTheme(){
      return ThemeData(
        primaryColor: _androidPrimaryColor,
        accentColor: _androidAccentColor,
-       splashColor: _androidPrimaryDerivedSplashColor
+       splashColor: _androidAccentColor.withAlpha(150)
      );
   }
 

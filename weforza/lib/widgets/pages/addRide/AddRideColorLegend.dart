e93c86844ec3220b1bc7942b4318b65a43b867a1
navaronbracke <brackenavaron@gmail.com>
2019-11-14 17:29:50 +0100

    Added AddRidePage color legend stub.

---
+++ weforza/lib/widgets/pages/addRide/AddRideColorLegend.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+///This [Widget] represents a color legend for AddRidePage.
+class AddRideColorLegend extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        //
+      ],
+    );
+  }
+}

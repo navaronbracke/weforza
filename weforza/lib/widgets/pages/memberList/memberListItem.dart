b9a70a80a4d647527d51653238d445e8e83d7a85
navaronbracke <brackenavaron@gmail.com>
2019-12-07 19:29:59 +0100

    Removed IconButton from MemberList items. Added tap gesture to MemberListItem itself.

--- weforza/lib/widgets/pages/memberList/memberListItem.dart
+++ weforza/lib/widgets/pages/memberList/memberListItem.dart
@@ -5,7 +5,6 @@ import 'package:weforza/model/member.dart';
 import 'package:weforza/theme/appTheme.dart';
 import 'package:weforza/widgets/pages/memberDetails/memberDetailsPage.dart';
 import 'package:weforza/widgets/pages/memberList/memberListPage.dart';
-import 'package:weforza/widgets/platform/cupertinoIconButton.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 
 ///This class represents a list item for [MemberListPage].
@@ -23,55 +22,43 @@ class MemberListItem extends StatelessWidget implements PlatformAwareWidget {
   @override
   Widget buildAndroidWidget(BuildContext context) {
     return ListTile(
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(_member.firstname,style: ApplicationTheme.memberListItemFirstNameTextStyle,
-              overflow: TextOverflow.ellipsis),
-          SizedBox(height: 5),
-          Text(_member.lastname, style: ApplicationTheme.memberListItemLastNameTextStyle,
-              overflow: TextOverflow.ellipsis),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+              Text(_member.firstname,style: ApplicationTheme.memberListItemFirstNameTextStyle, overflow: TextOverflow.ellipsis),
+              SizedBox(height: 5),
+              Text(_member.lastname, style: ApplicationTheme.memberListItemLastNameTextStyle, overflow: TextOverflow.ellipsis),
         ],
       ),
-      trailing: SizedBox(
-          width: 40,
-          child: IconButton(
-            icon: Icon(Icons.contacts, color: Theme.of(context).primaryColor),
-            onPressed: () => {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=> MemberDetailsPage(_member)))
-            },
-          ),
-        ),
+      onTap: (){
+        Navigator.of(context).push(MaterialPageRoute(builder: (context)=> MemberDetailsPage(_member)));
+      },
     );
   }
 
   @override
   Widget buildIosWidget(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(_member.firstname,
-                    style: ApplicationTheme.memberListItemFirstNameTextStyle,
-                    overflow: TextOverflow.ellipsis),
-                SizedBox(height: 4),
-                Text(_member.lastname,
-                    style: ApplicationTheme.memberListItemLastNameTextStyle,
-                    overflow: TextOverflow.ellipsis),
-              ],
-            ),
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(MaterialPageRoute(builder: (context)=> MemberDetailsPage(_member)));
+      },
+      child: Container(
+        decoration: BoxDecoration(),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_member.firstname,
+                  style: ApplicationTheme.memberListItemFirstNameTextStyle,
+                  overflow: TextOverflow.ellipsis),
+              SizedBox(height: 4),
+              Text(_member.lastname,
+                  style: ApplicationTheme.memberListItemLastNameTextStyle,
+                  overflow: TextOverflow.ellipsis),
+            ],
           ),
-          SizedBox(
-            width: 40,
-            child: CupertinoIconButton(Icons.contacts,CupertinoTheme.of(context).primaryColor,CupertinoTheme.of(context).primaryContrastingColor,(){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=> MemberDetailsPage(_member)));
-            }),
-          ),
-        ],
+        ),
       ),
     );
   }

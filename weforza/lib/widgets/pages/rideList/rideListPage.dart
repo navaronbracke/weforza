83042d307ccc12217027f6391952e47de5aec0d7
navaronbracke <brackenavaron@gmail.com>
2020-01-20 20:41:29 +0100

    Updated RideListPage with provider.

--- weforza/lib/widgets/pages/rideList/rideListPage.dart
+++ weforza/lib/widgets/pages/rideList/rideListPage.dart
@@ -1,60 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:weforza/blocs/rideListBloc.dart';
+import 'package:provider/provider.dart';
 import 'package:weforza/generated/i18n.dart';
-import 'package:weforza/injection/injector.dart';
 import 'package:weforza/model/ride.dart';
-import 'package:weforza/repository/rideRepository.dart';
 import 'package:weforza/widgets/pages/addRide/addRidePage.dart';
 import 'package:weforza/widgets/pages/rideList/rideListItem.dart';
 import 'package:weforza/widgets/platform/cupertinoIconButton.dart';
 import 'package:weforza/widgets/platform/platformAwareLoadingIndicator.dart';
 import 'package:weforza/widgets/pages/rideList/rideListRidesEmpty.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
+import 'package:weforza/widgets/provider/rideProvider.dart';
 
 ///This [Widget] shows the list of Rides.
 class RideListPage extends StatefulWidget {
 
   @override
-  State<RideListPage> createState() =>
-      _RideListPageState(RideListBloc(InjectionContainer.get<RideRepository>()));
+  State<RideListPage> createState() => _RideListPageState();
 }
 
 ///This class is the [State] for [RideListPage].
-class _RideListPageState extends State<RideListPage>
-    implements PlatformAwareWidget {
-  _RideListPageState(this._bloc) : assert(_bloc != null) {
-    _onReload = (bool reload){
-      if(reload != null && reload){
-        setState(() {
-          loadRidesFuture = _bloc.getRides();
-        });
-      }
-    };
-  }
-
-  ///The BLoC for this [Widget].
-  final RideListBloc _bloc;
-
-  ///This callback triggers a reload.
-  Function _onReload;
-
-  ///The future that loads the rides.
-  Future<List<Ride>> loadRidesFuture;
+class _RideListPageState extends State<RideListPage> implements PlatformAwareWidget {
 
   @override
-  void initState() {
-    super.initState();
-    loadRidesFuture = _bloc.getRides();
+  Widget build(BuildContext context){
+    Provider.of<RideProvider>(context).loadRidesIfNotLoaded();
+    return PlatformAwareWidgetBuilder.build(context, this);
   }
-
-  @override
-  Widget build(BuildContext context) =>
-      PlatformAwareWidgetBuilder.build(context, this);
 
   @override
   Widget buildAndroidWidget(BuildContext context) {
+    final provider = Provider.of<RideProvider>(context);
     return Scaffold(
         appBar: AppBar(
           title: Text(S.of(context).RideListRidesHeader),
@@ -62,18 +38,12 @@ class _RideListPageState extends State<RideListPage>
             IconButton(
               icon: Icon(Icons.add),
               color: Colors.white,
-              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=> AddRidePage())).then((value){
-                if(value != null && value){
-                  setState(() {
-                    loadRidesFuture = _bloc.getRides();
-                  });
-                }
-              }),
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=> AddRidePage())),
             ),
           ],
         ),
         body: FutureBuilder<List<Ride>>(
-          future: loadRidesFuture,
+          future: provider.ridesFuture,
           builder: (context,snapshot){
             if(snapshot.connectionState == ConnectionState.done){
               if(snapshot.hasError){
@@ -87,7 +57,7 @@ class _RideListPageState extends State<RideListPage>
                   return ListView.builder(
                     itemCount: snapshot.data.length,
                     itemBuilder: (context,index){
-                      return RideListItem(snapshot.data[index],_onReload);
+                      return RideListItem(snapshot.data[index]);
                     });
                 }
               }
@@ -101,6 +71,7 @@ class _RideListPageState extends State<RideListPage>
 
   @override
   Widget buildIosWidget(BuildContext context) {
+    final provider = Provider.of<RideProvider>(context);
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         transitionBetweenRoutes: false,
@@ -113,11 +84,7 @@ class _RideListPageState extends State<RideListPage>
               Icons.add,
               CupertinoTheme.of(context).primaryColor,
               CupertinoTheme.of(context).primaryContrastingColor,
-              () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=> AddRidePage())).then((value){
-                setState(() {
-                  loadRidesFuture = _bloc.getRides();
-                });
-              }),
+              () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=> AddRidePage())),
             ),
           ],
         ),
@@ -125,7 +92,7 @@ class _RideListPageState extends State<RideListPage>
       child: SafeArea(
           bottom: false,
           child: FutureBuilder<List<Ride>>(
-            future: loadRidesFuture,
+            future: provider.ridesFuture,
             builder: (context,snapshot){
               if(snapshot.connectionState == ConnectionState.done){
                 if(snapshot.hasError){
@@ -139,7 +106,7 @@ class _RideListPageState extends State<RideListPage>
                     return ListView.builder(
                         itemCount: snapshot.data.length,
                         itemBuilder: (context,index){
-                          return RideListItem(snapshot.data[index],_onReload);
+                          return RideListItem(snapshot.data[index]);
                         });
                   }
                 }

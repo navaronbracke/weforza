12f92b31f42a3adbc872591145a41dbe19ab8785
navaronbracke <brackenavaron@gmail.com>
2020-01-20 17:25:39 +0100

    Removed MemberListBloc. Updated Member model. Added Provider package/plugin.

--- weforza/lib/widgets/provider/memberProvider.dart
+++ weforza/lib/widgets/provider/memberProvider.dart
@@ -1,22 +1,20 @@
-
-import 'package:weforza/blocs/bloc.dart';
 import 'package:weforza/model/member.dart';
 import 'package:weforza/model/memberItem.dart';
 import 'package:weforza/repository/memberRepository.dart';
 
-///This class is the BLoC for the member list page.
-class MemberListBloc extends Bloc {
-  MemberListBloc(this._repository): assert(_repository != null);
+///This class provides a selected [MemberItem] and the future for loading members.
+class MemberProvider {
+  MemberProvider(this._repository): assert(_repository != null);
 
   final MemberRepository _repository;
 
-  @override
-  void dispose() {}
+  MemberItem selectedMember;
 
-  Future<List<MemberItem>> loadMembers() async {
+  Future<List<MemberItem>> membersFuture;
+
+  void loadMembers() async {
     List<Member> members = await _repository.getMembers();
     List<Future<MemberItem>> items = members.map((member) async => MemberItem(member,await _repository.loadProfileImageFromDisk(member.profileImageFilePath))).toList();
-    return Future.wait(items);
+    membersFuture = Future.wait(items);
   }
-
 }

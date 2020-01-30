c55c99b472ebdc7a3612cb781227dbf2977b0e17
navaronbracke <brackenavaron@gmail.com>
2020-01-30 18:01:07 +0100

    Added RideAttendeeAssignmentSaving widget. Updated RideAttendee Assignment to handle long saving submit with future builder.

--- weforza/lib/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentPage.dart
+++ weforza/lib/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentPage.dart
@@ -5,6 +5,7 @@ import 'package:weforza/model/rideAttendeeDisplayMode.dart';
 import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentError.dart';
 import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentList.dart';
 import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentLoadMembers.dart';
+import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentSaving.dart';
 import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentScanning.dart';
 
 class RideAttendeeAssignmentPage extends StatelessWidget {
@@ -25,15 +26,17 @@ class RideAttendeeAssignmentPage extends StatelessWidget {
         }else{
           switch(snapshot.data){
             case RideAttendeeDisplayMode.IDLE:
-              return RideAttendeeAssignmentList(title,_bloc.items,() => _bloc.startScan(),() async {
-                if(await _bloc.onSubmit()){
+              return RideAttendeeAssignmentList(title,_bloc.items,() => _bloc.startScan(),() {
+                _bloc.submitFuture = _bloc.onSubmit().then((_){
                   Navigator.of(context).pop(true);
-                }
+                });
               });
             case RideAttendeeDisplayMode.MEMBERS:
               return RideAttendeeAssignmentLoadMembers(title,_bloc.loadMembers());
             case RideAttendeeDisplayMode.SCANNING:
               return RideAttendeeAssignmentScanning(_bloc.scanFuture,title,() => _bloc.stopScan());
+            case RideAttendeeDisplayMode.SAVING:
+              return RideAttendeeAssignmentSaving(_bloc.submitFuture,title);
             default: return RideAttendeeAssignmentError(title);
           }
         }

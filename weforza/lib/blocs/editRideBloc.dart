a9e3d276a646948d6460f9d8cd2202ce38966184
navaronbracke <brackenavaron@gmail.com>
2020-02-02 13:57:48 +0100

    Updated EditRide Bloc.

--- weforza/lib/blocs/editRideBloc.dart
+++ weforza/lib/blocs/editRideBloc.dart
@@ -1,26 +1,35 @@
 
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
 import 'package:weforza/blocs/bloc.dart';
 import 'package:weforza/model/ride.dart';
 import 'package:weforza/repository/rideRepository.dart';
 
 class EditRideBloc extends Bloc {
-  EditRideBloc(this._repository): assert(_repository != null);
+  EditRideBloc(this._repository,this.ride): assert(_repository != null && ride != null){
+    titleInput = ride.title ?? "";
+    distanceInput = ride.distance;
+    departureInput = ride.startAddress ?? "";
+    destinationInput = ride.destinationAddress ?? "";
+  }
 
+  final StreamController<EditRideSubmitState> _submitController = BehaviorSubject();
+  Stream<EditRideSubmitState> get stream => _submitController.stream;
+
+  final Ride ride;
   final RideRepository _repository;
 
   ///Max values for input
   final int titleMaxLength = 80;
   final int maxDistanceInKm = 300;
-  final int departureMaxLength = 80;
-  final int destinationMaxLength = 80;
+  final int addressMaxLength = 80;
 
   ///The actual inputs
-  String _titleInput;
-  double _distanceInput;
-  String _departureInput;
-  String _destinationInput;
+  String titleInput;
+  double distanceInput;
+  String departureInput;
+  String destinationInput;
 
   ///The input errors
   String titleError;
@@ -32,14 +41,14 @@ class EditRideBloc extends Bloc {
   ///Validation should start once an input came into focus at least once.
   bool autoValidateTitle = false;
   bool autoValidateDistance = false;
-  bool autoValidateStartAddress = false;
+  bool autoValidateDepartureAddress = false;
   bool autoValidateDestinationAddress = false;
 
   ///Validate the title input.
   String validateTitle(String value,String isWhitespaceMessage, String maxLengthMessage){
     //Title is empty, return since we can stop validating.
     if(value == null || value.isEmpty){
-      _titleInput = null;
+      titleInput = null;
       titleError = null;
     }else{
       //Whitespace
@@ -48,9 +57,9 @@ class EditRideBloc extends Bloc {
       }else if(value.length > titleMaxLength){
         //Max Length
         titleError = maxLengthMessage;
-      }else if(value != _titleInput){
+      }else {
         titleError = null;
-        _titleInput = value;
+        titleInput = value;
       }
     }
     return titleError;
@@ -60,9 +69,10 @@ class EditRideBloc extends Bloc {
   String validateDistance(String value, String invalidDistanceMessage, String positiveDistanceRequiredMessage, String maximumDistanceMessage){
     //Distance is empty, return since we can stop validating.
     if(value == null || value.isEmpty){
-      _distanceInput = 0.0;
+      distanceInput = 0.0;
       distanceError = null;
     }else{
+      value = Ride.convertNumberSeparator(value, oldSeparator: ",", newSeparator: ".");
       double distance = double.tryParse(value);
       if(distance == null){
         distanceError = invalidDistanceMessage;
@@ -71,8 +81,8 @@ class EditRideBloc extends Bloc {
           distanceError = positiveDistanceRequiredMessage;
         }else if(distance > maxDistanceInKm){
           distanceError = maximumDistanceMessage;
-        }else if(distance != _distanceInput){
-          _distanceInput = distance;
+        }else {
+          distanceInput = distance;
           distanceError = null;
         }
       }
@@ -83,18 +93,18 @@ class EditRideBloc extends Bloc {
   //Validate the departure input.
   String validateDepartureAddress(String value,String isWhitespaceMessage,String maxLengthMessage,String invalidAddressMessage){
     if(value == null || value.isEmpty){
-      _departureInput = null;
+      departureInput = null;
       departureError = null;
     }else{
       if(value.trim().isEmpty){
         departureError = isWhitespaceMessage;
-      }else if(value.length > departureMaxLength){
+      }else if(value.length > addressMaxLength){
         departureError = maxLengthMessage;
-      }else if(!Ride.addressRegex.hasMatch(value)){
-        departureError = invalidAddressMessage;
-      }else if(value != _departureInput){
+      }else if(Ride.addressRegex.hasMatch(value)){
         departureError = null;
-        _departureInput = value;
+        departureInput = value;
+      }else{
+        departureError = invalidAddressMessage;
       }
     }
     return departureError;
@@ -103,31 +113,46 @@ class EditRideBloc extends Bloc {
   //Validate the destination input.
   String validateDestinationAddress(String value,String isWhitespaceMessage,String maxLengthMessage,String invalidAddressMessage){
     if(value == null || value.isEmpty){
-      _destinationInput = null;
+      destinationInput = null;
       destinationError = null;
     }else{
       if(value.trim().isEmpty){
         destinationError = isWhitespaceMessage;
-      }else if(value.length > destinationMaxLength){
+      }else if(value.length > addressMaxLength){
         destinationError = maxLengthMessage;
-      }else if(!Ride.addressRegex.hasMatch(value)){
-        destinationError = invalidAddressMessage;
-      }else if(value != _destinationInput){
+      }else if(Ride.addressRegex.hasMatch(value)){
         destinationError = null;
-        _destinationInput = value;
+        destinationInput = value;
+      }else {
+        destinationError = invalidAddressMessage;
       }
     }
     return destinationError;
   }
 
-  Future<void> editRide(Ride ride) async {
-    ride.title = _titleInput;
-    ride.distance = _distanceInput;
-    ride.startAddress = _departureInput;
-    ride.destinationAddress = _destinationInput;
-    await _repository.editRide(ride);
+  Future<void> editRide(void Function(Ride updatedRide) onSuccess) async {
+    _submitController.add(EditRideSubmitState.SUBMIT);
+    //Grab a copy first
+    final newRide = Ride(
+      date: ride.date,
+      title: titleInput,
+      numberOfAttendees: ride.numberOfAttendees,
+      destinationAddress: destinationInput,
+      startAddress: departureInput,
+      distance: distanceInput
+    );
+    //Save the copy, if it fails, nothing gets overwritten.
+    await _repository.editRide(newRide).then((_){
+      onSuccess(newRide);
+    },onError: (e)=> _submitController.add(EditRideSubmitState.ERROR));
   }
 
   @override
-  void dispose() {}
+  void dispose() {
+    _submitController.close();
+  }
+}
+
+enum EditRideSubmitState {
+  IDLE,SUBMIT,ERROR
 }

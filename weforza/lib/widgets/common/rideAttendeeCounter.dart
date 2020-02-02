2eac5c3b5ec4e399325d4a7f1f4d50310a3c71e2
Navaron Bracke <navaronbracke@MacBook-Air-van-Navaron.local>
2020-02-02 18:23:05 +0100

    updated AttendeeCounter + rideListItem

--- weforza/lib/widgets/common/rideAttendeeCounter.dart
+++ weforza/lib/widgets/common/rideAttendeeCounter.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:weforza/theme/appTheme.dart';
 
 class RideAttendeeCounter extends StatelessWidget {
-  RideAttendeeCounter(this.count): assert(count != null);
+  RideAttendeeCounter(this.count,{this.iconSize,this.counterStyle}): assert(count != null);
 
+  final double iconSize;
+  final TextStyle counterStyle;
   final String count;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Text(count),
+        counterStyle == null ? Text(count): Text(count,style: counterStyle),
         SizedBox(width: 5),
-        Icon(Icons.people,color: ApplicationTheme.rideAttendeeCounterIconColor),
+        Icon(Icons.people,size: iconSize ?? 24,color: ApplicationTheme.rideAttendeeCounterIconColor),
       ],
     );
   }

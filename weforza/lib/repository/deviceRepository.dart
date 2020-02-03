29d9d9f66131fe709e8a3aa0b345942482f4012a
navaronbracke <brackenavaron@gmail.com>
2020-02-03 23:00:45 +0100

    Updated DeviceDao. Added deleting devices of member when deleting member. Added Device class.

--- weforza/lib/repository/deviceRepository.dart
+++ weforza/lib/repository/deviceRepository.dart
@@ -1,5 +1,6 @@
 
 import 'package:weforza/database/deviceDao.dart';
+import 'package:weforza/model/device.dart';
 
 class DeviceRepository {
   DeviceRepository(this._dao): assert(_dao != null);
@@ -8,11 +9,11 @@ class DeviceRepository {
 
   Future<bool> deviceExists(String device) => _dao.deviceExists(device);
 
-  Future<void> addDevice(String ownerId,String device) => _dao.addDevice(ownerId, device);
+  Future<void> addDevice(Device device) => _dao.addDevice(device);
 
   Future<void> removeDevice(String device) => _dao.removeDevice(device);
 
-  Future<void> updateDevice(String ownerId,String oldDevice,String newDevice) => _dao.updateDevice(ownerId, oldDevice, newDevice);
+  Future<void> updateDevice(String oldDevice,Device newDevice) => _dao.updateDevice(oldDevice, newDevice);
 
-  Future<List<String>> getOwnerDevices(String uuid) => _dao.getOwnerDevices(uuid);
+  Future<List<Device>> getOwnerDevices(String uuid) => _dao.getOwnerDevices(uuid);
 }

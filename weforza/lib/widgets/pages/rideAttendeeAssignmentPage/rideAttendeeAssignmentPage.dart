e1e483e174fcee58801c64e40990e4a846c7551e
navaronbracke <brackenavaron@gmail.com>
2020-02-15 21:02:26 +0100

    Added RideAttendeeAssignmentNavigationBarContent widget + updated RideAttendeeAssignmentPage.

--- weforza/lib/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentPage.dart
+++ weforza/lib/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentPage.dart
@@ -1,44 +1,93 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:weforza/blocs/rideAttendeeAssignmentBloc.dart';
-import 'package:weforza/model/rideAttendeeDisplayMode.dart';
-import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentError.dart';
-import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentList.dart';
-import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentLoadMembers.dart';
-import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentSaving.dart';
-import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentScanning.dart';
+import 'package:weforza/blocs/rideAttendeeAssignmentItemBloc.dart';
+import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentGenericError.dart';
+import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentList/rideAttendeeAssignmentList.dart';
+import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentNavigationBarContent.dart';
+import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentSubmit/rideAttendeeAssignmentSubmit.dart';
 
-class RideAttendeeAssignmentPage extends StatelessWidget {
-  RideAttendeeAssignmentPage(this._bloc): assert(_bloc != null);
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 
-  final RideAttendeeAssignmentBloc _bloc;
+class RideAttendeeAssignmentPage extends StatefulWidget {
+  RideAttendeeAssignmentPage(this.bloc): assert(bloc != null);
+
+  final RideAttendeeAssignmentBloc bloc;
+
+  @override
+  _RideAttendeeAssignmentPageState createState() => _RideAttendeeAssignmentPageState();
+}
+
+class _RideAttendeeAssignmentPageState extends State<RideAttendeeAssignmentPage> {
+
+  Future<List<RideAttendeeAssignmentItemBloc>> listFuture;
+
+  Future<void> submitFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    listFuture = widget.bloc.loadMembers();
+  }
 
   @override
   Widget build(BuildContext context){
-    final title = _bloc.getTitle(context);
+    return PlatformAwareWidget(
+      android: () => _buildAndroidLayout(context),
+      ios: () => _buildIosLayout(context),
+    );
+  }
 
-    return StreamBuilder<RideAttendeeDisplayMode>(
-      stream: _bloc.displayModeStream,
-      initialData: RideAttendeeDisplayMode.MEMBERS,
+  Widget _buildIosLayout(BuildContext context){
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        transitionBetweenRoutes: false,
+        middle: RideAttendeeAssignmentNavigationBarContent(
+          title: widget.bloc.getTitle(context),
+          onStartScan: () => widget.bloc.startScan(),//TODO wrap this with a bluetooth enabled check
+          onSubmit: () => setState((){
+            submitFuture = widget.bloc.onSubmit().then((_){
+              Navigator.of(context).pop(true);
+            });
+          }),
+          stream: widget.bloc.actionsDisplayModeStream,
+        ),
+      ),
+      child: _buildBody(),
+    );
+  }
+
+  Widget _buildAndroidLayout(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: RideAttendeeAssignmentNavigationBarContent(
+          title: widget.bloc.getTitle(context),
+          onStartScan: () => widget.bloc.startScan(),
+          onSubmit: () => setState((){
+            submitFuture = widget.bloc.onSubmit().then((_){
+              Navigator.of(context).pop(true);
+            });
+          }),
+          stream: widget.bloc.actionsDisplayModeStream,
+        ),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody(){
+    return StreamBuilder<RideAttendeeAssignmentContentDisplayMode>(
+      initialData: RideAttendeeAssignmentContentDisplayMode.LIST,
       builder: (context,snapshot){
-        if(snapshot.hasError){
-          return RideAttendeeAssignmentError(title);
-        }else{
-          switch(snapshot.data){
-            case RideAttendeeDisplayMode.IDLE:
-              return RideAttendeeAssignmentList(title,_bloc.items,() => _bloc.startScan(),() {
-                _bloc.submitFuture = _bloc.onSubmit().then((_){
-                  Navigator.of(context).pop(true);
-                });
-              });
-            case RideAttendeeDisplayMode.MEMBERS:
-              return RideAttendeeAssignmentLoadMembers(title,_bloc.loadMembers());
-            case RideAttendeeDisplayMode.SCANNING:
-              return RideAttendeeAssignmentScanning(_bloc.scanFuture,title,() => _bloc.stopScan());
-            case RideAttendeeDisplayMode.SAVING:
-              return RideAttendeeAssignmentSaving(_bloc.submitFuture,title);
-            default: return RideAttendeeAssignmentError(title);
-          }
+        switch(snapshot.data){
+          case RideAttendeeAssignmentContentDisplayMode.LIST:
+            return RideAttendeeAssignmentList(future: listFuture);
+          case RideAttendeeAssignmentContentDisplayMode.SAVE:
+            return RideAttendeeAssignmentSubmit(future: submitFuture);
+          case RideAttendeeAssignmentContentDisplayMode.SCAN:
+            return null;//TODO put new scanning widget here that uses streambuilder
+          default: return RideAttendeeAssignmentGenericError();
         }
       },
     );

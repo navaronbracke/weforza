b27bb62dd280693cecae84387a7170da5275065b
navaronbracke <brackenavaron@gmail.com>
2020-02-21 13:54:43 +0100

    Updated RideAttendeeAssignmentNavigationBarContent and RideAttendeeScanner interface.

--- weforza/lib/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentNavigationBarContent.dart
+++ weforza/lib/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeAssignmentNavigationBarContent.dart
@@ -1,72 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:weforza/widgets/pages/rideAttendeeAssignmentPage/rideAttendeeNavigationBarDisplayMode.dart';
 import 'package:weforza/widgets/platform/cupertinoIconButton.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 
 class RideAttendeeAssignmentNavigationBarContent extends StatelessWidget {
   RideAttendeeAssignmentNavigationBarContent({
     @required this.title,
+    @required this.scanTitle,
     @required this.onStartScan,
     @required this.onSubmit,
     @required this.stream,
   }): assert(
-    title != null && onSubmit != null && onStartScan != null && stream != null
+    title != null && scanTitle != null && onSubmit != null &&
+        onStartScan != null && stream != null
   );
 
+  final String scanTitle;
   final String title;
   final VoidCallback onStartScan;
   final VoidCallback onSubmit;
-  final Stream<bool> stream;
+  final Stream<RideAttendeeNavigationBarDisplayMode> stream;
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<bool>(
+    return StreamBuilder<RideAttendeeNavigationBarDisplayMode>(
       stream: stream,
-      initialData: false,
+      initialData: RideAttendeeNavigationBarDisplayMode.LIST_NO_ACTIONS,
       builder: (context,snapshot){
-        return PlatformAwareWidget(
-          android: () => _buildAndroidLayout(context,snapshot.data),
-          ios: () => _buildIosLayout(context,snapshot.data),
-        );
+        switch(snapshot.data){
+          case RideAttendeeNavigationBarDisplayMode.LIST_ACTIONS:
+            return _buildNavbarWithListActions();
+          case RideAttendeeNavigationBarDisplayMode.LIST_NO_ACTIONS:
+            return _buildNavbarWithoutListActions();
+          case RideAttendeeNavigationBarDisplayMode.SCAN:
+            return _buildNavbarWithScanTitle();
+          default: return SizedBox();
+        }
       },
     );
   }
 
-  Widget _buildAndroidLayout(BuildContext context, bool hasActions){
-    return hasActions ? Row(
-      children: <Widget>[
-        Expanded(
-          child: Text(title,overflow: TextOverflow.ellipsis),
-        ),
-        IconButton(
-          icon: Icon(Icons.bluetooth),
-          onPressed: () => onStartScan(),
-        ),
-        IconButton(
-          icon: Icon(Icons.check),
-          onPressed: () => onSubmit(),
-        )
-      ],
-    ): Text(title,overflow: TextOverflow.ellipsis);
+  Widget _buildNavbarWithListActions(){
+    return PlatformAwareWidget(
+      android: () => Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(title,overflow: TextOverflow.ellipsis),
+          ),
+          IconButton(
+            icon: Icon(Icons.bluetooth),
+            onPressed: () => onStartScan(),
+          ),
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () => onSubmit(),
+          )
+        ],
+      ),
+      ios: () => Row(
+        children: <Widget>[
+          Expanded(
+            child: Center(child: Text(title,overflow: TextOverflow.ellipsis)),
+          ),
+          CupertinoIconButton(
+              icon: Icons.bluetooth,
+              onPressed: () => onStartScan()
+          ),
+          SizedBox(width: 10),
+          CupertinoIconButton(
+            icon: Icons.check,
+            onPressed: () => onSubmit(),
+          ),
+        ],
+      ),
+    );
   }
 
-  Widget _buildIosLayout(BuildContext context, bool hasActions){
-    return hasActions ? Row(
-      children: <Widget>[
-        Expanded(
-          child: Center(child: Text(title,overflow: TextOverflow.ellipsis)),
-        ),
-        CupertinoIconButton(
-            icon: Icons.bluetooth,
-            onPressed: () => onStartScan()
-        ),
-        SizedBox(width: 10),
-        CupertinoIconButton(
-          icon: Icons.check,
-          onPressed: () => onSubmit(),
-        ),
-      ],
-    ): Text(title,overflow: TextOverflow.ellipsis);
-  }
+  Widget _buildNavbarWithoutListActions()
+    => Text(title,overflow: TextOverflow.ellipsis);
+
+  Widget _buildNavbarWithScanTitle()
+    => Text(scanTitle,overflow: TextOverflow.ellipsis);
 }

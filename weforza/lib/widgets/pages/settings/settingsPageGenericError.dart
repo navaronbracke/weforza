697be1ea67b0d3fcc7663191c7e995b15795fa2d
navaronbracke <brackenavaron@gmail.com>
2020-02-29 20:55:32 +0100

    Added SettingsPage Submit/GenericError widgets.

---
+++ weforza/lib/widgets/pages/settings/settingsPageGenericError.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:weforza/generated/i18n.dart';
+import 'package:flutter/material.dart';
+import 'package:weforza/theme/appTheme.dart';
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
+
+class SettingsPageGenericError extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return PlatformAwareWidget(
+      android: () => Scaffold(
+        appBar: AppBar(
+          title: Text(S.of(context).SettingsTitle),
+        ),
+        body: _buildBody(context),
+      ),
+      ios: () => CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          transitionBetweenRoutes: false,
+          middle: Text(S.of(context).SettingsTitle),
+        ),
+        child: _buildBody(context),
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context){
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.warning,
+            color: ApplicationTheme.listInformationalIconColor,
+            size: MediaQuery.of(context).size.shortestSide * .1,
+          ),
+          SizedBox(height: 5),
+          Text(S.of(context).SettingsGenericError)
+        ],
+      ),
+    );
+  }
+}
---
+++ weforza/lib/widgets/pages/settings/settingsSubmit.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:weforza/widgets/platform/cupertinoIconButton.dart';
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
+
+abstract class SettingsSubmitHandler {
+  Stream<bool> get submitStream;
+
+  VoidCallback onSubmit;
+}
+
+class SettingsSubmit extends StatelessWidget {
+  SettingsSubmit({@required this.handler}): assert(handler != null);
+
+  final SettingsSubmitHandler handler;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<bool>(
+      initialData: false,
+      stream: handler.submitStream,
+      builder: (context,snapshot){
+        if(snapshot.hasError){
+          return SizedBox(width: 0,height: 0);
+        }else{
+          return PlatformAwareWidget(
+            android: () => IconButton(
+              icon: Icon(Icons.done,color: Colors.white),
+              onPressed: snapshot.data ? (){} : handler.onSubmit,
+            ),
+            ios: () => CupertinoIconButton(
+              icon: Icons.done,
+              onPressed: snapshot.data ? (){} : handler.onSubmit,
+            ),
+          );
+        }
+      },
+    );
+  }
+}

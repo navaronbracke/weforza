e2bd31f2c3b8f456564ce208feec4dc73bdaced6
Navaron Bracke <brackenavaron@gmail.com>
2020-03-06 12:47:55 +0100

    make scanDurationOption platform aware

--- weforza/lib/widgets/pages/settings/scanDurationOption.dart
+++ weforza/lib/widgets/pages/settings/scanDurationOption.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:weforza/generated/i18n.dart';
 import 'package:weforza/theme/appTheme.dart';
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 
 abstract class ScanDurationHandler {
   double get maxScanValue;
@@ -26,32 +27,62 @@ class ScanDurationOption extends StatefulWidget {
 class _ScanDurationOptionState extends State<ScanDurationOption> {
 
   @override
-  Widget build(BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(S.of(context).SettingsScanSliderHeader,style: ApplicationTheme.settingsOptionHeaderStyle),
-      SizedBox(height: 5),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: SliderTheme(
-          data: SliderTheme.of(context).copyWith(
-            trackHeight: 5,
-            thumbColor: ApplicationTheme.settingsScanSliderThumbColor
-          ),
-          child: Slider.adaptive(
-            value: widget.handler.currentValue,
-            onChanged: (value){
-              setState(() {
-                widget.handler.onChanged(value);
-              });
-            },
-            min: widget.handler.minScanValue,
-            max: widget.handler.maxScanValue,
-            divisions: 5,
+  Widget build(BuildContext context) => PlatformAwareWidget(
+    android: () => _buildAndroidWidget(context),
+    ios: () => _buildIosWidget(context),
+  );
+
+  Widget _buildAndroidWidget(BuildContext context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(S.of(context).SettingsScanSliderHeader,style: ApplicationTheme.settingsOptionHeaderStyle),
+        SizedBox(height: 5),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: SliderTheme(
+            data: SliderTheme.of(context).copyWith(
+              trackHeight: 5,
+              thumbColor: ApplicationTheme.settingsScanSliderThumbColor
+            ),
+            child: Slider(
+              value: widget.handler.currentValue,
+              onChanged: (value)=> setState(() => widget.handler.onChanged(value)),
+              min: widget.handler.minScanValue,
+              max: widget.handler.maxScanValue,
+              divisions: 5,
+            ),
           ),
         ),
-      ),
-      Center(child: Text("${widget.handler.currentValue.floor()}s")),
-    ],
-  );
+        Center(child: Text("${widget.handler.currentValue.floor()}s")),
+      ],
+    );
+  }
+
+  Widget _buildIosWidget(BuildContext context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(S.of(context).SettingsScanSliderHeader,style: ApplicationTheme.settingsOptionHeaderStyle),
+        SizedBox(height: 5),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: CupertinoSlider(
+                  value: widget.handler.currentValue,
+                  onChanged: (value) => setState(() => widget.handler.onChanged(value)),
+                  min: widget.handler.minScanValue,
+                  max: widget.handler.maxScanValue,
+                  divisions: 5,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Center(child: Text("${widget.handler.currentValue.floor()}s")),
+      ],
+    );
+  }
 }

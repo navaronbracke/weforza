4d3070b626061aae8d6ed4592c6c7c3de1904c92
navaronbracke <brackenavaron@gmail.com>
2020-06-17 13:09:59 +0200

    Updated Member/Ride list BLoc classes. Removed old device/member providers.

--- weforza/lib/provider/deviceProvider.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:weforza/model/device.dart';
-
-///This class provides a reload flag for loading [Device]s from a datasource.
-class DeviceProvider {
-
-  //Whether we have to reload the devices or not.
-  static bool reloadDevices = true;
-}

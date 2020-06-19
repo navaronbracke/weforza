0a4b342e46810f9ab514c68107663430fcfc68c2
navaronbracke <brackenavaron@gmail.com>
2020-06-19 18:12:47 +0200

    Deprecated ride provider

--- weforza/lib/provider/rideProvider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:weforza/model/ride.dart';
-
-///This class provides a global container for a selected [Ride] and a reload flag for loading [Ride]s from a datasource.
-class RideProvider {
-
-  ///A selected [Ride] is stored here.
-  static Ride selectedRide;
-}

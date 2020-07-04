27f4d7d19001526c470e070b86e9968ebc7d0b1c
navaronbracke <brackenavaron@gmail.com>
2020-07-04 19:26:21 +0200

    Update MemberDeviceListItem for the delete dialog

--- weforza/lib/widgets/pages/memberDetails/memberDevicesListItem.dart
+++ weforza/lib/widgets/pages/memberDetails/memberDevicesListItem.dart
@@ -1,18 +1,27 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/device.dart';
 import 'package:weforza/theme/appTheme.dart';
+import 'package:weforza/widgets/custom/deleteItemDialog/deleteItemDialog.dart';
 import 'package:weforza/widgets/pages/editDevice/editDevicePage.dart';
 import 'package:weforza/widgets/platform/cupertinoIconButton.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 import 'package:weforza/widgets/providers/selectedItemProvider.dart';
 
 class MemberDevicesListItem extends StatefulWidget {
-  MemberDevicesListItem({@required this.device}): assert(device != null);
+  MemberDevicesListItem({
+    @required this.device,
+    @required this.onDelete,
+    @required this.index,
+  }): assert(device != null && onDelete != null && index != null);
 
   //Initial value from the list builder
   final Device device;
+  final int index;
+  final Future<void> Function(String deviceName, int index) onDelete;
 
   @override
   _MemberDevicesListItemState createState() => _MemberDevicesListItemState();
@@ -30,7 +39,35 @@ class _MemberDevicesListItemState extends State<MemberDevicesListItem> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) => PlatformAwareWidget(
+    android: () => GestureDetector(
+      onLongPress: () => showDialog(
+          barrierDismissible: false,
+          context: context,
+          builder: (_) => DeleteItemDialog(
+            title: S.of(context).DeleteDeviceTitle,
+            description: S.of(context).DeleteDeviceDescription,
+            errorDescription: S.of(context).DeleteDeviceErrorDescription,
+            onDelete: () => widget.onDelete(device.name, widget.index),
+          ),
+      ),
+      child: _buildItem(context),
+    ),
+    ios: () => GestureDetector(
+      onLongPress: () => showCupertinoDialog(
+        context: context,
+        builder: (_) => DeleteItemDialog(
+          title: S.of(context).DeleteDeviceTitle,
+          description: S.of(context).DeleteDeviceDescription,
+          errorDescription: S.of(context).DeleteDeviceErrorDescription,
+          onDelete: () => widget.onDelete(device.name, widget.index),
+        ),
+      ),
+      child: _buildItem(context),
+    ),
+  );
+
+  Widget _buildItem(BuildContext context){
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Row(

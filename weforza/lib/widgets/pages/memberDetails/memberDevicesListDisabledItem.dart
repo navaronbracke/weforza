6b842c1a0f9283760d7291d95d02872c9e67c19d
navaronbracke <brackenavaron@gmail.com>
2020-07-05 12:31:51 +0200

    Added disabled device item

---
+++ weforza/lib/widgets/pages/memberDetails/memberDevicesListDisabledItem.dart
@@ -0,0 +1,56 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:weforza/model/device.dart';
+import 'package:weforza/theme/appTheme.dart';
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
+
+class MemberDevicesListDisabledItem extends StatelessWidget {
+  MemberDevicesListDisabledItem({
+    @required this.device,
+  }): assert(device != null);
+
+  final Device device;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: _mapDeviceTypeToIcon(),
+          ),
+          Expanded(
+              child: Text(device.name, overflow: TextOverflow.ellipsis)
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: _buildButtonWhitespace(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //During deletion we shouldn't show the edit button.
+  //However simply 'removing the button' might look ugly.
+  //Hence we show whitespace instead, that is the size of the button.
+  Widget _buildButtonWhitespace() => PlatformAwareWidget(
+    android: () => SizedBox(width: 40, height: 40),
+    ios: () => SizedBox(width: 24, height: 24),
+  );
+
+  Widget _mapDeviceTypeToIcon(){
+    switch(device.type){
+      case DeviceType.HEADSET: return Icon(Icons.headset,color: ApplicationTheme.deviceIconColor);
+      case DeviceType.WATCH: return Icon(Icons.watch,color: ApplicationTheme.deviceIconColor);
+      case DeviceType.TABLET: return Icon(Icons.tablet,color: ApplicationTheme.deviceIconColor);
+      case DeviceType.PHONE: return Icon(Icons.smartphone,color: ApplicationTheme.deviceIconColor);
+      case DeviceType.GPS: return Icon(Icons.gps_fixed,color: ApplicationTheme.deviceIconColor);
+      case DeviceType.PULSE_MONITOR: return Icon(Icons.favorite_border,color: ApplicationTheme.deviceIconColor);
+      default: return Icon(Icons.device_unknown,color: ApplicationTheme.deviceIconColor);
+    }
+  }
+}

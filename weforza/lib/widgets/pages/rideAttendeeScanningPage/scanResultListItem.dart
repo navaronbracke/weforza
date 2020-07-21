c1de6b4186c081732eb41438f14351de0d7d828b
Navaron Bracke <brackenavaron@gmail.com>
2020-07-21 20:58:05 +0200

    Use stateless widgets in scan result item builder

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/scanResultListItem.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/scanResultListItem.dart
@@ -15,53 +15,53 @@ class ScanResultListItem extends StatelessWidget {
 
   final ScanResultItem item;
 
-  Widget _buildErrorItem(){
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Text(item.deviceName, overflow: TextOverflow.ellipsis),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Icon(
-              Icons.error_outline,
-              color: ApplicationTheme.rideAttendeeScanResultWarningColor,
-            ),
-          ),
-        ],
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAwareWidget(
+      android: () => SizedBox(height: 45, child: _buildItem()),
+      ios: () => SizedBox(height: 50, child: _buildItem()),
     );
   }
 
-  Widget _buildNotFoundItem(){
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Text(item.deviceName, overflow: TextOverflow.ellipsis),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Icon(
-              Icons.help_outline,
-              color: ApplicationTheme.rideAttendeeScanResultUnknownDeviceColor,
-            ),
-          ),
-        ],
-      ),
-    );
+  Widget _buildItem(){
+    return FutureBuilder<Member>(
+        future: item.memberLookup,
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.done){
+            if(snapshot.hasError){
+              return ScanResultError(deviceName: item.deviceName);
+            }else{
+              if(snapshot.data == null){
+                return ScanResultOwnerNotFound(deviceName: item.deviceName);
+              }else{
+                return ScanResultOwnerFound(
+                  deviceName: item.deviceName,
+                  owner: snapshot.data,
+                );
+              }
+            }
+          }else{
+            return ScanResultLoading(deviceName: item.deviceName);
+          }
+        },
+      );
   }
+}
 
-  Widget _buildLoadingItem(){
+class ScanResultLoading extends StatelessWidget {
+  ScanResultLoading({@required this.deviceName}):
+   assert(deviceName != null && deviceName.isNotEmpty);
+
+  final String deviceName;
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Text(item.deviceName, overflow: TextOverflow.ellipsis),
+            child: Text(deviceName, overflow: TextOverflow.ellipsis),
           ),
           Padding(
             padding: const EdgeInsets.only(left: 10),
@@ -75,14 +75,83 @@ class ScanResultListItem extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildDeviceWithOwnerItem(BuildContext context, Member owner){
+class ScanResultError extends StatelessWidget {
+  ScanResultError({@required this.deviceName}):
+   assert(deviceName != null && deviceName.isNotEmpty);
+
+  final String deviceName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(deviceName, overflow: TextOverflow.ellipsis),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Icon(
+              Icons.error_outline,
+              color: ApplicationTheme.rideAttendeeScanResultWarningColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScanResultOwnerNotFound extends StatelessWidget {
+  ScanResultOwnerNotFound({@required this.deviceName}):
+   assert(deviceName != null && deviceName.isNotEmpty);
+
+  final String deviceName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(deviceName, overflow: TextOverflow.ellipsis),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Icon(
+              Icons.help_outline,
+              color: ApplicationTheme.rideAttendeeScanResultUnknownDeviceColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScanResultOwnerFound extends StatelessWidget {
+  ScanResultOwnerFound({
+    @required this.deviceName,
+    @required this.owner,
+  }): assert(
+    owner != null && deviceName != null && deviceName.isNotEmpty
+  );
+
+  final String deviceName;
+  final Member owner;
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text(item.deviceName, overflow: TextOverflow.ellipsis),
+          Text(deviceName, overflow: TextOverflow.ellipsis),
           SizedBox(height: 2),
           Row(
             children: <Widget>[
@@ -113,34 +182,5 @@ class ScanResultListItem extends StatelessWidget {
 
   String _formatDeviceOwnerTelephoneLabel(BuildContext context, String telephone){
     return "(${S.of(context).RideAttendeeScanningScanResultOwnerTelephoneLabel}$telephone)";
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return PlatformAwareWidget(
-      android: () => SizedBox(height: 45, child: _buildItem()),
-      ios: () => SizedBox(height: 50, child: _buildItem()),
-    );
-  }
-
-  Widget _buildItem(){
-    return FutureBuilder<Member>(
-        future: item.memberLookup,
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.done){
-            if(snapshot.hasError){
-              return _buildErrorItem();
-            }else{
-              if(snapshot.data == null){
-                return _buildNotFoundItem();
-              }else{
-                return _buildDeviceWithOwnerItem(context,snapshot.data);
-              }
-            }
-          }else{
-            return _buildLoadingItem();
-          }
-        },
-      );
   }
 }

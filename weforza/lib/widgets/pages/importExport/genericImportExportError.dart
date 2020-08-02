265b4a2afd35d9ebb37d7ddc05daa64e75f3a41f
navaronbracke <brackenavaron@gmail.com>
2020-08-02 10:14:15 +0200

    Updated AppTheme. Added import/export choice card. Added import export generic error widget.

---
+++ weforza/lib/widgets/pages/importExport/genericImportExportError.dart
@@ -0,0 +1,23 @@
+import 'package:weforza/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:weforza/theme/appTheme.dart';
+
+class GenericImportExportError extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.warning,
+            color: ApplicationTheme.listInformationalIconColor,
+            size: MediaQuery.of(context).size.shortestSide * .1,
+          ),
+          SizedBox(height: 5),
+          Text(S.of(context).GenericError)
+        ],
+      ),
+    );
+  }
+}

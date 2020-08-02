d6f276aeac14c567c17282ca2f91335e279ed84c
navaronbracke <brackenavaron@gmail.com>
2020-08-02 10:31:54 +0200

    Removed profile image picking state enum in favor of bool.

--- weforza/lib/widgets/custom/profileImage/iProfileImagePicker.dart
+++ weforza/lib/widgets/custom/profileImage/iProfileImagePicker.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 
-import 'package:weforza/widgets/custom/profileImage/profileImagePickingState.dart';
-
 ///This class defines a contract for picking profile images.
 abstract class IProfileImagePicker {
 
   ///Get the picking stream.
-  Stream<ProfileImagePickingState> get stream;
+  Stream<bool> get stream;
   ///Get the selected image.
   File get selectedImage;
   ///Pick a profile image.

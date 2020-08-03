32b4cacb2d01abd299b808ed4332a79a509e449d
navaronbracke <brackenavaron@gmail.com>
2020-08-03 21:47:25 +0200

    Removed export url from Settings class.

--- weforza/lib/model/settings.dart
+++ weforza/lib/model/settings.dart
@@ -1,21 +1,17 @@
 ///This class defines a wrapper for application settings.
 class Settings {
   Settings({
-    this.scanDuration = 20,
-    this.exportUrl = ""
+    this.scanDuration = 20
   }): assert(scanDuration != null && scanDuration > 0);
 
   ///The duration of a device scan, in seconds.
   ///Defaults to 20 seconds
   final int scanDuration;
 
-  final String exportUrl;
-
   ///Convert this object to a Map.
   Map<String,dynamic> toMap(){
     return {
       "scanDuration": scanDuration,
-      "exportUrl": exportUrl
     };
   }
 
@@ -23,8 +19,7 @@ class Settings {
   static Settings of(Map<String,dynamic> values){
     assert(values != null);
     return Settings(
-      scanDuration: values["scanDuration"] ?? 20,
-      exportUrl: values["exportUrl"] ?? ""
+      scanDuration: values["scanDuration"] ?? 20
     );
   }
 }

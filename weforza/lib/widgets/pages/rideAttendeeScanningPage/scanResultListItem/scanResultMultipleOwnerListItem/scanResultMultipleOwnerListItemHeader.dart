dd82187d807fec402366ebd0ad6813427e61c52f
navaronbracke <brackenavaron@gmail.com>
2020-09-20 19:29:41 +0200

    Refactored multiple owner list item header.

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/scanResultListItem/scanResultMultipleOwnerListItem/scanResultMultipleOwnerListItemHeader.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/scanResultListItem/scanResultMultipleOwnerListItem/scanResultMultipleOwnerListItemHeader.dart
@@ -1,79 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/theme/appTheme.dart';
 
 class ScanResultMultipleOwnerListItemHeader extends StatelessWidget {
   ScanResultMultipleOwnerListItemHeader({
     @required this.onTap,
-    @required this.ownerLabelTextStyle,
-    @required this.deviceNameStyle,
-    @required this.deviceOwnedByLabel,
-    @required this.iconBuilder,
-    @required this.deviceName
+    @required this.choiceRequired,
+    @required this.animation,
+    @required this.title
   }): assert(
-    onTap != null && deviceName != null && deviceName.isNotEmpty &&
-    iconBuilder != null && deviceOwnedByLabel != null &&
-        deviceOwnedByLabel.isNotEmpty && ownerLabelTextStyle != null
+    onTap != null && choiceRequired != null && animation != null
   );
 
   /// The on tap function for tapping the header.
   final void Function() onTap;
 
-  /// This function creates the menu icon.
-  final Widget Function() iconBuilder;
+  final bool choiceRequired;
 
-  /// The device name that should be displayed.
-  final String deviceName;
+  /// Is null when no owner is selected.
+  final String title;
 
-  /// The text that denotes who is selected as the device owner.
-  final String deviceOwnedByLabel;
-
-  /// The text style for the selectable device name.
-  /// Is required as constructor argument but can be null.
-  final TextStyle deviceNameStyle;
-
-  /// The text style for the owner label.
-  final TextStyle ownerLabelTextStyle;
+  final Animation<double> animation;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.all(5),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SelectableText(
-                      deviceName,
-                      style: deviceNameStyle,
-                      scrollPhysics: ClampingScrollPhysics()
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Text(
-                      deviceOwnedByLabel,
-                      style: ownerLabelTextStyle,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  )
-                ],
+      child: Container(
+        decoration: BoxDecoration(
+            color: choiceRequired ? ApplicationTheme.rideAttendeeScanResultOwnerChoiceRequiredBackgroundColor: null
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Row(
+            children: [
+              Expanded(
+                child: _buildTitle(context),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: SizedBox(
-                width: 30,
-                height: 30,
-                child: Center(
-                  child: iconBuilder(),
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: Center(
+                    child: _buildHeaderIcon(choiceRequired),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+      )
+    );
+  }
+
+  Widget _buildHeaderIcon(bool required){
+    return RotationTransition(
+      turns: animation,
+      child: Icon(
+          Icons.keyboard_arrow_down,
+          color: required ? ApplicationTheme.rideAttendeeScanResultOwnerChoiceRequiredBackgroundColor : null,
       ),
     );
+  }
+
+  Widget _buildTitle(BuildContext context){
+    if(title == null){
+      return Text(
+        S.of(context).RideAttendeeScanningScanResultMultipleOwnersPickAChoiceLabel,
+        style: ApplicationTheme.rideAttendeeScanResultMultipleOwnerChoiceLabelStyle.copyWith(
+          color: choiceRequired ? ApplicationTheme.rideAttendeeScanResultOwnerChoiceRequiredFontColor : null
+        ),
+      );
+    }else{
+      return Text(title);
+    }
   }
 }

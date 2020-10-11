6925359b9d48c99532786a2a9c8d1ad3d490e194
navaronbracke <brackenavaron@gmail.com>
2020-10-11 15:12:40 +0200

    Converted json file reader to new interface.

--- weforza/lib/file/jsonFileReader.dart
+++ weforza/lib/file/jsonFileReader.dart
@@ -3,43 +3,32 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:weforza/exceptions/exceptions.dart';
+import 'package:weforza/file/importMembersFileReader.dart';
 import 'package:weforza/model/device.dart';
 import 'package:weforza/model/exportableMember.dart';
 import 'package:weforza/model/member.dart';
 
 /// This class will read the contents of JSON files.
-class JsonFileReader {
+class JsonFileReader implements ImportMembersFileReader<Map<String, dynamic>> {
 
-  Future<List<Map<String, dynamic>>> readObjects(File file) async {
-    // The json decode can fail.
-    // The cast to a list can fail.
-    // The item cast to Map<String, dynamic> can fail.
-    try {
-      final Map<String, dynamic> json = jsonDecode(await file.readAsString());
-
-      return (json["members"] as List).map((item)=> item as Map<String, dynamic>).toList();
-    }catch(e){
-      return Future.error(JsonFormatIncompatibleException());
-    }
-  }
-
-  // Process the given json and add it to collection if its valid.
+  // Process the given data and add it to collection if its valid.
   // This function is a future, so we can process multiple json objects at once.
-  Future<void> processJson(Map<String, dynamic> json, List<ExportableMember> collection){
+  @override
+  Future<void> processData(Map<String, dynamic> data, List<ExportableMember> collection) async {
     String firstName;
     String lastName;
     String alias;
     List<String> deviceNames;
 
     try{
-      firstName = json["firstName"] as String;
-      lastName = json["lastName"] as String;
-      alias = json["alias"] as String;
-      deviceNames = (json["devices"] as List).cast<String>();
+      firstName = data["firstName"] as String;
+      lastName = data["lastName"] as String;
+      alias = data["alias"] as String;
+      deviceNames = (data["devices"] as List).cast<String>();
 
       // The first name, last name or alias are not suitable for a member.
       if(!Member.personNameAndAliasRegex.hasMatch(firstName) || !Member.personNameAndAliasRegex.hasMatch(lastName) || alias.isNotEmpty && !Member.personNameAndAliasRegex.hasMatch(alias)){
-        return null;
+        return;
       }
 
       collection.add(ExportableMember(
@@ -48,8 +37,24 @@ class JsonFileReader {
           alias: alias,
           devices: deviceNames.sublist(3).where((deviceName) => Device.deviceNameRegex.hasMatch(deviceName)).toSet()
       ));
+    }catch(e){
+      return Future.error(JsonFormatIncompatibleException());
+    }
+  }
 
-      return null;
+  /// Read the individual json objects of the given [file].
+  /// Throws a [JsonFormatIncompatibleException] if the file is not valid json.
+  /// Returns the lines that were read from the file.
+  /// Returns an empty list if the file is empty.
+  @override
+  Future<List<Map<String, dynamic>>> readFile(File file) async {
+    // The json decode can fail.
+    // The cast to a list can fail.
+    // The item cast to Map<String, dynamic> can fail.
+    try {
+      final Map<String, dynamic> json = jsonDecode(await file.readAsString());
+
+      return (json["members"] as List).map((item)=> item as Map<String, dynamic>).toList();
     }catch(e){
       return Future.error(JsonFormatIncompatibleException());
     }

45f5cef38f139da66b2ba91ac6d9687350f3a279
navaronbracke <brackenavaron@gmail.com>
2020-10-11 14:07:08 +0200

    Updated csvFileReader to use new signature.

--- weforza/lib/file/csvFileReader.dart
+++ weforza/lib/file/csvFileReader.dart
@@ -4,8 +4,8 @@ import 'dart:io';
 
 import 'package:weforza/exceptions/exceptions.dart';
 import 'package:weforza/model/device.dart';
+import 'package:weforza/model/exportableMember.dart';
 import 'package:weforza/model/member.dart';
-import 'package:weforza/model/tuple.dart';
 
 /// This class will read the contents of CSV files.
 class CsvFileReader {
@@ -16,17 +16,17 @@ class CsvFileReader {
   }
 
   /// Read the [Member]s and their [Device]s from the given file.
-  /// The [generateId] function is used to generate UUIDs for member objects.
   /// The [headerRegex] is used to strip any possible csv header.
-  Future<Tuple<Set<Member>,Set<Device>>> readMembersFromFile(File file, String Function() generateId, String headerRegex) async {
-    List<String> lines = await _readLines(file);
-    //Return fast when there are no lines.
-    if(lines.isEmpty) return Tuple<Set<Member>,Set<Device>>(Set(), Set());
+  Future<Iterable<ExportableMember>> readMembersFromFile(File file, String headerRegex) async {
+    // This list will hold the final output.
+    final List<ExportableMember> exports = [];
 
-    //These collections will contain the items we need to insert.
-    //Because these are Sets, they disallow duplicates.
-    final Set<Member> members = Set();
-    final Set<Device> devices = Set();
+    List<String> lines = await _readLines(file);
+
+    //Return fast when there are no lines.
+    if(lines.isEmpty) return exports;
+
+    // This list contains the line readers that will run in parallel.
     final List<Future<void>> lineReaders = [];
 
     //Remove all the spaces in the possible and turn into lower case.
@@ -43,51 +43,46 @@ class CsvFileReader {
 
     //Add a line processor for each line and run them in parallel.
     for(String line in lines){
-      lineReaders.add(_processLine(line, members, devices, generateId));
+      lineReaders.add(_processLine(line, exports));
     }
 
     await Future.wait(lineReaders);
 
-    return Tuple<Set<Member>,Set<Device>>(members, devices);
+    return exports;
   }
 
-  //Process the given line of data and add the member and devices to the given collections.
+  //Process the given line of data and add the member and devices to the given collection.
   //This function is a future, so we can process multiple lines at once.
-  Future<void> _processLine(String line, Set<Member> members, Set<Device> devices, String Function() generateId) async {
+  Future<void> _processLine(String line, List<ExportableMember> collection) async {
     final List<String> values = line.split(',');
 
     //If the line doesn't have enough cells to fill the required fields
     // (first name, last name and alias, in that order), skip it
     if(values.length < 3){
-      return null;
+      return;
     }
 
     //Invalid data lines are skipped
     if(!Member.personNameAndAliasRegex.hasMatch(values[0]) || !Member.personNameAndAliasRegex.hasMatch(values[1]) || (values[2].isNotEmpty && !Member.personNameAndAliasRegex.hasMatch(values[2]))){
-      return null;
+      return;
     }
 
-    //Create the member and add it to the list
-    final Member member = Member(
-      generateId(),
-      values[0],
-      values[1],
-      values[2],
-    );
-
-    members.add(member);
+    // This Set will keep the device names.
+    // Since its a Set, duplicates are ignored.
+    final Set<String> devices = Set();
 
     //Besides First Name, Last Name, Alias, there are more values
     //These are the device names: Device1, Device2,... , DeviceN
     if(values.length > 3){
-      //Check the device names
-      final deviceNames = values.sublist(3);
-
-      //Remove the invalid ones but keep the rest
-      deviceNames.retainWhere((deviceName) => Device.deviceNameRegex.hasMatch(deviceName));
-
-      //Add the devices to the collection. Since its a Set, duplicates are ignored.
-      deviceNames.forEach((String deviceName) => devices.add(Device(name: deviceName, creationDate: DateTime.now(), ownerId: member.uuid, type: DeviceType.UNKNOWN)));
+      //Filter the invalid device names.
+      devices.addAll(values.sublist(3).where((deviceName) => Device.deviceNameRegex.hasMatch(deviceName)));
     }
+
+    collection.add(ExportableMember(
+      firstName: values[0],
+      lastName: values[1],
+      alias: values[2],
+      devices: devices
+    ));
   }
 }

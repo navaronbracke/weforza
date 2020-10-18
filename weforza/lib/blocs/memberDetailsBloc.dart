ebff5e44ef54ee142ebc8fefc7115fd42881c78f
navaronbracke <brackenavaron@gmail.com>
2020-10-18 18:30:36 +0200

    Added setActive(uuid, value) method to memberDetailsBloc.

--- weforza/lib/blocs/memberDetailsBloc.dart
+++ weforza/lib/blocs/memberDetailsBloc.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:weforza/blocs/bloc.dart';
 import 'package:weforza/model/device.dart';
 import 'package:weforza/model/member.dart';
@@ -18,10 +20,16 @@ class MemberDetailsBloc extends Bloc {
   }): assert(
     memberRepository != null && deviceRepository != null && member != null
         && profileImage != null && attendingCountFuture != null
-  );
+  ){
+    _isActiveController = BehaviorSubject.seeded(member.isActiveMember);
+  }
 
   final MemberRepository memberRepository;
   final DeviceRepository deviceRepository;
+
+  StreamController<bool> _isActiveController;
+
+  Stream<bool> get isActiveStream => _isActiveController.stream;
 
   final Future<int> attendingCountFuture;
   Member member;
@@ -40,11 +48,22 @@ class MemberDetailsBloc extends Bloc {
   }
 
   @override
-  void dispose() {}
+  void dispose() {
+    _isActiveController.close();
+  }
 
   Future<void> deleteDevice(Device device) => deviceRepository.removeDevice(device);
 
   Future<void> deleteMember() => memberRepository.deleteMember(member.uuid);
 
   Future<List<Device>> getMemberDevices() => deviceRepository.getOwnerDevices(member.uuid);
+
+  void setMemberActive(bool value) async {
+    if(member.isActiveMember != value){
+      await memberRepository.setMemberActive(member.uuid, value).then((_){
+        member.isActiveMember = value;
+        _isActiveController.add(value);
+      }).catchError(_isActiveController.addError);
+    }
+  }
 }

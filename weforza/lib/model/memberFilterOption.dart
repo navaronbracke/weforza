6b0edd0c8dc924f464737b2419611d33eeb3918d
navaronbracke <brackenavaron@gmail.com>
2020-10-31 18:42:16 +0100

    Added member filter option enum.

---
+++ weforza/lib/model/memberFilterOption.dart
@@ -0,0 +1,9 @@
+/// This enum declares the different filter options.
+/// [MemberFilterOption.ALL] No filter.
+/// [MemberFilterOption.ACTIVE] Only active members.
+/// [MemberFilterOption.INACTIVE] Only inactive members.
+enum MemberFilterOption {
+  ALL,
+  ACTIVE,
+  INACTIVE
+}

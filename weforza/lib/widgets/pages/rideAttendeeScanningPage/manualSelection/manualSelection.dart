531e69f6cb6dbf1582b67b9f7413e58bc7e09743
navaronbracke <brackenavaron@gmail.com>
2020-11-12 17:37:38 +0100

    Use a Future in ManualSelection.

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelection.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelection.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/widgets.dart';
+import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/member.dart';
+import 'package:weforza/widgets/common/genericError.dart';
 import 'package:weforza/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionListEmpty.dart';
+import 'package:weforza/widgets/platform/platformAwareLoadingIndicator.dart';
 
 class RideAttendeeManualSelection extends StatelessWidget {
   RideAttendeeManualSelection({
-    @required this.items,
+    @required this.future,
     @required this.itemBuilder,
     @required this.saveButtonBuilder
-  }): assert(items != null && itemBuilder != null && saveButtonBuilder != null);
+  }): assert(
+    future != null && itemBuilder != null && saveButtonBuilder != null
+  );
 
-  final List<Member> items;
+  final Future<List<Member>> future;
   final Widget Function(Member item) itemBuilder;
   final Widget Function(BuildContext context) saveButtonBuilder;
 
   @override
   Widget build(BuildContext context) {
-    if(items.isEmpty){
-      return Center(child: ManualSelectionListEmpty());
-    }else{
-      return Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) => itemBuilder(items[index]),
-              itemCount: items.length,
-            ),
-          ),
-          saveButtonBuilder(context),
-        ],
-      );
-    }
+    return FutureBuilder<List<Member>>(
+      future: future,
+      builder: (context, snapshot){
+        if(snapshot.connectionState == ConnectionState.done){
+          if(snapshot.hasError){
+            return GenericError(text: S.of(context).GenericError);
+          }else {
+            if(snapshot.data.isEmpty){
+              return ManualSelectionListEmpty();
+            }
+
+            return Column(
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) => itemBuilder(snapshot.data[index]),
+                    itemCount: snapshot.data.length,
+                  ),
+                ),
+                saveButtonBuilder(context),
+              ],
+            );
+          }
+        }else{
+          return Center(child: PlatformAwareLoadingIndicator());
+        }
+      },
+    );
   }
 }

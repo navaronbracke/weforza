a1197c3a06dd2f73e5faf76d299d9f375353767f
navaronbracke <brackenavaron@gmail.com>
2021-03-28 15:43:43 +0200

    Added attendee counter & updated android layout for the save button.

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionSubmit.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionSubmit.dart
@@ -2,46 +2,91 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/theme/appTheme.dart';
+import 'package:weforza/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionItemCounter.dart';
 import 'package:weforza/widgets/platform/platformAwareLoadingIndicator.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 
 class ManualSelectionSubmit extends StatelessWidget {
   ManualSelectionSubmit({
     required this.onSave,
-    required this.isSaving
+    required this.isSaving,
+    required this.attendeeCountStream,
+    required this.getAttendeeCount,
   });
 
   final void Function() onSave;
   final Stream<bool> isSaving;
+  final Stream<int> attendeeCountStream;
+  final int Function() getAttendeeCount;
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: StreamBuilder<bool>(
-        stream: isSaving,
-        initialData: false,
-        builder: (context, snapshot){
-          return PlatformAwareWidget(
-            android: () => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: snapshot.data! ? PlatformAwareLoadingIndicator() : TextButton(
+  Widget build(BuildContext context) => PlatformAwareWidget(
+    android: _buildAndroidWidget,
+    ios: _buildIosWidget,
+  );
+
+  //TODO if both widgets work similarly, rewrite the stream builder
+
+  Widget _buildAndroidWidget(){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        ManualSelectionItemCounter(
+          backgroundColor: ApplicationTheme.primaryColor,
+          countStream: attendeeCountStream,
+          initialData: getAttendeeCount(),
+        ),
+        Expanded(child: Center()),
+        StreamBuilder<bool>(
+          stream: isSaving,
+          initialData: false,
+          builder: (context, snapshot){
+            return snapshot.data! ? Padding(
+              padding: const EdgeInsets.only(right: 15, top: 15, bottom: 15),
+              child: SizedBox(
+                width: 25, height: 25,
+                child: CircularProgressIndicator(strokeWidth: 2),
+              ),
+            ): Padding(
+              padding: const EdgeInsets.only(right: 10, top: 10, bottom: 10),
+              child: ElevatedButton(
                 child: Text(S.of(context).Save),
                 onPressed: onSave,
               ),
-            ),
-            ios: () => Padding(
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildIosWidget(){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        ManualSelectionItemCounter(
+          backgroundColor: ApplicationTheme.primaryColor,
+          countStream: attendeeCountStream,
+          initialData: getAttendeeCount(),
+        ),
+        Expanded(child: Center()),
+        StreamBuilder<bool>(
+          stream: isSaving,
+          initialData: false,
+          builder: (context, snapshot){
+            return Padding(
               padding: const EdgeInsets.only(bottom: 20, top: 10),
               child: snapshot.data! ? PlatformAwareLoadingIndicator() : CupertinoButton(
                 child: Text(
-                    S.of(context).Save,
-                    style: TextStyle(color: ApplicationTheme.primaryColor)
+                  S.of(context).Save,
+                  style: TextStyle(color: ApplicationTheme.primaryColor),
                 ),
                 onPressed: onSave,
               ),
-            ),
-          );
-        },
-      ),
+            );
+          },
+        ),
+      ],
     );
   }
 }

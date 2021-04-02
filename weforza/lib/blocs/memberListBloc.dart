5427cbaaba9f54b556d63c7401fa75fb3f822bea
navaronbracke <brackenavaron@gmail.com>
2021-04-02 20:40:27 +0200

    Load the members together with the filter.

--- weforza/lib/blocs/memberListBloc.dart
+++ weforza/lib/blocs/memberListBloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:weforza/file/fileHandler.dart';
-import 'package:weforza/model/member.dart';
+import 'package:weforza/model/membersListWithFilterModel.dart';
 import 'package:weforza/repository/memberRepository.dart';
 import 'package:weforza/blocs/bloc.dart';
 import 'package:weforza/repository/settingsRepository.dart';
@@ -18,14 +18,17 @@ class MemberListBloc extends Bloc {
   final SettingsRepository _settingsRepository;
   final IFileHandler _fileHandler;
 
-  Future<List<Member>>? membersFuture;
+  Future<MembersListWithFilterModel>? membersFuture;
 
   /// Load the members.
   /// This function first does a lookup for the settings that contain the specified filter value.
   void loadMembers() {
-    membersFuture = _settingsRepository.loadApplicationSettings().then(
-          (settings)=> _repository.getMembers(settings.memberListFilter),
-    );
+    membersFuture = _settingsRepository.loadApplicationSettings().then((settings) async {
+      return MembersListWithFilterModel(
+        items: await _repository.getMembers(settings.memberListFilter),
+        filter: settings.memberListFilter,
+      );
+    });
   }
 
   Future<File?> getMemberProfileImage(String? path) => _fileHandler.loadProfileImageFromDisk(path);

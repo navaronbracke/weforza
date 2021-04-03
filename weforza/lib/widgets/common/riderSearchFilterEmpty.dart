86d783e4eb242cb0c3b33848f993a2412331fd61
navaronbracke <brackenavaron@gmail.com>
2021-04-03 11:30:18 +0200

    Replace ManualSelectionFilterEmpty with a generic variant.

---
+++ weforza/lib/widgets/common/riderSearchFilterEmpty.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart' show CupertinoIcons;
+import 'package:flutter/material.dart';
+import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/theme/appTheme.dart';
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
+
+class RiderSearchFilterEmpty extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        PlatformAwareWidget(
+          android: () => Icon(
+            Icons.people,
+            color: ApplicationTheme.listInformationalIconColor,
+            size: MediaQuery.of(context).size.shortestSide * .1,
+          ),
+          ios: () => Icon(
+            CupertinoIcons.person_2_fill,
+            color: ApplicationTheme.listInformationalIconColor,
+            size: MediaQuery.of(context).size.shortestSide * .1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 5, left: 16, right: 16),
+          child: Text(S.of(context).RiderSearchFilterNoResults),
+        ),
+      ],
+    );
+  }
+}

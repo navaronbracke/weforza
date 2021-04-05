f54cf739f39d86d8c8ddc5c0983a7a2e1eceb2da
navaronbracke <brackenavaron@gmail.com>
2021-04-05 15:34:19 +0200

    Add extra class for the importable member update payload.

--- weforza/lib/model/importableMember.dart
+++ weforza/lib/model/importableMember.dart
@@ -2,8 +2,8 @@ import 'dart:ui';
 
 /// This class serves as a wrapper during the import members phase.
 /// It allows for using [firstName], [lastName] and [alias] as keys in a Map.
-class ImportableMember {
-  ImportableMember({
+class ImportableMemberKey {
+  ImportableMemberKey({
     required this.firstName,
     required this.lastName,
     required this.alias,
@@ -14,10 +14,46 @@ class ImportableMember {
   final String alias;
 
   @override
-  bool operator ==(Object other) => other is ImportableMember
+  bool operator ==(Object other) => other is ImportableMemberKey
       && firstName == other.firstName && lastName == other.lastName
       && alias == other.alias;
 
   @override
   int get hashCode => hashValues(firstName, lastName, alias);
+}
+
+/// This class serves as a wrapper during the import members phase.
+/// It wraps only the [Member.lastUpdated] and [Member.uuid] fields.
+/// It provides the updatable content for the import members phase.
+class ImportableMember {
+  ImportableMember({
+    required this.uuid,
+    required this.updatedOn,
+  });
+
+  final String uuid;
+  final DateTime updatedOn;
+
+  /// Convert [updatedOn] to a 'YYYY-MM-DD HH-MM-SSZ' string.
+  /// E.g. 1969-07-20 20:18:04Z
+  String _lastUpdatedToString(){
+    final String s = updatedOn.toString();
+
+    // Strip the milliseconds and append a Z.
+    return s.substring(0, s.length - 4) + "Z";
+  }
+
+  ///Convert this object to a Map.
+  Map<String,dynamic> toMap(){
+    return {
+      "lastUpdated": _lastUpdatedToString(),
+    };
+  }
+
+  @override
+  bool operator ==(Object other) => other is ImportableMember
+      && uuid == other.uuid && updatedOn == other.updatedOn;
+
+  @override
+  int get hashCode => hashValues(uuid, updatedOn);
 }

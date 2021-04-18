d485ecc352e8933c22f72abf62ca87eb37595f74
navaronbracke <brackenavaron@gmail.com>
2021-04-18 09:53:59 +0200

    Add better centering for empty list widgets.

--- weforza/lib/widgets/common/riderSearchFilterEmpty.dart
+++ weforza/lib/widgets/common/riderSearchFilterEmpty.dart
@@ -24,7 +24,10 @@ class RiderSearchFilterEmpty extends StatelessWidget {
         ),
         Padding(
           padding: const EdgeInsets.only(top: 5, left: 16, right: 16),
-          child: Text(S.of(context).RiderSearchFilterNoResults),
+          child: Text(
+            S.of(context).RiderSearchFilterNoResults,
+            textAlign: TextAlign.center,
+          ),
         ),
       ],
     );

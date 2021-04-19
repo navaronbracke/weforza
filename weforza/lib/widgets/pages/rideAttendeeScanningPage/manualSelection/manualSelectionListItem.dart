d9feaa370bc84b9a5e99a6490995de76e5f563df
navaronbracke <brackenavaron@gmail.com>
2021-04-19 20:07:17 +0200

    Add a value key to list items in manual selection.

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionListItem.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionListItem.dart
@@ -7,6 +7,30 @@ import 'package:weforza/theme/appTheme.dart';
 import 'package:weforza/widgets/common/memberNameAndAlias.dart';
 import 'package:weforza/widgets/custom/profileImage/asyncProfileImage.dart';
 
+/// This class is used as the type parameter of [ManualSelectionListItem]'s key.
+class ManualSelectionListItemKey {
+  ManualSelectionListItemKey({
+    required this.firstName,
+    required this.lastName,
+    required this.alias,
+  });
+
+  final String firstName;
+  final String lastName;
+  final String alias;
+
+  @override
+  bool operator ==(Object other) {
+    return other is ManualSelectionListItemKey
+        && firstName == other.firstName
+        && lastName == other.lastName
+        && alias == other.alias;
+  }
+
+  @override
+  int get hashCode => hashValues(firstName, lastName, alias);
+}
+
 class ManualSelectionListItem extends StatefulWidget {
   ManualSelectionListItem({
     required this.isSelected,
@@ -16,7 +40,15 @@ class ManualSelectionListItem extends StatefulWidget {
     required this.lastName,
     required this.alias,
     required this.personInitials,
-  });
+  }): super(
+    key: ValueKey<ManualSelectionListItemKey>(
+      ManualSelectionListItemKey(
+        firstName: firstName,
+        lastName: lastName,
+        alias: alias,
+      ),
+    ),
+  );
 
   final bool Function() isSelected;
   final VoidCallback onTap;

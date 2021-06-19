4613d414d7fdda89d7c0c2bb6262807283c57581
navaronbracke <brackenavaron@gmail.com>
2021-06-19 16:08:38 +0200

    Added CupertinoBottomBar widget.

---
+++ weforza/lib/widgets/platform/cupertinoBottomBar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+
+/// A Cupertino equivalent of the Material [BottomAppBar].
+/// Styles have been adapted from [CupertinoTabBar].
+class CupertinoBottomBar extends StatelessWidget {
+  CupertinoBottomBar({ required this.child });
+
+  final Widget child;
+
+  /// Standard iOS 10 tab bar height.
+  /// Taken from [CupertinoTabBar].
+  final double _kTabBarHeight = 50.0;
+
+  /// BorderSide resolution, as defined in [CupertinoTabBar].
+  BorderSide resolveBorderSide(BuildContext context,  BorderSide side){
+    return side.copyWith(color: CupertinoDynamicColor.resolve(side.color, context));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double bottomPadding = MediaQuery.of(context).padding.bottom;
+    final Color backgroundColor = CupertinoTheme.of(context).barBackgroundColor;
+
+    final Border border = Border(
+      top: BorderSide(
+        // Color resolution is the same as in CupertinoTabBar.
+        color: CupertinoDynamicColor.withBrightness(
+          color: Color(0x4C000000),
+          darkColor: Color(0x29000000),
+        ),
+        //TODO: if 0.0 produces a 2 pixel border, use 1.0.
+        width: 0.0, // One physical pixel.
+        style: BorderStyle.solid,
+      ),
+    );
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border(top: resolveBorderSide(context, border.top)),
+        color: backgroundColor,
+      ),
+      child: SizedBox(
+        // We create a SizedBox that takes up the combined bar + notch height.
+        // Then we push the child upwards, using the notch height.
+        height: _kTabBarHeight + bottomPadding,
+        child: DefaultTextStyle(
+          style: CupertinoTheme.of(context).textTheme.textStyle,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: bottomPadding),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}

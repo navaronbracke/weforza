7afdc38f8e3fef82ea90d07658b5fb8c26951f7c
navaronbracke <brackenavaron@gmail.com>
2021-06-19 16:23:39 +0200

    Fix some styling issues on Android.

--- weforza/lib/widgets/pages/rideDetails/rideDetailsAttendees/rideDetailsAttendeesList.dart
+++ weforza/lib/widgets/pages/rideDetails/rideDetailsAttendees/rideDetailsAttendeesList.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/member.dart';
+import 'package:weforza/theme/appTheme.dart';
 import 'package:weforza/widgets/common/genericError.dart';
 import 'package:weforza/widgets/pages/rideDetails/rideDetailsAttendees/rideDetailsAttendeesListEmpty.dart';
 import 'package:weforza/widgets/pages/rideDetails/rideDetailsAttendees/rideDetailsAttendeesListItem.dart';
@@ -65,26 +66,35 @@ class RideDetailsAttendeesList extends StatelessWidget {
 
   Widget _buildAndroidLayout(BuildContext context, int total, int? scannedNumber){
     return BottomAppBar(
+      color: ApplicationTheme.primaryColor,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
+        padding: const EdgeInsets.all(8),
         child: Row(
           children: [
             Tooltip(
               message: S.of(context).RideDetailsTotalAttendeesTooltip,
-              child: const Icon(Icons.people, color: Colors.white),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 4),
-              child: Text("$total", style: TextStyle(color: Colors.white)),
+              child: Row(
+                children: [
+                  const Icon(Icons.people, color: Colors.white),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Text("$total", style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
             ),
             Expanded(child: Center()),
             Tooltip(
               message: S.of(context).RideDetailsScannedAttendeesTooltip,
-              child: const Icon(Icons.bluetooth_searching, color: Colors.white),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 4),
-              child: Text(scannedNumber == null ? "?" : "$scannedNumber", style: TextStyle(color: Colors.white)),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 4),
+                    child: Text(scannedNumber == null ? "?" : "$scannedNumber", style: TextStyle(color: Colors.white)),
+                  ),
+                  const Icon(Icons.bluetooth_searching, color: Colors.white),
+                ],
+              ),
             ),
           ],
         ),
@@ -110,16 +120,16 @@ class RideDetailsAttendeesList extends StatelessWidget {
               ),
             ),
             Expanded(child: Center()),
-            const Icon(
-              Icons.bluetooth_searching,
-              color: CupertinoColors.activeBlue,
-            ),
             Padding(
-              padding: const EdgeInsets.only(left: 4),
+              padding: const EdgeInsets.only(right: 4),
               child: Text(
                 scannedNumber == null ? "?" : "$scannedNumber",
                 style: TextStyle(color: CupertinoColors.activeBlue),
               ),
+            ),
+            const Icon(
+              Icons.bluetooth_searching,
+              color: CupertinoColors.activeBlue,
             ),
           ],
         ),

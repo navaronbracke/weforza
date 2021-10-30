17da77ec27a8d42d2697bd85cfb419ad0f2c0128
navaronbracke <brackenavaron@gmail.com>
2021-10-30 17:49:22 +0200

    Add the salt to the cipher implementation. Switch to CTR mode.

--- weforza/lib/cipher/data_cipher.dart
+++ weforza/lib/cipher/data_cipher.dart
@@ -1,36 +1,43 @@
-
 import 'package:encrypt/encrypt.dart';
 import 'package:weforza/cipher/cipher.dart';
 
 class DataCipher implements Cipher {
   DataCipher({
     required String encryptionKey,
+    required String encryptionSalt,
   }) {
     assert(
       encryptionKey.length == 32,
       "A DataCipher requires an encryption key that is 32 characters long. Current key length: ${encryptionKey.length}",
     );
-    _encrypter = Encrypter(AES(Key.fromUtf8(encryptionKey)));
+    _encrypter = Encrypter(
+      AES(
+        Key.fromUtf8(encryptionKey),
+        mode: AESMode.ctr,
+        padding: null,
+      ),
+    );
+    _salt = IV.fromUtf8(encryptionSalt);
   }
 
   /// The internal encrypter.
   late final Encrypter _encrypter;
 
   /// The salt that is used for encryption.
-  final IV _salt = IV.fromLength(16);
+  late final IV _salt;
 
   @override
   String decrypt(String value) {
-    if(value.isEmpty){
+    if (value.isEmpty) {
       return value;
     }
 
-    return _encrypter.decrypt64(value, iv: _salt);
+    return _encrypter.decrypt(Encrypted.fromBase64(value), iv: _salt);
   }
 
   @override
   String encrypt(String value) {
-    if(value.isEmpty){
+    if (value.isEmpty) {
       return value;
     }
 

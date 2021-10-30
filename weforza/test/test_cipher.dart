1d565bbf6447f87f6a221922e56a9ebb77304d90
navaronbracke <brackenavaron@gmail.com>
2021-10-30 17:45:53 +0200

    Add a salt to the test cipher.

--- weforza/test/test_cipher.dart
+++ weforza/test/test_cipher.dart
@@ -2,5 +2,8 @@ import 'package:weforza/cipher/data_cipher.dart';
 
 /// This [DataCipher] uses a fixed encryption key that is suitable for testing.
 class TestCipher extends DataCipher {
-  TestCipher(): super(encryptionKey: "01234567890123456789012345678901");
+  TestCipher(): super(
+      encryptionKey: "01234567890123456789012345678901",
+      encryptionSalt: "HyTLaZVttOjWriUx",
+  );
 }

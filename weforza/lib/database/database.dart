0b91cb89ea2a797db48999c8351e9d319b15a497
navaronbracke <brackenavaron@gmail.com>
2021-10-30 17:53:35 +0200

    Add the onVersionChanged function. Move the database store names out of the database file.

--- weforza/lib/database/database.dart
+++ weforza/lib/database/database.dart
@@ -1,13 +1,8 @@
 
-import 'dart:io';
-
-import 'package:file/file.dart' show FileSystem;
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:weforza/database/databaseFactory.dart';
-import 'package:weforza/model/rideAttendee.dart';
-import 'package:weforza/model/member.dart';
-import 'package:weforza/model/ride.dart';
+import 'package:weforza/database/database_migration.dart';
 import 'package:path/path.dart';
 
 ///This class wraps a Sembast [Database] and datastore instances along with initializer and close methods.
@@ -19,65 +14,26 @@ class ApplicationDatabase {
   //The database filename.
   final String databaseName;
 
-  ///The data store for [Member].
-  final memberStore = stringMapStoreFactory.store("member");
-
-  ///The data store for [Ride].
-  final rideStore = stringMapStoreFactory.store("ride");
-
-  ///The data store for [RideAttendee].
-  final rideAttendeeStore = stringMapStoreFactory.store("rideAttendee");
-
-  ///The data store for the member devices.
-  final deviceStore = stringMapStoreFactory.store("device");
-
-  ///The data store for general application settings.
-  final settingsStore = stringMapStoreFactory.store("settings");
-
   // The internal instance of the database.
   // We can assume that it is created before it is used.
   late Database _database;
 
-  ///Get the database instance.
+  /// Get the database instance.
   Database getDatabase() => _database;
 
-  //TODO remove when migrated to new directory.
-  /// Moves the database from the old Documents directory
-  /// to the Application Support directory for the current platform.
-  Future<void> _moveDatabase(String newDatabasePath, FileSystem fileSystem) async {
-    final oldDirectory = await getApplicationDocumentsDirectory();
-    final oldDatabasePath = join(oldDirectory.path, databaseName);
-    final file = fileSystem.file(oldDatabasePath);
-
-    if(await file.exists()){
-      print("Found database on old location: $oldDatabasePath");
-      print("Migrating database to new location: $newDatabasePath");
-
-      try {
-        print("Using migration strategy: RENAME FILE");
-
-        await file.rename(newDatabasePath);
-      } on FileSystemException {
-        print("Migration using migration strategy: RENAME FILE failed.");
-        print("Retrying with migration strategy: MOVE FILE");
-
-        await file.copy(newDatabasePath);
-        await file.delete();
-      }
-
-      print("Done migrating database.");
-    }
-  }
-
   ///Initialize the database.
-  Future<void> openDatabase(ApplicationDatabaseFactory databaseFactory) async {
+  Future<void> openDatabase(ApplicationDatabaseFactory databaseFactory, DatabaseMigration migration, {int version = 2}) async {
     final databaseDirectory = await getApplicationSupportDirectory();
     final dbPath = join(databaseDirectory.path, databaseName);
 
     // Move the database to the new directory if required.
-    await _moveDatabase(dbPath, databaseFactory.fileSystem);
+    await migration.moveDatabase(dbPath, databaseFactory.fileSystem, databaseName);
 
-    _database = await databaseFactory.factory.openDatabase(dbPath);
+    _database = await databaseFactory.factory.openDatabase(
+      dbPath,
+      version: version,
+      onVersionChanged: migration.migrateDatabaseToUseEncryption,
+    );
   }
 
   void dispose() async =>  await _database.close();

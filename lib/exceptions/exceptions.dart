d313eb8af7671c0413ed0c4351c87bb2e04c9865
Navaron Bracke <brackenavaron@gmail.com>
2022-03-06 14:11:54 +0100

    format exceptions

--- lib/exceptions/exceptions.dart
+++ lib/exceptions/exceptions.dart
@@ -1,16 +1,15 @@
-
 class CsvHeaderMissingError extends ArgumentError {
-  CsvHeaderMissingError(): super.value("A csv file requires a header line.");
+  CsvHeaderMissingError() : super.value('A csv file requires a header line.');
 }
 
 class NoFileChosenError extends ArgumentError {
-  NoFileChosenError(): super.notNull("No file was chosen.");
+  NoFileChosenError() : super.notNull('No file was chosen.');
 }
 
 class InvalidFileExtensionError extends ArgumentError {
-  InvalidFileExtensionError(): super("Invalid file extension.");
+  InvalidFileExtensionError() : super('Invalid file extension.');
 }
 
 class JsonFormatIncompatibleException extends ArgumentError {
-  JsonFormatIncompatibleException(): super("The given json is incompatible.");
+  JsonFormatIncompatibleException() : super('The given json is incompatible.');
 }

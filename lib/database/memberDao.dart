b25fb5db1906fcaf59fe0f16efd8c175dd89ebe7
Navaron Bracke <brackenavaron@gmail.com>
2022-03-06 16:23:50 +0100

    dart format + replace quotes

--- lib/database/memberDao.dart
+++ lib/database/memberDao.dart
@@ -42,33 +42,30 @@ abstract class IMemberDao {
 
 ///This class is an implementation of [IMemberDao].
 class MemberDao implements IMemberDao {
-  MemberDao(
-      this._database,
-      this._memberStore,
-      this._rideAttendeeStore,
+  MemberDao(this._database, this._memberStore, this._rideAttendeeStore,
       this._deviceStore);
 
-  MemberDao.withProvider(ApplicationDatabase provider): this(
-    provider.getDatabase(),
-    provider.memberStore,
-    provider.rideAttendeeStore,
-    provider.deviceStore
-  );
+  MemberDao.withProvider(ApplicationDatabase provider)
+      : this(provider.getDatabase(), provider.memberStore,
+            provider.rideAttendeeStore, provider.deviceStore);
 
   ///A reference to the application database.
   final Database _database;
+
   ///A reference to the [Member] store.
   final StoreRef<String, Map<String, dynamic>> _memberStore;
+
   ///A reference to the [RideAttendee] store.
   final StoreRef<String, Map<String, dynamic>> _rideAttendeeStore;
+
   ///A reference to the [Device] store.
   final StoreRef<String, Map<String, dynamic>> _deviceStore;
 
   @override
-  Future<void> addMember(Member member) async  {
-    ///The uuid is already used
-    if(await _memberStore.findFirst(_database, finder: Finder(filter: Filter.byKey(member.uuid))) != null){
-      throw Exception("The member's uuid: ${member.uuid} is already in use");
+  Future<void> addMember(Member member) async {
+    final finder = Finder(filter: Filter.byKey(member.uuid));
+    if (await _memberStore.findFirst(_database, finder: finder) != null) {
+      throw Exception('The uuid ${member.uuid} is already in use');
     }
     await _memberStore.record(member.uuid).add(_database, member.toMap());
   }
@@ -77,30 +74,38 @@ class MemberDao implements IMemberDao {
   Future<void> deleteMember(String uuid) async {
     //delete the ride attendee records, the member's devices and the member
     final memberFinder = Finder(filter: Filter.byKey(uuid));
-    final memberRidesFinder = Finder(filter: Filter.equals("attendee", uuid));
-    final memberDeviceFinder = Finder(filter: Filter.equals("owner", uuid));
-    
+    final memberRidesFinder = Finder(filter: Filter.equals('attendee', uuid));
+    final memberDeviceFinder = Finder(filter: Filter.equals('owner', uuid));
+
     await _database.transaction((txn) async {
-      await _memberStore.delete(txn,finder: memberFinder);
-      await _rideAttendeeStore.delete(txn,finder: memberRidesFinder);
-      await _deviceStore.delete(txn,finder: memberDeviceFinder);
+      await _memberStore.delete(txn, finder: memberFinder);
+      await _rideAttendeeStore.delete(txn, finder: memberRidesFinder);
+      await _deviceStore.delete(txn, finder: memberDeviceFinder);
     });
   }
 
   @override
   Future<List<Member>> getMembers(MemberFilterOption filterOption) async {
-    final Finder finder = Finder(
-        sortOrders: [SortOrder("firstname"),SortOrder("lastname"),SortOrder("alias")]
-    );
+    final Finder finder = Finder(sortOrders: [
+      SortOrder('firstname'),
+      SortOrder('lastname'),
+      SortOrder('alias')
+    ]);
 
-    switch(filterOption){
-      case MemberFilterOption.ACTIVE: finder.filter = Filter.equals("active", true); break;
-      case MemberFilterOption.INACTIVE: finder.filter = Filter.equals("active", false); break;
-      default: break;
+    switch (filterOption) {
+      case MemberFilterOption.ACTIVE:
+        finder.filter = Filter.equals('active', true);
+        break;
+      case MemberFilterOption.INACTIVE:
+        finder.filter = Filter.equals('active', false);
+        break;
+      default:
+        break;
     }
 
-    final Iterable<Member> members = await _memberStore.find(_database,finder: finder).then((records){
-      return records.map((record)=> Member.of(record.key, record.value));
+    final Iterable<Member> members =
+        await _memberStore.find(_database, finder: finder).then((records) {
+      return records.map((record) => Member.of(record.key, record.value));
     });
 
     return members.toList();
@@ -112,38 +117,40 @@ class MemberDao implements IMemberDao {
       filter: Filter.byKey(member.uuid),
     );
 
-    await _memberStore.update(_database,member.toMap(),finder: finder);
+    await _memberStore.update(_database, member.toMap(), finder: finder);
   }
 
   @override
-  Future<bool> memberExists(String firstname, String lastname, String alias, [String? uuid]) async {
+  Future<bool> memberExists(String firstname, String lastname, String alias,
+      [String? uuid]) async {
     final List<Filter> filters = [
-      Filter.equals("firstname", firstname),
-      Filter.equals("lastname", lastname),
-      Filter.equals("alias", alias),
+      Filter.equals('firstname', firstname),
+      Filter.equals('lastname', lastname),
+      Filter.equals('alias', alias),
     ];
-    if(uuid != null && uuid.isNotEmpty){
+    if (uuid != null && uuid.isNotEmpty) {
       filters.add(Filter.notEquals(Field.key, uuid));
     }
 
     final finder = Finder(filter: Filter.and(filters));
 
-    return await _memberStore.findFirst(_database,finder: finder) != null;
+    return await _memberStore.findFirst(_database, finder: finder) != null;
   }
 
   @override
   Future<int> getAttendingCountForAttendee(String uuid) {
-    return _rideAttendeeStore.count(_database, filter: Filter.equals("attendee", uuid));
+    return _rideAttendeeStore.count(_database,
+        filter: Filter.equals('attendee', uuid));
   }
 
   @override
   Future<void> setMemberActive(String uuid, bool value) async {
     final record = _memberStore.record(uuid);
 
-    if(await record.exists(_database)){
+    if (await record.exists(_database)) {
       await record.update(_database, {
-        "active": value,
-        "lastUpdated": DateTime.now().toUtc().toStringWithoutMilliseconds()
+        'active': value,
+        'lastUpdated': DateTime.now().toUtc().toStringWithoutMilliseconds()
       });
     }
   }

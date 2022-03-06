83a6be4547b0ab81b45749736999f4d367234f26
Navaron Bracke <brackenavaron@gmail.com>
2022-03-06 17:02:11 +0100

    fix lints

--- lib/theme/appTheme.dart
+++ lib/theme/appTheme.dart
@@ -4,139 +4,176 @@ import 'package:flutter/services.dart';
 
 ///This class provides the application [ThemeData].
 abstract class ApplicationTheme {
-
   /// Primary / Secondary Theme colors.
   /// The primary color is defined as a [MaterialColor].
-  static final MaterialColor primaryColor = Colors.blue;
+  static const MaterialColor primaryColor = Colors.blue;
   static final Color secondaryColor = Colors.blue.shade300;
 
   //Ride Calendar Item Styling for an item that is now or in the future, which has no ride.
-  static final Color rideCalendarFutureDayNoRideFontColor = Colors.black;
+  static const Color rideCalendarFutureDayNoRideFontColor = Colors.black;
 
   // Ride Calendar Item font color, except for future days without rides.
-  static final Color rideCalendarDayFontColor = Colors.white;
+  static const Color rideCalendarDayFontColor = Colors.white;
 
-  static final Color rideCalendarFutureDayWithRideBackgroundColor = Colors.blue.shade700;
-  static final Color rideCalendarSelectedDayBackgroundColor = Colors.blue.shade200;
-  static final Color rideCalendarPastDayWithoutRideBackgroundColor = Colors.grey.shade400;
-  static final Color rideCalendarPastDayWithRideBackgroundColor = Colors.grey.shade700;
+  static final Color rideCalendarFutureDayWithRideBackgroundColor =
+      Colors.blue.shade700;
+  static final Color rideCalendarSelectedDayBackgroundColor =
+      Colors.blue.shade200;
+  static final Color rideCalendarPastDayWithoutRideBackgroundColor =
+      Colors.grey.shade400;
+  static final Color rideCalendarPastDayWithRideBackgroundColor =
+      Colors.grey.shade700;
 
   //Ride Calendar Header Font Color
-  static final Color rideCalendarHeaderColor = Colors.black;
+  static const Color rideCalendarHeaderColor = Colors.black;
 
   ///Colors for an arrow which dictates there is another choice before/ahead of the current item.
   ///Example: < "Choice" >
   ///
   ///The idle color is shown when the button is not pressed.
   ///The onPressed Color is shown otherwise.
-  static final Color choiceArrowIdleColor = Colors.black;
-  static final Color choiceArrowOnPressedColor = Colors.black45;
+  static const Color choiceArrowIdleColor = Colors.black;
+  static const Color choiceArrowOnPressedColor = Colors.black45;
 
   ///The profile image placeholder's icon color.
   static const Color profileImagePlaceholderIconColor = Colors.white;
+
   ///The profile image placeholder icon's background color.
-  static final Color profileImagePlaceholderIconBackgroundColor = Colors.blue.shade700;
+  static final Color profileImagePlaceholderIconBackgroundColor =
+      Colors.blue.shade700;
 
   static const TextStyle personInitialsTextStyle = TextStyle(
-      fontFamily: 'Roboto',
-      color: Colors.white,
+    fontFamily: 'Roboto',
+    color: Colors.white,
   );
 
   ///The background color for an unselected ride attendee.
-  static final Color rideAttendeeUnSelectedBackgroundColor = Colors.transparent;
+  static const Color rideAttendeeUnSelectedBackgroundColor = Colors.transparent;
+
   ///The background color for a selected ride attendee.
   static final Color rideAttendeeSelectedBackgroundColor = Colors.blue.shade700;
 
   //MemberList Item First Name Text Style
-  static final TextStyle memberListItemFirstNameTextStyle = TextStyle(fontSize: 18,fontWeight: FontWeight.w500);
+  static const TextStyle memberListItemFirstNameTextStyle =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w500);
 
   //MemberList Item Last Name Text Style
-  static final TextStyle memberListItemLastNameTextStyle = TextStyle(fontSize: 14);
+  static const TextStyle memberListItemLastNameTextStyle =
+      TextStyle(fontSize: 14);
 
   //Cupertino Form Input Error Text Style
-  static final TextStyle iosFormErrorStyle = TextStyle(fontSize: 14, color: CupertinoColors.destructiveRed);
+  static const TextStyle iosFormErrorStyle =
+      TextStyle(fontSize: 14, color: CupertinoColors.destructiveRed);
 
   // Cupertino Button TextStyle
-  static final TextStyle iosButtonTextStyle = TextStyle(color: primaryColor);
+  static const TextStyle iosButtonTextStyle = TextStyle(color: primaryColor);
 
-  static final TextStyle androidRideAttendeeListCounterTextStyle = TextStyle(fontSize: 14.0, color: Colors.white);
+  static const TextStyle androidRideAttendeeListCounterTextStyle =
+      TextStyle(fontSize: 14.0, color: Colors.white);
 
-  static final Color deviceIconColor = Colors.blue;
+  static const Color deviceIconColor = Colors.blue;
   static final Color deviceTypePickerDotColor = Colors.blue.shade100;
-  static final Color deviceTypePickerCurrentDotColor = Colors.blue;
+  static const Color deviceTypePickerCurrentDotColor = Colors.blue;
+
   ///The member devices list header + button color for android.
-  static final Color memberDevicesListHeaderAddDeviceButtonColor = Colors.blue;
+  static const Color memberDevicesListHeaderAddDeviceButtonColor = Colors.blue;
+
   ///The member devices list header + button colors for IOS.
-  static final Color memberDevicesListHeaderAddDeviceButtonIdleColor = Colors.blue;
-  static final Color memberDevicesListHeaderAddDeviceButtonPressedColor = Colors.blue.shade100;
-  static final TextStyle memberDevicesListHeaderTextStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
+  static const Color memberDevicesListHeaderAddDeviceButtonIdleColor =
+      Colors.blue;
+  static final Color memberDevicesListHeaderAddDeviceButtonPressedColor =
+      Colors.blue.shade100;
+  static const TextStyle memberDevicesListHeaderTextStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
 
   ///The member device item edit button color for android.
   static final Color memberDevicesListEditDeviceColor = Colors.blue.shade300;
+
   ///The member device item edit button colors for IOS.
-  static final Color memberDevicesListEditDeviceIdleColor = Colors.blue.shade300;
-  static final Color memberDevicesListEditDevicePressedColor = Colors.blue.shade100;
+  static final Color memberDevicesListEditDeviceIdleColor =
+      Colors.blue.shade300;
+  static final Color memberDevicesListEditDevicePressedColor =
+      Colors.blue.shade100;
 
   ///This color is used for Icons in lists that show some information when there is nothing to show.
-  static final Color listInformationalIconColor = primaryColor;
+  static const Color listInformationalIconColor = primaryColor;
 
-  static final TextStyle settingsOptionHeaderStyle = TextStyle(fontSize: 14);
+  static const TextStyle settingsOptionHeaderStyle = TextStyle(fontSize: 14);
   static final Color settingsScanSliderThumbColor = Colors.blue.shade300;
-  static final TextStyle settingsResetRideCalendarDescriptionTextStyle = TextStyle(fontSize: 12, fontStyle: FontStyle.italic);
+  static const TextStyle settingsResetRideCalendarDescriptionTextStyle =
+      TextStyle(fontSize: 12, fontStyle: FontStyle.italic);
 
-  static final Color androidRideAttendeeScanProcessCurrentStepColor = Colors.lightGreen.shade200;
-  static final Color androidRideAttendeeScanProcessOtherStepColor = Colors.white;
-  static final Color androidRideAttendeeScanProcessArrowColor = Colors.lightBlue.shade200;
-  static final Color iosRideAttendeeScanProcessCurrentStepColor = CupertinoColors.activeGreen;
-  static final Color iosRideAttendeeScanProcessOtherStepColor = CupertinoColors.inactiveGray;
-  static final Color iosRideAttendeeScanProcessArrowColor = CupertinoColors.activeBlue;
-  static final Color androidRideAttendeeScanProgressbarColor = Color.fromARGB(255,98,204,98);
-  static final Color androidRideAttendeeScanProgressbarBackgroundColor = Color.fromARGB(120,98,204,98);
-  
-  static final Color iosRideAttendeeScanProgressbarColor = CupertinoColors.activeGreen;
-  static final Color iosRideAttendeeScanProgressbarBackgroundColor = CupertinoColors.activeGreen.withOpacity(0.4);
+  static final Color androidRideAttendeeScanProcessCurrentStepColor =
+      Colors.lightGreen.shade200;
+  static const Color androidRideAttendeeScanProcessOtherStepColor =
+      Colors.white;
+  static final Color androidRideAttendeeScanProcessArrowColor =
+      Colors.lightBlue.shade200;
+  static const Color iosRideAttendeeScanProcessCurrentStepColor =
+      CupertinoColors.activeGreen;
+  static const Color iosRideAttendeeScanProcessOtherStepColor =
+      CupertinoColors.inactiveGray;
+  static const Color iosRideAttendeeScanProcessArrowColor =
+      CupertinoColors.activeBlue;
+  static const Color androidRideAttendeeScanProgressbarColor =
+      Color.fromARGB(255, 98, 204, 98);
+  static const Color androidRideAttendeeScanProgressbarBackgroundColor =
+      Color.fromARGB(120, 98, 204, 98);
 
-  static final Color rideAttendeeScanResultSingleOwnerColor = Colors.blue;
-  static final Color rideAttendeeScanResultMultipleOwnerColor = Colors.orange;
-  static final Color rideAttendeeScanResultOwnerChoiceRequiredBackgroundColor = Colors.red;
-  static final Color rideAttendeeScanResultOwnerChoiceRequiredFontColor = Colors.white;
-  static final Color androidManualSelectionSwitchActiveTrackColor = Colors.lightBlue.shade200;
+  static const Color iosRideAttendeeScanProgressbarColor =
+      CupertinoColors.activeGreen;
+  static final Color iosRideAttendeeScanProgressbarBackgroundColor =
+      CupertinoColors.activeGreen.withOpacity(0.4);
 
-  static final Color androidManualSelectionSaveButtonPrimaryColor = Color(0xFF1666a5);
+  static const Color rideAttendeeScanResultSingleOwnerColor = Colors.blue;
+  static const Color rideAttendeeScanResultMultipleOwnerColor = Colors.orange;
+  static const Color rideAttendeeScanResultOwnerChoiceRequiredBackgroundColor =
+      Colors.red;
+  static const Color rideAttendeeScanResultOwnerChoiceRequiredFontColor =
+      Colors.white;
+  static final Color androidManualSelectionSwitchActiveTrackColor =
+      Colors.lightBlue.shade200;
 
-  static final TextStyle rideAttendeeScanResultMultipleOwnersLabelStyle = TextStyle(
-      fontStyle: FontStyle.italic,
-      fontSize: 12,
-      color: rideAttendeeScanResultMultipleOwnerColor,
+  static const Color androidManualSelectionSaveButtonPrimaryColor =
+      Color(0xFF1666a5);
+
+  static const TextStyle rideAttendeeScanResultMultipleOwnersLabelStyle =
+      TextStyle(
+    fontStyle: FontStyle.italic,
+    fontSize: 12,
+    color: rideAttendeeScanResultMultipleOwnerColor,
   );
 
-  static final TextStyle rideAttendeeScanResultFirstNameTextStyle = TextStyle(fontWeight: FontWeight.w600, fontSize: 16.0);
-  static final TextStyle rideAttendeeMultipleOwnersListTooltipStyle = TextStyle(fontStyle: FontStyle.italic, fontSize: 12, color: Colors.grey);
+  static const TextStyle rideAttendeeScanResultFirstNameTextStyle =
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 16.0);
+  static const TextStyle rideAttendeeMultipleOwnersListTooltipStyle =
+      TextStyle(fontStyle: FontStyle.italic, fontSize: 12, color: Colors.grey);
 
-  static final Color deleteItemButtonTextColor = Colors.red;
+  static const Color deleteItemButtonTextColor = Colors.red;
 
   static final TextStyle importMembersHeaderExampleTextStyle = TextStyle(
-      fontSize: 12,fontStyle: FontStyle.italic, color: Colors.grey.shade600
-  );
+      fontSize: 12, fontStyle: FontStyle.italic, color: Colors.grey.shade600);
 
-  static final TextStyle importWarningTextStyle = TextStyle(color: Colors.red);
+  static const TextStyle importWarningTextStyle = TextStyle(color: Colors.red);
 
-  static final Color importMembersDoneIconColor = Colors.green;
+  static const Color importMembersDoneIconColor = Colors.green;
 
-  static final TextStyle appVersionTextStyle = TextStyle(fontStyle: FontStyle.italic, fontSize: 12);
+  static const TextStyle appVersionTextStyle =
+      TextStyle(fontStyle: FontStyle.italic, fontSize: 12);
 
-  static final Color rideListItemEvenMonthColor = Colors.blue;
+  static const Color rideListItemEvenMonthColor = Colors.blue;
 
   /// Provide the Android theme.
-  static ThemeData androidTheme(){
-     return ThemeData(
+  static ThemeData androidTheme() {
+    return ThemeData(
       splashColor: secondaryColor.withAlpha(150),
-      appBarTheme: AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.light),
+      appBarTheme:
+          const AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.light),
       textButtonTheme: TextButtonThemeData(
         style: TextButton.styleFrom(
           primary: primaryColor,
-          minimumSize: Size(88, 36),
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          minimumSize: const Size(88, 36),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(2.0)),
           ),
@@ -146,13 +183,13 @@ abstract class ApplicationTheme {
         style: ElevatedButton.styleFrom(
           onPrimary: Colors.white,
           primary: primaryColor,
-          minimumSize: Size(88, 36),
-          padding: EdgeInsets.symmetric(horizontal: 16),
+          minimumSize: const Size(88, 36),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(2)),
           ),
         ),
-      ), 
+      ),
       colorScheme: ColorScheme.fromSwatch(
         primarySwatch: primaryColor,
       ).copyWith(secondary: secondaryColor),
@@ -160,10 +197,8 @@ abstract class ApplicationTheme {
   }
 
   /// Provide the IOS theme.
-  static CupertinoThemeData iosTheme(){
+  static CupertinoThemeData iosTheme() {
     return CupertinoThemeData(
-      primaryColor: primaryColor,
-      primaryContrastingColor: secondaryColor
-    );
+        primaryColor: primaryColor, primaryContrastingColor: secondaryColor);
   }
 }
--- lib/widgets/common/memberNameAndAlias.dart
+++ lib/widgets/common/memberNameAndAlias.dart
@@ -3,42 +3,42 @@ import 'package:flutter/material.dart';
 /// This widget displays the first & last name of a member, combined with the alias in the middle.
 class MemberNameAndAlias extends StatelessWidget {
   MemberNameAndAlias({
+    Key? key,
     required this.firstNameStyle,
     required this.lastNameStyle,
     required this.firstName,
     required this.lastName,
     required this.alias,
     this.isTwoLine = true,
-  }): assert(firstName.isNotEmpty && lastName.isNotEmpty);
+  })  : assert(firstName.isNotEmpty && lastName.isNotEmpty),
+        super(key: key);
 
   final String firstName;
   final String lastName;
   final String alias;
   final TextStyle firstNameStyle;
   final TextStyle lastNameStyle;
+
   /// Whether to use the two line variant.
   final bool isTwoLine;
 
-  Widget _buildTwoLineVariant(){
-    final Widget firstNameAndAlias = alias.isEmpty ? Text(
-      firstName,
-      style: firstNameStyle,
-      overflow: TextOverflow.ellipsis,
-    ): Text.rich(
-      TextSpan(
-          text: firstName,
-          style: firstNameStyle,
-          children: [
-            TextSpan(
-              text: " '$alias'",
-              style: firstNameStyle.copyWith(fontStyle: FontStyle.italic),
-            ),
-          ]
-      ),
-      softWrap: true,
-      overflow: TextOverflow.ellipsis,
-    );
-
+  Widget _buildTwoLineVariant() {
+    final Widget firstNameAndAlias = alias.isEmpty
+        ? Text(
+            firstName,
+            style: firstNameStyle,
+            overflow: TextOverflow.ellipsis,
+          )
+        : Text.rich(
+            TextSpan(text: firstName, style: firstNameStyle, children: [
+              TextSpan(
+                text: " '$alias' ",
+                style: firstNameStyle.copyWith(fontStyle: FontStyle.italic),
+              ),
+            ]),
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          );
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -52,35 +52,32 @@ class MemberNameAndAlias extends StatelessWidget {
     );
   }
 
-  Widget _buildOneLineVariant(){
-    return alias.isEmpty ? Text(
-        "$firstName $lastName",
-        softWrap: true,
-        style: lastNameStyle,
-        overflow: TextOverflow.ellipsis
-    ): Text.rich(
-      TextSpan(
-          text: firstName,
-          style: lastNameStyle,
-          children: <InlineSpan>[
+  Widget _buildOneLineVariant() {
+    return alias.isEmpty
+        ? Text('$firstName $lastName',
+            softWrap: true,
+            style: lastNameStyle,
+            overflow: TextOverflow.ellipsis)
+        : Text.rich(
             TextSpan(
-              text: " '$alias' ",
-              style: lastNameStyle.copyWith(
-                  fontStyle: FontStyle.italic
-              ),
-            ),
-            TextSpan(
-              text: lastName,
-              style: lastNameStyle,
-            ),
-          ]
-      ),
-      softWrap: true,
-      overflow: TextOverflow.ellipsis,
-    );
+                text: firstName,
+                style: lastNameStyle,
+                children: <InlineSpan>[
+                  TextSpan(
+                    text: " '$alias' ",
+                    style: lastNameStyle.copyWith(fontStyle: FontStyle.italic),
+                  ),
+                  TextSpan(
+                    text: lastName,
+                    style: lastNameStyle,
+                  ),
+                ]),
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          );
   }
 
   @override
-  Widget build(BuildContext context)
-    => isTwoLine? _buildTwoLineVariant(): _buildOneLineVariant();
+  Widget build(BuildContext context) =>
+      isTwoLine ? _buildTwoLineVariant() : _buildOneLineVariant();
 }

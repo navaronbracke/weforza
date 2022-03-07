e89dab5be165fc5d971049237f1abb8ed5046761
Navaron Bracke <brackenavaron@gmail.com>
2022-03-07 08:26:40 +0100

    rename add ride page & submit; fix an import in member_dao

--- lib/widgets/pages/rideList/rideListPage.dart
+++ lib/widgets/pages/rideList/rideListPage.dart
@@ -6,7 +6,7 @@ import 'package:weforza/injection/injectionContainer.dart';
 import 'package:weforza/model/ride.dart';
 import 'package:weforza/repository/ride_repository.dart';
 import 'package:weforza/widgets/common/generic_error.dart';
-import 'package:weforza/widgets/pages/addRide/addRidePage.dart';
+import 'package:weforza/widgets/pages/add_ride/add_ride_page.dart';
 import 'package:weforza/widgets/pages/exportRides/exportRidesPage.dart';
 import 'package:weforza/widgets/pages/rideDetails/rideDetailsPage.dart';
 import 'package:weforza/widgets/pages/rideList/rideListEmpty.dart';

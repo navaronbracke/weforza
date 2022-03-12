b1bae99dac48e0262307ce7bf0834ddd4876ce36
Navaron Bracke <brackenavaron@gmail.com>
2022-03-12 16:45:40 +0100

    implement settings repository provider

---
+++ lib/riverpod/repository/settings_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weforza/repository/settings_repository.dart';
+import 'package:weforza/riverpod/database/database_dao_provider.dart';
+
+/// This provider provides the [SettingsRepository].
+final settingsRepositoryProvider = Provider<SettingsRepository>((ref) {
+  return SettingsRepository(ref.read(settingsDaoProvider));
+});

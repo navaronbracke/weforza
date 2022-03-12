da97aad3e62847a794401e0d5bd15ad1b2a401b0
Navaron Bracke <brackenavaron@gmail.com>
2022-03-12 16:00:31 +0100

    move the stores into providers

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -8,9 +8,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 
 import 'package:weforza/database/database_factory.dart';
-import 'package:weforza/model/member.dart';
-import 'package:weforza/model/ride.dart';
-import 'package:weforza/model/ride_attendee.dart';
 
 ///This class wraps a Sembast [Database] and datastore instances along with initializer and close methods.
 class ApplicationDatabase {
@@ -18,21 +15,6 @@ class ApplicationDatabase {
 
   //The database filename.
   final String databaseName;
-
-  ///The data store for [Member].
-  final memberStore = stringMapStoreFactory.store('member');
-
-  ///The data store for [Ride].
-  final rideStore = stringMapStoreFactory.store('ride');
-
-  ///The data store for [RideAttendee].
-  final rideAttendeeStore = stringMapStoreFactory.store('rideAttendee');
-
-  ///The data store for the member devices.
-  final deviceStore = stringMapStoreFactory.store('device');
-
-  ///The data store for general application settings.
-  final settingsStore = stringMapStoreFactory.store('settings');
 
   // The internal instance of the database.
   // We can assume that it is created before it is used.

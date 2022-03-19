392fcec0146ff3a3ef08bc6c8790a030bc88b15e
Navaron Bracke <brackenavaron@gmail.com>
2022-03-19 19:27:35 +0100

    remove the app widget from main.dart; preload the settings; preload settings deps

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:file/local.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:weforza/database/database.dart';
 import 'package:weforza/database/database_factory.dart';
-import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/database/database_tables.dart';
+import 'package:weforza/database/settings_dao.dart';
+import 'package:weforza/repository/settings_repository.dart';
+import 'package:weforza/riverpod/database/database_dao_provider.dart';
 import 'package:weforza/riverpod/database/database_provider.dart';
-import 'package:weforza/theme/app_theme.dart';
-import 'package:weforza/widgets/pages/home_page.dart';
-import 'package:weforza/widgets/platform/platform_aware_widget.dart';
+import 'package:weforza/riverpod/repository/settings_repository_provider.dart';
+import 'package:weforza/riverpod/settings_provider.dart';
+import 'package:weforza/widgets/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,80 +25,35 @@ void main() async {
     ),
   );
 
+  final databaseTables = DatabaseTables();
+
+  final settingsDao = SettingsDao(
+    database.getDatabase(),
+    databaseTables.settings,
+    databaseTables.ride,
+  );
+
+  final settingsRepository = SettingsRepository(settingsDao);
+
   runApp(
     ProviderScope(
       overrides: [
         // Inject the database after it is ready.
         databaseProvider.overrideWithValue(database),
+        databaseTableProvider.overrideWithValue(databaseTables),
+        // Inject the preloaded settings.
+        settingsProvider.overrideWithValue(
+          SettingsNotifier(
+            currentSettings: await settingsRepository.loadApplicationSettings(),
+            settingsRepository: settingsRepository,
+          ),
+        ),
+        // Inject the preloaded settings repository.
+        settingsRepositoryProvider.overrideWithValue(settingsRepository),
+        // Inject the preloaded settings dao.
+        settingsDaoProvider.overrideWithValue(settingsDao),
       ],
       child: const WeForzaApp(),
     ),
   );
-}
-
-/// This class represents the application.
-class WeForzaApp extends ConsumerStatefulWidget {
-  const WeForzaApp({Key? key}) : super(key: key);
-
-  @override
-  _WeForzaAppState createState() => _WeForzaAppState();
-}
-
-class _WeForzaAppState extends ConsumerState<WeForzaApp> {
-  static const _appName = 'WeForza';
-
-  @override
-  Widget build(BuildContext context) {
-    // Only portrait is supported at the moment.
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-
-    return GestureDetector(
-      child: PlatformAwareWidget(
-        android: () => _buildAndroidWidget(),
-        ios: () => _buildIosWidget(),
-      ),
-      onTap: () {
-        // Enable tap to dismiss the keyboard.
-        FocusScope.of(context).unfocus();
-      },
-    );
-  }
-
-  Widget _buildAndroidWidget() {
-    return MaterialApp(
-      title: _appName,
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      theme: ApplicationTheme.androidTheme(),
-      home: const HomePage(),
-    );
-  }
-
-  Widget _buildIosWidget() {
-    return CupertinoApp(
-      title: _appName,
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      theme: ApplicationTheme.iosTheme(),
-      home: const HomePage(),
-    );
-  }
-
-  @override
-  void dispose() {
-    ref.read(databaseProvider).dispose();
-    super.dispose();
-  }
 }

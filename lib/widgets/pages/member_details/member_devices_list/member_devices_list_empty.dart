28b9f32caab2d68dcb344c96b1dc6d3f228d696d
Navaron Bracke <brackenavaron@gmail.com>
2022-04-02 17:08:08 +0200

    refactor member devices list empty

--- lib/widgets/pages/member_details/member_devices_list/member_devices_list_empty.dart
+++ lib/widgets/pages/member_details/member_devices_list/member_devices_list_empty.dart
@@ -2,55 +2,67 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/theme/app_theme.dart';
+import 'package:weforza/widgets/pages/add_device/add_device_page.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
-///This widget is shown as replacement of a [Member]'s devices, if it has none.
+/// This widget represents the empty member devices list.
 class MemberDevicesListEmpty extends StatelessWidget {
-  const MemberDevicesListEmpty({
-    Key? key,
-    required this.onAddDevicePageButtonPressed,
-  }) : super(key: key);
+  const MemberDevicesListEmpty({Key? key}) : super(key: key);
 
-  final void Function() onAddDevicePageButtonPressed;
+  void onAddDevicePressed(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => const AddDevicePage()),
+    );
+  }
 
   @override
-  Widget build(BuildContext context) => Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            PlatformAwareWidget(
-              android: () => Icon(
-                Icons.devices_other,
-                color: ApplicationTheme.listInformationalIconColor,
-                size: MediaQuery.of(context).size.shortestSide * .1,
-              ),
-              ios: () => Icon(
-                Icons.devices_other,
-                color: ApplicationTheme.listInformationalIconColor,
-                size: MediaQuery.of(context).size.shortestSide * .1,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 8, 16, 32),
-              child: Text(
-                S.of(context).MemberDetailsNoDevices,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            PlatformAwareWidget(
-              android: () => ElevatedButton(
-                onPressed: onAddDevicePageButtonPressed,
-                child: Text(S.of(context).AddDeviceTitle),
-              ),
-              ios: () => CupertinoButton.filled(
-                onPressed: onAddDevicePageButtonPressed,
-                child: Text(
-                  S.of(context).AddDeviceTitle,
-                  style: const TextStyle(color: Colors.white),
+  Widget build(BuildContext context) {
+    final translator = S.of(context);
+
+    return Center(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final size = constraints.biggest;
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              PlatformAwareWidget(
+                android: () => Icon(
+                  Icons.devices_other,
+                  color: ApplicationTheme.listInformationalIconColor,
+                  size: size.shortestSide * .1,
+                ),
+                ios: () => Icon(
+                  Icons.devices_other,
+                  color: ApplicationTheme.listInformationalIconColor,
+                  size: size.shortestSide * .1,
                 ),
               ),
-            )
-          ],
-        ),
-      );
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 32),
+                child: Text(
+                  translator.MemberDetailsNoDevices,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              PlatformAwareWidget(
+                android: () => ElevatedButton(
+                  onPressed: () => onAddDevicePressed(context),
+                  child: Text(translator.AddDeviceTitle),
+                ),
+                ios: () => CupertinoButton.filled(
+                  onPressed: () => onAddDevicePressed(context),
+                  child: Text(
+                    translator.AddDeviceTitle,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      ),
+    );
+  }
 }

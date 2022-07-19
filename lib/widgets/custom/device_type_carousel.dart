cacd44225374e33f15b9ec76f1b3c5baefee602c
Navaron Bracke <brackenavaron@gmail.com>
2022-07-19 15:48:12 +0200

    clean up the device type carousel

--- lib/widgets/custom/device_type_carousel.dart
+++ lib/widgets/custom/device_type_carousel.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
-import 'package:weforza/model/device.dart';
+import 'package:weforza/model/device_type.dart';
 import 'package:weforza/theme/app_theme.dart';
 
+/// This widget represents a carousel for selecting a device type.
 class DeviceTypeCarousel extends StatelessWidget {
   const DeviceTypeCarousel({
     Key? key,
@@ -11,63 +12,18 @@ class DeviceTypeCarousel extends StatelessWidget {
     required this.currentPageStream,
   }) : super(key: key);
 
+  /// The controller that manages the currently selected page.
   final PageController controller;
-  final void Function(int page) onPageChanged;
+
+  /// The Stream that provides the index of the current page.
+  /// The [PageController.page] attribute is a double,
+  /// thus the controller cannot provide the page index directly.
   final Stream<int> currentPageStream;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Flexible(
-          flex: 9,
-          child: PageView.builder(
-            itemCount: DeviceType.values.length,
-            itemBuilder: _buildIcon,
-            onPageChanged: onPageChanged,
-            controller: controller,
-          ),
-        ),
-        Flexible(
-          flex: 2,
-          child: Center(
-            child: StreamBuilder<int>(
-              stream: currentPageStream,
-              builder: (context, snapshot) {
-                final children = <Widget>[];
-                for (int i = 0; i < DeviceType.values.length; i++) {
-                  children.add(_buildPageDot(i == snapshot.data));
-                }
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: children,
-                );
-              },
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+  /// The function that handles page changes.
+  final void Function(int page) onPageChanged;
 
-  Widget _buildPageDot(bool isCurrentPage) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: Container(
-        width: 10,
-        height: 10,
-        decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: isCurrentPage
-                ? ApplicationTheme.deviceTypePickerCurrentDotColor
-                : ApplicationTheme.deviceTypePickerDotColor),
-      ),
-    );
-  }
-
-  Widget _buildIcon(BuildContext context, int index) {
-    final deviceType = DeviceType.values[index];
-
+  Widget _buildIcon(DeviceType deviceType, S translator) {
     return Column(
       children: <Widget>[
         Expanded(
@@ -84,11 +40,68 @@ class DeviceTypeCarousel extends StatelessWidget {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.symmetric(vertical: 8),
           child: Center(
             child: Text(
-              deviceType.getLabel(S.of(context)),
+              deviceType.getLabel(translator),
               style: const TextStyle(fontSize: 14.0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPageDot(bool isCurrentPage) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: Container(
+        width: 12,
+        height: 12,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isCurrentPage
+              ? ApplicationTheme.deviceTypePickerCurrentDotColor
+              : ApplicationTheme.deviceTypePickerDotColor,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final translator = S.of(context);
+
+    return Column(
+      children: <Widget>[
+        Flexible(
+          flex: 9,
+          child: PageView.builder(
+            itemCount: DeviceType.values.length,
+            itemBuilder: (_, index) {
+              return _buildIcon(DeviceType.values[index], translator);
+            },
+            onPageChanged: onPageChanged,
+            controller: controller,
+          ),
+        ),
+        Flexible(
+          flex: 2,
+          child: Center(
+            child: StreamBuilder<int>(
+              stream: currentPageStream,
+              builder: (context, snapshot) {
+                final children = <Widget>[];
+
+                for (int i = 0; i < DeviceType.values.length; i++) {
+                  children.add(_buildPageDot(i == snapshot.data));
+                }
+
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                );
+              },
             ),
           ),
         ),

bc18df9bf3e110946b7c24a7a00954db710bb71f
Navaron Bracke <brackenavaron@gmail.com>
2022-08-23 21:29:53 +0200

    convert member list provider into a future provider; fix another ref

--- lib/widgets/pages/member_list/member_list_title.dart
+++ lib/widgets/pages/member_list/member_list_title.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/generated/l10n.dart';
-import 'package:weforza/model/member.dart';
 import 'package:weforza/riverpod/member/member_list_provider.dart';
 
 /// This widget represents the title in the member list page.
@@ -10,23 +9,13 @@ class MemberListTitle extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final future = ref.watch(memberListProvider);
+    final memberList = ref.watch(memberListProvider);
+    final translator = S.of(context);
 
-    return FutureBuilder<List<Member>>(
-      future: future,
-      builder: (context, snapshot) {
-        final translator = S.of(context);
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return Text(translator.Riders);
-          }
-
-          return Text(translator.RidersListTitle(snapshot.data?.length ?? 0));
-        }
-
-        return Text(translator.Riders);
-      },
+    return memberList.when(
+      data: (items) => Text(translator.RidersListTitle(items.length)),
+      error: (error, stackTrace) => Text(translator.Riders),
+      loading: () => Text(translator.Riders),
     );
   }
 }

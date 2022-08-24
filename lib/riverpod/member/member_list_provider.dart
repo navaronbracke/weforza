7b066e701e89b4cb78db666c2299fa33b430c076
Navaron Bracke <brackenavaron@gmail.com>
2022-08-24 17:51:29 +0200

    use select async in member list provider

--- lib/riverpod/member/member_list_provider.dart
+++ lib/riverpod/member/member_list_provider.dart
@@ -5,12 +5,12 @@ import 'package:weforza/riverpod/repository/member_repository_provider.dart';
 import 'package:weforza/riverpod/settings_provider.dart';
 
 /// This provider provides the list of members.
-final memberListProvider = FutureProvider<List<Member>>((ref) {
+final memberListProvider = FutureProvider<List<Member>>((ref) async {
   final repository = ref.read(memberRepositoryProvider);
 
   // Watch the member list filter for changes.
-  final filter = ref.watch(
-    settingsProvider.select((settings) => settings.memberListFilter),
+  final filter = await ref.watch(
+    settingsProvider.selectAsync((settings) => settings.memberListFilter),
   );
 
   return repository.getMembers(filter);

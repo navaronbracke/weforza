41dc5192e744545eff8fb97fb74a509961cd140f
Navaron Bracke <brackenavaron@gmail.com>
2022-08-24 19:08:37 +0200

    preload the package info; put package info in provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:file/local.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:weforza/database/database.dart';
 import 'package:weforza/database/database_factory.dart';
@@ -8,6 +9,7 @@ import 'package:weforza/database/database_tables.dart';
 import 'package:weforza/database/settings_dao.dart';
 import 'package:weforza/repository/settings_repository.dart';
 import 'package:weforza/riverpod/database/database_provider.dart';
+import 'package:weforza/riverpod/package_info_provider.dart';
 import 'package:weforza/riverpod/settings_provider.dart';
 import 'package:weforza/widgets/app.dart';
 
@@ -32,11 +34,16 @@ void main() async {
   );
   final settings = await settingsRepository.loadApplicationSettings();
 
+  // Preload the package info.
+  final packageInfo = await PackageInfo.fromPlatform();
+
   runApp(
     ProviderScope(
       overrides: [
         // Inject the database after it is ready.
         databaseProvider.overrideWithValue(database),
+        // Inject the preloaded package info.
+        packageInfoProvider.overrideWithValue(packageInfo),
         // Inject the preloaded settings.
         settingsProvider.overrideWithValue(StateController(settings)),
       ],

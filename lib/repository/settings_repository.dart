b002e06bbd31bb3ee1036532e7d8efa3a561ad59
Navaron Bracke <brackenavaron@gmail.com>
2022-08-24 17:24:41 +0200

    inject rides count

--- lib/repository/settings_repository.dart
+++ lib/repository/settings_repository.dart
@@ -1,17 +1,13 @@
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:weforza/database/ride_dao.dart';
 import 'package:weforza/database/settings_dao.dart';
 import 'package:weforza/model/settings.dart';
 
 class SettingsRepository {
-  SettingsRepository(this._rideDao, this._settingsDao);
-
-  final IRideDao _rideDao;
+  SettingsRepository(this._settingsDao);
 
   final ISettingsDao _settingsDao;
 
-  Future<Settings> loadApplicationSettings() async {
-    final ridesCount = await _rideDao.getRidesCount();
+  Future<Settings> loadApplicationSettings(int ridesCount) async {
     final packageInfo = await PackageInfo.fromPlatform();
 
     return _settingsDao.readApplicationSettings(

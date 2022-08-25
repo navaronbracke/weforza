d6bc36d515bb04f88a3d14f487bef9046bbae2b7
Navaron Bracke <brackenavaron@gmail.com>
2022-08-25 22:21:42 +0200

    fix deprecated hashValues() in bluetooth periperal; format

--- lib/bluetooth/bluetooth_peripheral.dart
+++ lib/bluetooth/bluetooth_peripheral.dart
@@ -1,20 +1,20 @@
-
-import 'dart:ui';
-
-///This class wraps a Bluetooth device's name and id.
+/// This class wraps a Bluetooth device's name and id.
 class BluetoothPeripheral {
   BluetoothPeripheral({
     required this.id,
     required this.deviceName,
-  }): assert(id.isNotEmpty);
+  }) : assert(id.isNotEmpty);
 
   final String id;
   final String deviceName;
 
   @override
-  bool operator ==(Object other) => other is BluetoothPeripheral
-      && id == other.id && deviceName == other.deviceName;
+  bool operator ==(Object other) {
+    return other is BluetoothPeripheral &&
+        id == other.id &&
+        deviceName == other.deviceName;
+  }
 
   @override
-  int get hashCode => hashValues(id, deviceName);
+  int get hashCode => Object.hash(id, deviceName);
 }

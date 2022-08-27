76081320dca08b2e3b0e35b642233b42f9650b67
Navaron Bracke <brackenavaron@gmail.com>
2022-08-27 19:49:41 +0200

    reimplement bluetooth disabled error widget

---
+++ lib/widgets/pages/ride_attendee_scanning_page/bluetooth_disabled_error.dart
@@ -0,0 +1,66 @@
+import 'package:app_settings/app_settings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/theme/app_theme.dart';
+import 'package:weforza/widgets/platform/platform_aware_widget.dart';
+
+class BluetoothDisabledError extends StatelessWidget {
+  const BluetoothDisabledError({super.key, required this.onRetryScan});
+
+  final void Function() onRetryScan;
+
+  @override
+  Widget build(BuildContext context) {
+    final translator = S.of(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          Icons.bluetooth_disabled,
+          color: ApplicationTheme.listInformationalIconColor,
+          size: MediaQuery.of(context).size.shortestSide * .1,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8, bottom: 20),
+          child: Text(translator.ScanAbortedBluetoothDisabled),
+        ),
+        Wrap(
+          spacing: 16,
+          runSpacing: 8,
+          alignment: WrapAlignment.center,
+          direction: Axis.horizontal,
+          children: [
+            PlatformAwareWidget(
+              android: () => ElevatedButton(
+                onPressed: () => AppSettings.openBluetoothSettings(),
+                child: Text(translator.GoToSettings),
+              ),
+              ios: () => CupertinoButton.filled(
+                onPressed: () => AppSettings.openBluetoothSettings(),
+                child: Text(
+                  translator.GoToSettings,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            PlatformAwareWidget(
+              android: () => TextButton(
+                onPressed: onRetryScan,
+                child: Text(translator.RetryScan),
+              ),
+              ios: () => CupertinoButton(
+                onPressed: onRetryScan,
+                child: Text(
+                  translator.RetryScan,
+                  style: const TextStyle(color: ApplicationTheme.primaryColor),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

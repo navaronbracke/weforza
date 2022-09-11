ce234350ea5006bd334488ed144ef7e0e5d77c38
Navaron Bracke <brackenavaron@gmail.com>
2022-09-11 16:33:33 +0200

    use an adaptive profile image for MemberProfileImage

--- lib/widgets/pages/member_details/member_profile_image.dart
+++ lib/widgets/pages/member_details/member_profile_image.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/riverpod/member/selected_member_provider.dart';
 import 'package:weforza/widgets/custom/profile_image/profile_image.dart';
-import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
 class MemberProfileImage extends ConsumerWidget {
   const MemberProfileImage({super.key});
@@ -14,19 +13,10 @@ class MemberProfileImage extends ConsumerWidget {
       selectedMemberProvider.select((selectedMember) => selectedMember!),
     );
 
-    return PlatformAwareWidget(
-      android: () => ProfileImage(
-        icon: Icons.person,
-        image: selectedMember.profileImage,
-        personInitials: selectedMember.value.initials,
-        size: 72,
-      ),
-      ios: () => ProfileImage(
-        icon: CupertinoIcons.person_fill,
-        image: selectedMember.profileImage,
-        personInitials: selectedMember.value.initials,
-        size: 72,
-      ),
+    return AdaptiveProfileImage(
+      image: selectedMember.profileImage,
+      personInitials: selectedMember.value.initials,
+      size: 72,
     );
   }
 }

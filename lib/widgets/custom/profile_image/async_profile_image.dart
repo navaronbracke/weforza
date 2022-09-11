50a235f4fc65c3670766144b491c0abe0e516c7b
Navaron Bracke <brackenavaron@gmail.com>
2022-09-11 15:16:39 +0200

    clean up async profile image widget; defer to profile image widget even when it is loading

--- lib/widgets/custom/profile_image/async_profile_image.dart
+++ lib/widgets/custom/profile_image/async_profile_image.dart
@@ -3,48 +3,49 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:weforza/theme/app_theme.dart';
 import 'package:weforza/widgets/custom/profile_image/profile_image.dart';
-import 'package:weforza/widgets/platform/platform_aware_loading_indicator.dart';
 
-/// This widget asynchronously loads a given profile image.
-/// When the image could not be loaded, the given initials are displayed.
+/// This widget asynchronously loads and displays a given profile image.
+/// When the image could not be loaded, the [personInitials] are displayed.
+/// If the [personInitials] have not been provided,
+/// the [icon] is displayed instead.
 class AsyncProfileImage extends StatelessWidget {
   const AsyncProfileImage({
     super.key,
-    this.personInitials,
-    required this.icon,
     required this.future,
+    required this.icon,
+    this.personInitials,
     this.size = 40,
   });
 
+  /// The future that represents the loading of the image.
   final Future<File?> future;
-  final String? personInitials;
-  final double size;
+
+  /// The icon that is used as fallback if [personInitials] is null or empty.
   final IconData icon;
+
+  /// The person's initials that are used as placeholder
+  /// when the image is unavailable.
+  final String? personInitials;
+
+  /// The size of this widget.
+  ///
+  /// Defaults to 40.
+  final double size;
 
   @override
   Widget build(BuildContext context) {
-    const iconColor = ApplicationTheme.profileImagePlaceholderIconColor;
     final bgColor = ApplicationTheme.profileImagePlaceholderIconBackgroundColor;
 
     return FutureBuilder<File?>(
       future: future,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return ProfileImage(
-            icon: icon,
-            image: snapshot.data,
-            size: size,
-            personInitials: personInitials,
-            iconColor: iconColor,
-            backgroundColor: bgColor,
-          );
-        }
-
-        return SizedBox.square(
-          dimension: size,
-          child: const Center(child: PlatformAwareLoadingIndicator()),
-        );
-      },
+      builder: (context, snapshot) => ProfileImage(
+        backgroundColor: bgColor,
+        icon: icon,
+        iconColor: ApplicationTheme.profileImagePlaceholderIconColor,
+        image: snapshot.data,
+        personInitials: personInitials,
+        size: size,
+      ),
     );
   }
 }

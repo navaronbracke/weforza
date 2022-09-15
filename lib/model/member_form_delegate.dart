e4c3e1717648483bfa56774e462fd52aafe9b8ae
Navaron Bracke <brackenavaron@gmail.com>
2022-09-15 18:33:01 +0200

    revert back to returning futures

--- lib/model/member_form_delegate.dart
+++ lib/model/member_form_delegate.dart
@@ -20,15 +20,10 @@ class MemberFormDelegate {
 
   final MemberRepository repository;
 
-  /// The future that represents the submit.
-  Future<void>? _future;
-
   final _uuidGenerator = const Uuid();
 
-  /// Get the submit future.
-  Future<void>? get future => _future;
-
-  Future<void> _addMember(MemberPayload model) async {
+  /// Add a new member.
+  Future<void> addMember(MemberPayload model) async {
     final exists = await repository.memberExists(
       model.firstName,
       model.lastName,
@@ -58,7 +53,8 @@ class MemberFormDelegate {
     ref.refresh(memberListProvider);
   }
 
-  Future<void> _editMember(MemberPayload model) async {
+  /// Edit an existing member.
+  Future<void> editMember(MemberPayload model) async {
     final uuid = model.uuid;
 
     if (uuid == null) {
@@ -99,20 +95,5 @@ class MemberFormDelegate {
 
     // An item in the list was updated.
     ref.refresh(memberListProvider);
-  }
-
-  /// Add a new member.
-  void addMember(MemberPayload model) {
-    _future = _addMember(model);
-  }
-
-  /// Edit an existing member.
-  void editMember(MemberPayload model) {
-    _future = _editMember(model);
-  }
-
-  /// Reset the submit computation.
-  void reset() {
-    _future = null;
   }
 }

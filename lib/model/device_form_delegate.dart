9e3af4932c5933da107d720592ad16f270d4f97e
Navaron Bracke <brackenavaron@gmail.com>
2022-09-15 18:14:40 +0200

    remove the submit controller from the device form delegate

--- lib/model/device_form_delegate.dart
+++ lib/model/device_form_delegate.dart
@@ -1,4 +1,3 @@
-import 'package:rxdart/rxdart.dart';
 import 'package:weforza/model/device_payload.dart';
 import 'package:weforza/riverpod/member/selected_member_devices_provider.dart';
 
@@ -9,52 +8,24 @@ class DeviceFormDelegate {
   /// The notifier that handles adding and editing devices.
   final SelectedMemberDevicesNotifier notifier;
 
-  /// This controller manages a submit flag.
-  /// If the current value is true, the delegate is submitting the form.
-  /// If the current value is false, the delegate is idle.
-  /// If the current value is an error, the submit failed.
-  ///
-  /// Once the delegate enters the submitting state,
-  /// it does not exit said state upon successful completion.
-  /// It is up to the caller of [addDevice] or [editDevice]
-  /// to handle the result of the future.
-  final _submitController = BehaviorSubject.seeded(false);
+  /// The future that represents the submit.
+  Future<void>? _future;
 
-  bool get isSubmitting => _submitController.value;
+  /// Get the submit future.
+  Future<void>? get future => _future;
 
-  Stream<bool> get isSubmittingStream => _submitController;
-
-  Future<void> addDevice(DevicePayload model) async {
-    _submitController.add(true);
-
-    try {
-      await notifier.addDevice(model);
-    } catch (error) {
-      _submitController.addError(error);
-
-      rethrow;
-    }
+  /// Add a new device.
+  void addDevice(DevicePayload model) {
+    _future = notifier.addDevice(model);
   }
 
-  Future<void> editDevice(DevicePayload model) async {
-    _submitController.add(true);
-
-    try {
-      await notifier.editDevice(model);
-    } catch (error) {
-      _submitController.addError(error);
-
-      rethrow;
-    }
+  /// Edit an existing device.
+  void editDevice(DevicePayload model) {
+    _future = notifier.editDevice(model);
   }
 
-  void resetSubmit(String? _) {
-    if (!_submitController.isClosed && _submitController.hasError) {
-      _submitController.add(false);
-    }
-  }
-
-  void dispose() {
-    _submitController.close();
+  /// Reset the submit computation.
+  void reset() {
+    _future = null;
   }
 }

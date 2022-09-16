c1659773bb938df5e40d8e93d4b01e5961329bc4
Navaron Bracke <brackenavaron@gmail.com>
2022-09-16 12:46:24 +0200

    add an artificial delay to member & device forms

--- lib/model/device_form_delegate.dart
+++ lib/model/device_form_delegate.dart
@@ -1,16 +1,35 @@
+import 'package:weforza/extensions/artificial_delay_mixin.dart';
 import 'package:weforza/model/device_payload.dart';
 import 'package:weforza/riverpod/member/selected_member_devices_provider.dart';
 
 /// This class represents the delegate for the add / edit device form.
-class DeviceFormDelegate {
+class DeviceFormDelegate with ArtificialDelay {
   DeviceFormDelegate({required this.notifier});
 
   /// The notifier that handles adding and editing devices.
   final SelectedMemberDevicesNotifier notifier;
 
   /// Add a new device.
-  Future<void> addDevice(DevicePayload model) => notifier.addDevice(model);
+  Future<void> addDevice(DevicePayload model) async {
+    // Allow the event loop some time to add an error handler.
+    // When the database has almost no data, this future completes even before
+    // the event loop could schedule a new frame,
+    // which results in an unhandled exception in the FutureBuilder.
+    //
+    // This also gives the loading indicator some time to appear properly.
+    await waitForDelay();
+    await notifier.addDevice(model);
+  }
 
   /// Edit an existing device.
-  Future<void> editDevice(DevicePayload model) => notifier.editDevice(model);
+  Future<void> editDevice(DevicePayload model) async {
+    // Allow the event loop some time to add an error handler.
+    // When the database has almost no data, this future completes even before
+    // the event loop could schedule a new frame,
+    // which results in an unhandled exception in the FutureBuilder.
+    //
+    // This also gives the loading indicator some time to appear properly.
+    await waitForDelay();
+    await notifier.editDevice(model);
+  }
 }

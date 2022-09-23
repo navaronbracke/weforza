0599f3d0db32b35accbe90640ebb88d514a0658d
Navaron Bracke <brackenavaron@gmail.com>
2022-09-23 19:26:19 +0200

    refactor the scan duration option

--- lib/widgets/pages/settings/scan_duration_option.dart
+++ lib/widgets/pages/settings/scan_duration_option.dart
@@ -4,66 +4,100 @@ import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 import 'package:weforza/widgets/theme.dart';
 
+/// This widget represents the scan duration slider.
+///
+/// It provides an interactive slider to change the scan duration,
+/// and a centered label under the slider
+/// to indicate the current value in seconds.
 class ScanDurationOption extends StatelessWidget {
   const ScanDurationOption({
     super.key,
-    required this.initialScanDuration,
+    required this.initialValue,
     required this.onChanged,
     required this.stream,
   });
 
-  final double initialScanDuration;
+  /// The initially selected scan duration, in seconds.
+  final double initialValue;
 
+  /// The maximum scan duration of 60 seconds.
   final double maxScanDuration = 60;
 
+  /// The minimum scan duration of 10 seconds.
   final double minScanDuration = 10;
 
+  /// The handler function that is called when the value changes.
   final void Function(double value) onChanged;
 
+  /// The stream that provides updates about the current value.
   final Stream<double> stream;
+
+  Widget _buildCurrentDurationLabel() {
+    return StreamBuilder<double>(
+      initialData: initialValue,
+      stream: stream,
+      builder: (context, snapshot) => Text('${snapshot.data!.floor()}s'),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<double>(
-      initialData: initialScanDuration,
-      stream: stream,
-      builder: (context, snapshot) {
-        final currentScanDuration = snapshot.data!;
+    final translator = S.of(context);
 
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              S.of(context).SettingsScanSliderHeader,
-              style: AppTheme.settings.optionHeaderStyle,
+    return PlatformAwareWidget(
+      android: () => Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 4),
+            child: Text(
+              translator.ScanDuration,
+              style: AppTheme.settings.android.optionHeaderStyle,
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12, right: 12, top: 4),
-              child: PlatformAwareWidget(
-                android: () => Slider(
-                  value: currentScanDuration,
-                  onChanged: onChanged,
-                  min: minScanDuration,
-                  max: maxScanDuration,
-                  divisions: 5,
-                ),
-                ios: () => SizedBox(
-                  width: double.infinity,
-                  child: CupertinoSlider(
-                    value: currentScanDuration,
-                    onChanged: onChanged,
-                    min: minScanDuration,
-                    max: maxScanDuration,
-                    divisions: 5,
-                  ),
-                ),
+          ),
+          StreamBuilder<double>(
+            initialData: initialValue,
+            stream: stream,
+            builder: (context, snapshot) => Slider(
+              value: snapshot.data!,
+              onChanged: onChanged,
+              min: minScanDuration,
+              max: maxScanDuration,
+              divisions: 5,
+            ),
+          ),
+          Center(child: _buildCurrentDurationLabel()),
+        ],
+      ),
+      ios: () => CupertinoFormRow(
+        helper: Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Row(
+            children: [
+              Expanded(child: Text(translator.ScanDuration)),
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 6),
+                child: _buildCurrentDurationLabel(),
               ),
+            ],
+          ),
+        ),
+        child: StreamBuilder<double>(
+          initialData: initialValue,
+          stream: stream,
+          builder: (context, snapshot) => SizedBox(
+            width: double.infinity,
+            child: CupertinoSlider(
+              value: snapshot.data!,
+              onChanged: onChanged,
+              min: minScanDuration,
+              max: maxScanDuration,
+              divisions: 5,
             ),
-            Center(child: Text('${currentScanDuration.floor()}s')),
-          ],
-        );
-      },
+          ),
+        ),
+      ),
     );
   }
 }

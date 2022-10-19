3f3aeb0caf2bae753469040b2ab732ca84d75072
Navaron Bracke <brackenavaron@gmail.com>
2022-10-19 21:13:43 +0200

    refactor the terms list to use a builder

--- lib/widgets/pages/settings/excluded_terms/excluded_terms_list.dart
+++ lib/widgets/pages/settings/excluded_terms/excluded_terms_list.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:weforza/model/excluded_terms_delegate.dart';
-import 'package:weforza/model/selected_excluded_term_delegate.dart';
-import 'package:weforza/widgets/pages/settings/excluded_terms/edit_excluded_term_input_field.dart';
 import 'package:weforza/widgets/pages/settings/excluded_terms/excluded_terms_list_empty.dart';
 
 /// This widget represents the list of excluded terms.
@@ -10,24 +8,15 @@ import 'package:weforza/widgets/pages/settings/excluded_terms/excluded_terms_lis
 class ExcludedTermsList extends StatelessWidget {
   const ExcludedTermsList({
     super.key,
-    this.decorator,
+    required this.builder,
     required this.delegate,
-    required this.selectedTermTextFieldKey,
-    required this.selectionDelegate,
   });
 
-  /// The function that decorates a given child.
-  final Widget? Function(Widget, int, List<String>)? decorator;
+  /// The builder function that builds the individual items.
+  final Widget Function(List<String> items, int index) builder;
 
   /// The delegate that provides the list of terms.
   final ExcludedTermsDelegate delegate;
-
-  /// The global key that keeps track
-  /// of the currently active editable term text form field.
-  final GlobalKey<FormFieldState<String>> selectedTermTextFieldKey;
-
-  /// The delegate that manages the selected term.
-  final SelectedExcludedTermDelegate selectionDelegate;
 
   @override
   Widget build(BuildContext context) {
@@ -45,20 +34,7 @@ class ExcludedTermsList extends StatelessWidget {
 
         return SliverList(
           delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              final term = terms[index];
-
-              final child = EditExcludedTermInputField(
-                key: ValueKey(term),
-                delegate: delegate,
-                index: index,
-                selectionDelegate: selectionDelegate,
-                term: term,
-                textFormFieldKey: selectedTermTextFieldKey,
-              );
-
-              return decorator?.call(child, index, terms) ?? child;
-            },
+            (context, index) => builder(terms, index),
             childCount: terms.length,
           ),
         );

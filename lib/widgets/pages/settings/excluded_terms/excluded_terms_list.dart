3f45f69efc9fac4a020b2bcb7416a8b4923d97ac
Navaron Bracke <brackenavaron@gmail.com>
2022-10-19 17:45:52 +0200

    use the new editable term input field widget in the terms list

--- lib/widgets/pages/settings/excluded_terms/excluded_terms_list.dart
+++ lib/widgets/pages/settings/excluded_terms/excluded_terms_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:weforza/model/excluded_terms_delegate.dart';
-import 'package:weforza/widgets/pages/settings/excluded_terms/excluded_term_input_field.dart';
+import 'package:weforza/model/selected_excluded_term_delegate.dart';
+import 'package:weforza/widgets/pages/settings/excluded_terms/edit_excluded_term_input_field.dart';
 import 'package:weforza/widgets/pages/settings/excluded_terms/excluded_terms_list_empty.dart';
 
 /// This widget represents the list of excluded terms.
@@ -11,6 +12,8 @@ class ExcludedTermsList extends StatelessWidget {
     super.key,
     this.decorator,
     required this.delegate,
+    required this.selectedTermTextFieldKey,
+    required this.selectionDelegate,
   });
 
   /// The function that decorates a given child.
@@ -18,6 +21,13 @@ class ExcludedTermsList extends StatelessWidget {
 
   /// The delegate that provides the list of terms.
   final ExcludedTermsDelegate delegate;
+
+  /// The global key that keeps track
+  /// of the currently active editable term text form field.
+  final GlobalKey<FormFieldState<String>> selectedTermTextFieldKey;
+
+  /// The delegate that manages the selected term.
+  final SelectedExcludedTermDelegate selectionDelegate;
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +48,13 @@ class ExcludedTermsList extends StatelessWidget {
             (context, index) {
               final term = terms[index];
 
-              final child = ExcludedTermInputField(
+              final child = EditExcludedTermInputField(
                 key: ValueKey(term),
                 delegate: delegate,
                 index: index,
+                selectionDelegate: selectionDelegate,
+                term: term,
+                textFormFieldKey: selectedTermTextFieldKey,
               );
 
               return decorator?.call(child, index, terms) ?? child;

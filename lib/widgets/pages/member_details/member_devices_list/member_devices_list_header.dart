379a47bab99f6cff26ff82a6e1d7650bfbef07fc
Navaron Bracke <brackenavaron@gmail.com>
2022-11-09 20:22:30 +0100

    remove member devices list theme class; update usages

--- lib/widgets/pages/member_details/member_devices_list/member_devices_list_header.dart
+++ lib/widgets/pages/member_details/member_devices_list/member_devices_list_header.dart
@@ -1,24 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
-import 'package:weforza/widgets/theme.dart';
 
 class MemberDevicesListHeader extends StatelessWidget {
   const MemberDevicesListHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const theme = AppTheme.memberDevicesList;
-    final child = Text(S.of(context).Devices, style: theme.headerStyle);
+    final title = S.of(context).Devices;
 
     return PlatformAwareWidget(
-      android: (_) => Padding(
-        padding: const EdgeInsets.only(top: 4),
-        child: child,
-      ),
-      ios: (_) => Padding(
+      android: (context) {
+        final textTheme = Theme.of(context).textTheme;
+
+        return Padding(
+          padding: const EdgeInsets.only(top: 4),
+          child: Text(
+            title,
+            style: textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+          ),
+        );
+      },
+      ios: (context) => Padding(
         padding: const EdgeInsets.only(top: 12),
-        child: child,
+        child: Text(
+          title,
+          style: CupertinoTheme.of(context).textTheme.navTitleTextStyle,
+        ),
       ),
     );
   }

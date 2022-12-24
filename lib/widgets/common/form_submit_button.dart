4306cc228a35e8d5a0b96c1dba5bad22995ec1f0
Navaron Bracke <brackenavaron@gmail.com>
2022-12-24 15:13:03 +0100

    refactor the form submit button

--- lib/widgets/common/form_submit_button.dart
+++ lib/widgets/common/form_submit_button.dart
@@ -1,93 +1,76 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/widgets/platform/platform_aware_loading_indicator.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
-/// This widget represents a submit button for a form layout.
-class FormSubmitButton extends StatefulWidget {
+/// This widget represents a submit button for a form.
+class FormSubmitButton<T> extends ConsumerWidget {
   const FormSubmitButton({
-    required this.errorMessageBuilder,
+    required this.errorBuilder,
     required this.label,
     required this.onPressed,
+    required this.provider,
     super.key,
-    this.future,
   });
 
-  /// The builder that builds the error message.
-  final Widget Function(Object error) errorMessageBuilder;
+  /// The builder for the error message.
+  ///
+  /// If `error` is null, the button is in the idle state.
+  final Widget Function(BuildContext context, Object? error) errorBuilder;
 
-  /// The future that represents the submit computation.
-  final Future<void>? future;
-
-  /// The label for the submit button.
+  /// The label for the button.
   final String label;
 
-  /// The onPressed handler for the button.
+  /// The onTap handler for the button.
   final void Function() onPressed;
 
-  @override
-  State<FormSubmitButton> createState() => _FormSubmitButtonState();
-}
-
-class _FormSubmitButtonState extends State<FormSubmitButton> {
-  void _onSubmitButtonPressed() {
-    widget.onPressed();
-
-    setState(() {});
-  }
+  /// The provider that will be watched for changes in the async computation.
+  final ProviderListenable<AsyncValue<T>?> provider;
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<void>(
-      future: widget.future,
-      builder: (context, snapshot) {
-        const loading = PlatformAwareLoadingIndicator();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(provider);
 
-        final button = PlatformAwareWidget(
-          android: (_) => ElevatedButton(
-            onPressed: _onSubmitButtonPressed,
-            child: Text(widget.label),
+    final button = PlatformAwareWidget(
+      android: (_) => ElevatedButton(
+        onPressed: onPressed,
+        child: Text(label),
+      ),
+      ios: (_) => CupertinoButton.filled(
+        onPressed: onPressed,
+        child: Text(label),
+      ),
+    );
+
+    if (value == null) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 4),
+            child: errorBuilder(context, null),
           ),
-          ios: (_) => CupertinoButton.filled(
-            onPressed: _onSubmitButtonPressed,
-            child: Text(widget.label),
+          button,
+        ],
+      );
+    }
+
+    const loading = PlatformAwareLoadingIndicator();
+
+    return value.when(
+      data: (value) => loading, // The submit button does not have a done state.
+      error: (error, stackTrace) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 4),
+            child: errorBuilder(context, error),
           ),
-        );
-
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 4),
-                  child: Text(''),
-                ),
-                button,
-              ],
-            );
-          case ConnectionState.active:
-          case ConnectionState.waiting:
-            return loading;
-          case ConnectionState.done:
-            final exception = snapshot.error;
-
-            if (exception != null) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 4),
-                    child: widget.errorMessageBuilder(exception),
-                  ),
-                  button,
-                ],
-              );
-            }
-
-            return loading;
-        }
-      },
+          button,
+        ],
+      ),
+      loading: () => loading,
     );
   }
 }

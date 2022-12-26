9f4bbb364e0586320fb8f5bb948ac5aa80f5fad2
Navaron Bracke <brackenavaron@gmail.com>
2022-12-26 20:07:56 +0100

    clean up base file reader interface

---
+++ lib/file/import_riders_file_reader.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:weforza/model/rider/serializable_rider.dart';
+
+/// This interface defines a file reader for importing a rider datasource file.
+abstract class ImportRidersFileReader<T> {
+  /// Process the given [chunk].
+  ///
+  /// Valid chunks are added to the given [serializedRiders] collection.
+  Future<void> processChunk(T chunk, List<SerializableRider> serializedRiders);
+
+  /// Read the given file.
+  /// Returns the file contents in readable chunks.
+  Future<List<T>> readFile(File file);
+}

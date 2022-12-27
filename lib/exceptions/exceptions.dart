2664a6772711344676651446e1b4770fac6808d5
Navaron Bracke <brackenavaron@gmail.com>
2022-12-27 15:27:06 +0100

    rename member exists exception

--- lib/exceptions/exceptions.dart
+++ lib/exceptions/exceptions.dart
@@ -1,13 +1,16 @@
+/// An exception that is thrown when a device already exists.
 class DeviceExistsException implements Exception {}
 
+/// An exception that is thrown when a [File] already exists.
 class FileExistsException implements Exception {}
 
 class InvalidFileExtensionError extends ArgumentError {
   InvalidFileExtensionError() : super('Invalid file extension.');
 }
 
-class MemberExistsException implements Exception {}
-
 class NoFileChosenError extends ArgumentError {
   NoFileChosenError() : super.notNull('No file was chosen.');
 }
+
+/// An exception that is thrown when a rider already exists.
+class RiderExistsException implements Exception {}

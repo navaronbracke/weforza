cd426cf62048c1df11d2f2d9ecf46c59e11a3dc6
Navaron Bracke <brackenavaron@gmail.com>
2022-12-30 16:01:28 +0100

    fix delegate argument

--- lib/widgets/pages/export_riders_page.dart
+++ lib/widgets/pages/export_riders_page.dart
@@ -2,10 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/generated/l10n.dart';
-import 'package:weforza/model/rider/export_riders_delegate.dart';
+import 'package:weforza/model/export/export_riders_delegate.dart';
 import 'package:weforza/riverpod/file_handler_provider.dart';
 import 'package:weforza/riverpod/repository/serialize_riders_repository_provider.dart';
-import 'package:weforza/widgets/common/export_file_format_selection.dart';
 import 'package:weforza/widgets/pages/export_data_page/export_data_page.dart';
 
 class ExportRidersPage extends ConsumerStatefulWidget {
@@ -33,23 +32,11 @@ class _ExportRidersPageState extends ConsumerState<ExportRidersPage> {
     final translator = S.of(context);
 
     return ExportDataPage(
+      delegate: _delegate,
       exportingLabel: translator.ExportingRiders,
-      fileExistsLabel: ExportFileNameExistsLabel(
-        initialData: _delegate.fileExists,
-        stream: _delegate.fileExistsStream,
+      onPressed: () => _delegate.exportDataToFile(
+        ExportRidersOptions(csvHeader: translator.ExportRidersCsvHeader),
       ),
-      fileFormatSelection: ExportFileFormatSelection(
-        initialValue: _delegate.currentFileFormat,
-        onFormatSelected: _delegate.setFileFormat,
-        stream: _delegate.fileFormatStream,
-      ),
-      fileNameController: _delegate.fileNameController,
-      fileNameFormFieldKey: _delegate.fileNameKey,
-      initialData: _delegate.currentState,
-      onPressed: () => _delegate.exportRiders(
-        csvHeader: translator.ExportRidersCsvHeader,
-      ),
-      stream: _delegate.stream,
       title: translator.ExportRiders,
     );
   }

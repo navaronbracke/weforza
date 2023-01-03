d5b282b897199f5beda700b169108483b2e68140
Navaron Bracke <brackenavaron@gmail.com>
2023-01-03 20:37:46 +0100

    dispose the database when the provider container is destroyed; Make WeforzaApp stateless

--- lib/riverpod/database/database_provider.dart
+++ lib/riverpod/database/database_provider.dart
@@ -2,6 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/database/database.dart';
 
 /// This provider provides the application database.
-final databaseProvider = Provider<ApplicationDatabase>(
-  (_) => ApplicationDatabase(),
-);
+final databaseProvider = Provider<ApplicationDatabase>((ref) {
+  final database = ApplicationDatabase();
+
+  ref.onDispose(database.dispose);
+
+  return database;
+});

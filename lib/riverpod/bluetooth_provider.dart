27fb88ad04887d6c6497e92267aef5032e0a1311
Navaron Bracke <brackenavaron@gmail.com>
2023-01-03 20:38:32 +0100

    make the bluetooth provider an autodispose provider to fix mock scanner only being available once; dispose of bluetooth scanner

--- lib/riverpod/bluetooth_provider.dart
+++ lib/riverpod/bluetooth_provider.dart
@@ -2,6 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/bluetooth/bluetooth_device_scanner.dart';
 
 /// This provider provides the bluetooth scanner.
-final bluetoothProvider = Provider<BluetoothDeviceScanner>(
-  (_) => BluetoothDeviceScannerImpl(),
-);
+final bluetoothProvider = Provider.autoDispose<BluetoothDeviceScanner>((ref) {
+  final scanner = BluetoothDeviceScannerImpl();
+
+  ref.onDispose(scanner.dispose);
+
+  return scanner;
+});

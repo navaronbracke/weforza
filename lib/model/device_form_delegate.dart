aeda20971ece55760341cc9db865157f2f161a54
Navaron Bracke <brackenavaron@gmail.com>
2023-01-08 12:46:20 +0100

    Revert "manage last existing device name; move device name key to delegate"
    This reverts commit ae71d7a5711cdad3196fafeba911285b3f1f06fb.

--- lib/model/device_form_delegate.dart
+++ lib/model/device_form_delegate.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/widgets.dart';
-import 'package:weforza/exceptions/exceptions.dart';
 import 'package:weforza/model/async_computation_delegate.dart';
 import 'package:weforza/model/device_model.dart';
 import 'package:weforza/riverpod/member/selected_member_devices_provider.dart';
@@ -8,53 +6,8 @@ import 'package:weforza/riverpod/member/selected_member_devices_provider.dart';
 class DeviceFormDelegate extends AsyncComputationDelegate<void> {
   DeviceFormDelegate({required this.notifier});
 
-  /// The [Key] for the device name input field.
-  final GlobalKey<FormFieldState<String>> deviceNameKey = GlobalKey();
-
   /// The notifier that handles adding and editing devices.
   final SelectedMemberDevicesNotifier notifier;
-
-  /// The name of the last device that is known to exist.
-  ///
-  /// When the [addDevice] or [editDevice]
-  /// method determines that a given device name already exists,
-  /// then this field is set to that device name.
-  ///
-  /// When the device name is validated using the relevant validator function,
-  /// it invokes [deviceExists] with the current device name,
-  /// which checks equality against this field.
-  String? _lastExistingDeviceName;
-
-  /// Whether the current error is a generic error.
-  ///
-  /// Returns false if error is null or a [DeviceExistsException].
-  /// Returns true otherwise.
-  bool get hasGenericError {
-    final error = currentState?.error;
-
-    // The `DeviceExistsException` is handled using `deviceExists()`.
-    if (error == null || error is DeviceExistsException) {
-      return false;
-    }
-
-    return true;
-  }
-
-  /// Returns whether the given [deviceName] matches the [_lastExistingDeviceName].
-  bool deviceExists(String deviceName) {
-    if (_lastExistingDeviceName == null) {
-      return false;
-    }
-
-    return _lastExistingDeviceName == deviceName;
-  }
-
-  void _setLastExistingDeviceName(Object error, String deviceName) {
-    if (error is DeviceExistsException) {
-      _lastExistingDeviceName = deviceName;
-      deviceNameKey.currentState?.validate();
-    }
-  }
 
   /// Add a new device.
   /// The [whenComplete] function is called if the operation was successful.
@@ -71,8 +24,6 @@ class DeviceFormDelegate extends AsyncComputationDelegate<void> {
       setDone(null);
       whenComplete();
     } catch (error, stackTrace) {
-      _setLastExistingDeviceName(error, model.name);
-
       setError(error, stackTrace);
     }
   }
@@ -92,15 +43,7 @@ class DeviceFormDelegate extends AsyncComputationDelegate<void> {
       setDone(null);
       whenComplete();
     } catch (error, stackTrace) {
-      _setLastExistingDeviceName(error, model.name);
-
       setError(error, stackTrace);
     }
-  }
-
-  @override
-  void reset() {
-    _lastExistingDeviceName = null;
-    super.reset();
   }
 }

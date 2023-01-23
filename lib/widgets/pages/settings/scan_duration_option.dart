f6c7696ae99b26fa3e61b83a58d96ee822a2baf4
Navaron Bracke <brackenavaron@gmail.com>
2023-01-23 21:36:14 +0100

    pass scan duration delegate to slider

--- lib/widgets/pages/settings/scan_duration_option.dart
+++ lib/widgets/pages/settings/scan_duration_option.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/model/settings/scan_duration_delegate.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
 /// This widget represents the scan duration slider.
@@ -10,14 +11,12 @@ import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 /// to indicate the current value in seconds.
 class ScanDurationOption extends StatelessWidget {
   const ScanDurationOption({
-    required this.initialValue,
-    required this.onChanged,
-    required this.stream,
+    required this.delegate,
     super.key,
   });
 
-  /// The initially selected scan duration, in seconds.
-  final double initialValue;
+  /// The delegate that handles changes to the value.
+  final ScanDurationDelegate delegate;
 
   /// The maximum scan duration of 60 seconds.
   final double maxScanDuration = 60;
@@ -25,16 +24,10 @@ class ScanDurationOption extends StatelessWidget {
   /// The minimum scan duration of 10 seconds.
   final double minScanDuration = 10;
 
-  /// The handler function that is called when the value changes.
-  final void Function(double value) onChanged;
-
-  /// The stream that provides updates about the current value.
-  final Stream<double> stream;
-
   Widget _buildCurrentDurationLabel() {
     return StreamBuilder<double>(
-      initialData: initialValue,
-      stream: stream,
+      initialData: delegate.currentValue,
+      stream: delegate.stream,
       builder: (context, snapshot) => Text('${snapshot.data!.floor()}s'),
     );
   }
@@ -56,11 +49,11 @@ class ScanDurationOption extends StatelessWidget {
             ),
           ),
           StreamBuilder<double>(
-            initialData: initialValue,
-            stream: stream,
+            initialData: delegate.currentValue,
+            stream: delegate.stream,
             builder: (context, snapshot) => Slider(
               value: snapshot.data!,
-              onChanged: onChanged,
+              onChanged: delegate.onValueChanged,
               min: minScanDuration,
               max: maxScanDuration,
               divisions: 5,
@@ -89,13 +82,13 @@ class ScanDurationOption extends StatelessWidget {
         ),
         padding: const EdgeInsets.all(6),
         child: StreamBuilder<double>(
-          initialData: initialValue,
-          stream: stream,
+          initialData: delegate.currentValue,
+          stream: delegate.stream,
           builder: (context, snapshot) => SizedBox(
             width: double.infinity,
             child: CupertinoSlider(
               value: snapshot.data!,
-              onChanged: onChanged,
+              onChanged: delegate.onValueChanged,
               min: minScanDuration,
               max: maxScanDuration,
               divisions: 5,

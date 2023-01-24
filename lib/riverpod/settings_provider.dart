b4c5bb8a5bba85d32793add8b095194cf00b1f65
Navaron Bracke <brackenavaron@gmail.com>
2023-01-24 19:38:02 +0100

    reimplement settings provider

--- lib/riverpod/settings_provider.dart
+++ lib/riverpod/settings_provider.dart
@@ -1,9 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/model/settings/settings.dart';
+import 'package:weforza/repository/settings_repository.dart';
 
 /// This provider provides the application settings.
 ///
 /// This provider is overridden at startup with the preloaded settings.
-final settingsProvider = StateProvider<Settings>((_) {
-  throw UnsupportedError('The settings should be preloaded at startup.');
-});
+final settingsProvider = StateNotifierProvider<SettingsNotifier, Settings>(
+  (ref) {
+    throw UnsupportedError('The settings should be preloaded at startup.');
+  },
+);
+
+/// The notifier that manages the settings.
+class SettingsNotifier extends StateNotifier<Settings> {
+  SettingsNotifier(
+    super.initialValue,
+    this._settingsRepository,
+  );
+
+  /// The repository that manages the settings.
+  final SettingsRepository _settingsRepository;
+}

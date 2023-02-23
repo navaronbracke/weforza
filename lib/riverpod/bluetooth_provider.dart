32e143080c75b8d02cb2645de51881d511ebf811
Navaron Bracke <brackenavaron@gmail.com>
2023-02-23 22:06:31 +0100

    remove TODO's that are converted into issues on Github

--- lib/riverpod/bluetooth_provider.dart
+++ lib/riverpod/bluetooth_provider.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/bluetooth/bluetooth_device_scanner.dart';
 
-// TODO: regression test for scanning twice in a row not showing the scanning progress bar the second time
-// TODO: fix the iOS CBCenteralManager not initializing properly the first time scanning is requested
-
 /// This provider provides the Bluetooth scanner.
 ///
 /// This provider is not an `autoDispose` provider,

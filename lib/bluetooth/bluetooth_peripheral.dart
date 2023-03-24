4cf4036801c0129e8d740992fd8731cdf175c123
Navaron Bracke <brackenavaron@gmail.com>
2023-03-24 18:42:42 +0100

    implement BluetoothPeripheral.fromMap

--- lib/bluetooth/bluetooth_peripheral.dart
+++ lib/bluetooth/bluetooth_peripheral.dart
@@ -1,9 +1,18 @@
 /// This class represents a Bluetooth device.
 class BluetoothPeripheral {
+  /// The default constructor.
   BluetoothPeripheral({
     required this.deviceName,
     required this.id,
   });
+
+  /// Create a [BluetoothPeripheral] from the given [map].
+  factory BluetoothPeripheral.fromMap(Map<String, Object?> map) {
+    return BluetoothPeripheral(
+      deviceName: map['deviceName'] as String,
+      id: map['deviceId'] as String,
+    );
+  }
 
   /// The name of the device.
   final String deviceName;

781fb2fdfb5d873f2057a9bed5a79d8f7ad8a451
Navaron Bracke <brackenavaron@gmail.com>
2023-03-24 15:36:27 +0100

    add factory constructor to bluetooth state; sort bluetooth state values

--- lib/bluetooth/bluetooth_state.dart
+++ lib/bluetooth/bluetooth_state.dart
@@ -1,16 +1,7 @@
 /// The different states of the Bluetooth adapter.
 enum BluetoothState {
-  /// The state of the Bluetooth adapter is currently unknown.
-  unknown,
-
-  /// The device does not support Bluetooth.
-  unavailable,
-
-  /// Access to the Bluetooth adapter is prohibited.
-  unauthorized,
-
-  /// The Bluetooth adapter is currently turning on.
-  turningOn,
+  /// The Bluetooth adapter is currently disabled.
+  off,
 
   /// The Bluetooth adapter is currently enabled.
   on,
@@ -18,6 +9,37 @@ enum BluetoothState {
   /// The Bluetooth adapter is currently turning off.
   turningOff,
 
-  /// The Bluetooth adapter is currently disabled.
-  off
+  /// The Bluetooth adapter is currently turning on.
+  turningOn,
+
+  /// Access to the Bluetooth adapter is prohibited.
+  unauthorized,
+
+  /// The device does not support Bluetooth.
+  unavailable,
+
+  /// The state of the Bluetooth adapter is currently unknown.
+  unknown;
+
+  /// Create a [BluetoothState] from the given [value].
+  factory BluetoothState.fromValue(String? value) {
+    switch (value) {
+      case 'off':
+        return off;
+      case 'on':
+        return on;
+      case 'turningOff':
+        return turningOff;
+      case 'turningOn':
+        return turningOn;
+      case 'unauthorized':
+        return unauthorized;
+      case 'unavailable':
+        return unavailable;
+      case 'unknown':
+        return unknown;
+      default:
+        throw ArgumentError.value(value);
+    }
+  }
 }

700f679ce0f7c54908021f5c2bb31da592103d1b
Navaron Bracke <brackenavaron@gmail.com>
2023-03-27 16:42:50 +0200

    setup event channels on Android; implement remaining method call handler bits on Android

--- android/app/src/main/kotlin/be/weforza/app/MainActivity.kt
+++ android/app/src/main/kotlin/be/weforza/app/MainActivity.kt
@@ -1,13 +1,27 @@
 package be.weforza.app
 
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanSettings
+import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val methodChannel = "be.weforza.app/methods"
+    private val bluetoothDeviceDiscoveryChannel = "be.weforza.app/bluetooth_device_discovery"
+    private val bluetoothStateChannel = "be.weforza.app/bluetooth_state"
 
+    private val bluetoothAdapterDelegate: BluetoothAdapter
     private val permissionDelegate = PermissionHandler()
+
+    init {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapterDelegate = BluetoothAdapter(bluetoothManager.adapter,
+            BluetoothAdapterStateStreamHandler(this)
+        )
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -15,9 +29,8 @@ class MainActivity: FlutterActivity() {
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, methodChannel).setMethodCallHandler {
                 call, result ->
             when(call.method) {
-                // TODO: move bluetooth calls into bluetooth handler class
-                // TODO: return null if not supported
-                "isBluetoothEnabled" -> result.notImplemented() // TODO: implement bluetooth on/off
+                "getBluetoothAdapterState" -> bluetoothAdapterDelegate.getState(result)
+                "isBluetoothOn" -> bluetoothAdapterDelegate.isBluetoothOn(result)
                 "requestBluetoothScanPermission" -> permissionDelegate.requestBluetoothScanPermission(
                         this,
                         object: PermissionResultCallback {
@@ -33,11 +46,26 @@ class MainActivity: FlutterActivity() {
                             }
                         }
                 )
-                "startScan" -> result.notImplemented() // TODO: implement start scan
-                "stopScan" -> result.notImplemented() // TODO: implement stop scan
+                "startBluetoothScan" -> {
+                    val options = BluetoothScanOptions(
+                        call.argument<Boolean>("allowDuplicates")?: false,
+                        call.argument<Int>("androidScanMode") ?: ScanSettings.SCAN_MODE_BALANCED
+                    )
+
+                    bluetoothAdapterDelegate.startBluetoothScan(options, result)
+                }
+                "stopBluetoothScan" -> bluetoothAdapterDelegate.stopBluetoothScan(result, this)
                 else -> result.notImplemented()
             }
         }
+
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, bluetoothDeviceDiscoveryChannel).setStreamHandler(
+            bluetoothAdapterDelegate.deviceDiscoveryStreamHandler
+        )
+
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, bluetoothStateChannel).setStreamHandler(
+            bluetoothAdapterDelegate.stateStreamHandler
+        )        
     }
 
     override fun onRequestPermissionsResult(

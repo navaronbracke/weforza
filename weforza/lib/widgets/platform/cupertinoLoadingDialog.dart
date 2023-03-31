6580d3449dcb25db0ffd0d9cee3c791f9b3193a3
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:55:48 +0200

    add ios loading dialog widget

---
+++ weforza/lib/widgets/platform/cupertinoLoadingDialog.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class CupertinoLoadingDialog extends StatelessWidget {
+  const CupertinoLoadingDialog({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CupertinoPopupSurface(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: CupertinoActivityIndicator(),
+        ),
+      ),
+    );
+  }
+}

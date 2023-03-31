4d550c39be523af341bfeccbf1ad23a15362a296
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:45 +0200

    add suffix widget support to the editable term field; show suffix icon for add term input field

--- lib/widgets/pages/settings/excluded_terms/add_excluded_term_input_field.dart
+++ lib/widgets/pages/settings/excluded_terms/add_excluded_term_input_field.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/excluded_terms_delegate.dart';
 import 'package:weforza/widgets/pages/settings/excluded_terms/excluded_term_input_field.dart';
+import 'package:weforza/widgets/platform/cupertino_icon_button.dart';
+import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
 /// This widget represents the input field for adding an excluded term.
 ///
@@ -55,9 +57,59 @@ class AddExcludedTermInputField extends StatelessWidget {
       maxLength: delegate.maxLength,
       onEditingComplete: _onEditingComplete,
       onTap: onTap,
+      suffix: _AddExcludedTermSuffixIcon(
+        controller: controller,
+        onTap: _onEditingComplete,
+        validator: (context, value) => delegate.validateTerm(
+          value,
+          S.of(context),
+        ),
+      ),
       textFieldKey: formKey,
       validator: (value) => delegate.validateTerm(value, translator),
       placeholder: translator.AddDisallowedWord,
+    );
+  }
+}
+
+class _AddExcludedTermSuffixIcon extends StatelessWidget {
+  const _AddExcludedTermSuffixIcon({
+    required this.controller,
+    required this.onTap,
+    required this.validator,
+  });
+
+  /// The controller that provides updates about the current text editing value.
+  final ValueNotifier<TextEditingValue> controller;
+
+  /// The onTap handler for the suffix button.
+  final void Function() onTap;
+
+  /// The validator for the term value.
+  final String? Function(BuildContext context, String? value) validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        if (validator(context, controller.value.text) != null) {
+          return const SizedBox.shrink();
+        }
+
+        return PlatformAwareWidget(
+          android: () => IconButton(
+            color: Colors.blue,
+            icon: const Icon(Icons.check),
+            onPressed: onTap,
+          ),
+          ios: () => CupertinoIconButton(
+            color: CupertinoColors.activeBlue,
+            icon: CupertinoIcons.checkmark_alt,
+            onPressed: onTap,
+          ),
+        );
+      },
     );
   }
 }

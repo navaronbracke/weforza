f5368e2df992cc99d0ba5a1734c7a391739638fb
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:13:42 +0200

    optimize use of initials

--- lib/widgets/pages/member_details/member_profile_image.dart
+++ lib/widgets/pages/member_details/member_profile_image.dart
@@ -6,41 +6,26 @@ import 'package:weforza/widgets/custom/profile_image/async_profile_image.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
 class MemberProfileImage extends ConsumerWidget {
-  const MemberProfileImage({Key? key}) : super(key: key);
-
-  String _getInitials({
-    required String firstName,
-    required String lastName,
-  }) {
-    return firstName[0] + lastName[0];
-  }
+  const MemberProfileImage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final profileImage = ref.watch(
-      selectedMemberProvider.select((value) => value!.profileImage),
-    );
-
-    final firstName = ref.watch(
-      selectedMemberProvider.select((value) => value!.value.firstname),
-    );
-
-    final lastName = ref.watch(
-      selectedMemberProvider.select((value) => value!.value.lastname),
+    final selectedMember = ref.watch(
+      selectedMemberProvider.select((selectedMember) => selectedMember!),
     );
 
     return PlatformAwareWidget(
       android: () => AsyncProfileImage(
         icon: Icons.person,
         size: 75,
-        personInitials: _getInitials(firstName: firstName, lastName: lastName),
-        future: profileImage,
+        personInitials: selectedMember.value.initials,
+        future: selectedMember.profileImage,
       ),
       ios: () => AsyncProfileImage(
         icon: CupertinoIcons.person_fill,
         size: 75,
-        personInitials: _getInitials(firstName: firstName, lastName: lastName),
-        future: profileImage,
+        personInitials: selectedMember.value.initials,
+        future: selectedMember.profileImage,
       ),
     );
   }

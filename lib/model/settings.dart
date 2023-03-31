b097649394189b0dc2d7e8e276f0412e5395c74a
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:09:44 +0200

    refactor the settings class

--- lib/model/settings.dart
+++ lib/model/settings.dart
@@ -3,13 +3,39 @@ import 'package:weforza/model/member_filter_option.dart';
 /// This class defines the persistent application settings.
 class Settings {
   Settings({
-    this.appVersion = '',
+    required this.appVersion,
+    required this.hasRideCalendar,
     this.memberListFilter = MemberFilterOption.all,
     this.scanDuration = 20,
   }) : assert(scanDuration > 0);
 
+  factory Settings.of(
+    Map<String, Object?> values,
+    String appVersion,
+    bool hasRideCalendar,
+  ) {
+    final memberListFilter = values['memberListFilter'] as int?;
+    final scanDuration = values['scanDuration'] as int?;
+
+    return Settings(
+      appVersion: appVersion,
+      hasRideCalendar: hasRideCalendar,
+      memberListFilter: memberListFilter == null
+          ? MemberFilterOption.all
+          : MemberFilterOption.values[memberListFilter],
+      scanDuration: scanDuration ?? 20,
+    );
+  }
+
   /// The current version of the application.
+  ///
+  /// This value is not persisted, as this is part of the application metadata.
   final String appVersion;
+
+  /// Whether the app has a ride calendar.
+  ///
+  /// This value is not persisted in the settings, as this is a computed value.
+  final bool hasRideCalendar;
 
   /// The persisted member list filter.
   final MemberFilterOption memberListFilter;
@@ -24,17 +50,5 @@ class Settings {
       'memberListFilter': memberListFilter.index,
       'scanDuration': scanDuration,
     };
-  }
-
-  /// Convert the given [values] into a settings object.
-  static Settings of(Map<String, dynamic> values) {
-    int? memberListFilter = values['memberListFilter'];
-
-    return Settings(
-      memberListFilter: memberListFilter == null
-          ? MemberFilterOption.all
-          : MemberFilterOption.values[memberListFilter],
-      scanDuration: values['scanDuration'] ?? 20,
-    );
   }
 }

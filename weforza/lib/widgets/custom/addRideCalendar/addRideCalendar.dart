d0eb334ab8314efb5a0793873da6f80740cab6ff
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:13:35 +0200

    created new calendar wrapper for the body/header

---
+++ weforza/lib/widgets/custom/addRideCalendar/addRideCalendar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weforza/widgets/custom/addRideCalendar/addRideCalendarBody.dart';
+import 'package:weforza/widgets/custom/addRideCalendar/addRideCalendarHeader.dart';
+import 'package:weforza/widgets/providers/addRideBlocProvider.dart';
+
+class AddRideCalendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = AddRideBlocProvider.of(context).bloc;
+    return Column(children: <Widget>[
+      AddRideCalenderHeader(
+        onMonthBackward: () => bloc.onHeaderBackward(),
+        onMonthForward: () => bloc.onHeaderForward(),
+        stream: bloc.headerStream,
+      ),
+      Expanded(
+        child: AddRideCalendarBody()
+      ),
+    ]);
+  }
+}

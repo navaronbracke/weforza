e6c43af19bf3c35ff9d749a0eac6306a934efefa
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:35:10 +0200

    Apply text color based off the odd/even month. Made future arg required. Extract content widget out of android/ios builders.

--- weforza/lib/widgets/pages/rideList/rideListItem.dart
+++ weforza/lib/widgets/pages/rideList/rideListItem.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:weforza/model/ride.dart';
+import 'package:weforza/theme/appTheme.dart';
 import 'package:weforza/widgets/common/rideAttendeeCounter.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 
 ///This class represents a single item for the ride list page.
 class RideListItem extends StatelessWidget {
-  RideListItem({@required this.ride, this.onPressed, this.rideAttendeeFuture}):
-        assert(ride != null && rideAttendeeFuture != null && onPressed != null);
+  RideListItem({
+    @required this.ride,
+    @required this.rideAttendeeFuture,
+    this.onPressed,
+  }): assert(ride != null && rideAttendeeFuture != null && onPressed != null);
 
   final Ride ride;
 
@@ -16,40 +20,41 @@ class RideListItem extends StatelessWidget {
   final Future<int> rideAttendeeFuture;
 
   @override
-  Widget build(BuildContext context) => GestureDetector(
-    child: PlatformAwareWidget(
-          android: () => _buildAndroidLayout(context),
-          ios: () => _buildIOSLayout(context),
-        ),
-    //Pass the still running future to the on tap, so we can wait for it to finish in the detail page.
-    onTap: () => onPressed(rideAttendeeFuture, ride),
-  );
+  Widget build(BuildContext context){
+    final Color textColor = ride.date.month % 2 == 0 ?
+      ApplicationTheme.rideListItemEvenMonthColor:
+      ApplicationTheme.rideListItemOddMonthColor;
 
-  Widget _buildAndroidLayout(BuildContext context) {
-    return ListTile(
-      title: Row(
-        children: <Widget>[
-          Text(ride.getFormattedDate(context, false)),
-          Expanded(child: Center()),
-          RideAttendeeCounter(future: rideAttendeeFuture),
-        ],
-      ),
+    final textStyle = TextStyle(color: textColor);
+
+    final content = Row(
+      children: <Widget>[
+        Expanded(
+            child: Text(
+              ride.getFormattedDate(context, false),
+              style: textStyle
+          ),
+        ),
+        RideAttendeeCounter(
+            future: rideAttendeeFuture,
+            counterStyle: textStyle
+        ),
+      ],
     );
-  }
 
-  Widget _buildIOSLayout(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(),
-      child: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Row(
-          children: <Widget>[
-            Text(ride.getFormattedDate(context, false)),
-            Expanded(child: Center()),
-            RideAttendeeCounter(future: rideAttendeeFuture),
-          ],
+    return GestureDetector(
+      child: PlatformAwareWidget(
+        android: () => ListTile(title: content),
+        ios: () => Container(
+          decoration: BoxDecoration(),
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: content,
+          ),
         ),
       ),
+      // Pass the still running future to the on tap, so we can wait for it to finish in the detail page.
+      onTap: () => onPressed(rideAttendeeFuture, ride),
     );
   }
 }

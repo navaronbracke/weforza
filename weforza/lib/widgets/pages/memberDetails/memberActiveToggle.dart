0e55b444d1ca947e71a466b5494645b88860220a
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:34:05 +0200

    Added active member toggle widget.

---
+++ weforza/lib/widgets/pages/memberDetails/memberActiveToggle.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
+
+/// This widget resembles the toggle switch for the 'active' state of a member.
+class MemberActiveToggle extends StatelessWidget {
+  MemberActiveToggle({
+    @required this.label,
+    @required this.stream,
+    @required this.onChanged,
+    @required this.onErrorBuilder
+  }): assert(
+    label != null && label.isNotEmpty && onChanged != null
+        && onErrorBuilder != null
+  );
+
+  final String label;
+  final Stream<bool> stream;
+  final void Function(bool value) onChanged;
+  final Widget Function() onErrorBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(label),
+        Padding(
+          padding: const EdgeInsets.only(left: 5),
+          child: StreamBuilder<bool>(
+            stream: stream,
+            builder: (context, snapshot){
+              if(snapshot.hasError){
+                return onErrorBuilder();
+              }
+
+              return PlatformAwareWidget(
+                android: () => Switch(
+                  value: snapshot.data,
+                  onChanged: onChanged,
+                ),
+                ios: () => CupertinoSwitch(
+                  value: snapshot.data,
+                  onChanged: onChanged,
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}

c711a9d2f24996a69b511a53503735bdf2177213
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:13:36 +0200

    clean up the settings model

--- lib/model/settings.dart
+++ lib/model/settings.dart
@@ -1,39 +1,40 @@
 import 'package:weforza/model/member_filter_option.dart';
 
-///This class defines a wrapper for application settings.
+/// This class defines the persistent application settings.
 class Settings {
   Settings({
-    this.scanDuration = 20,
-    this.memberListFilter = MemberFilterOption.all,
     this.appVersion = '',
+    this.memberListFilter = MemberFilterOption.all,
+    this.scanDuration = 20,
   }) : assert(scanDuration > 0);
 
-  /// The duration of a device scan, in seconds.
-  /// Defaults to 20 seconds
-  final int scanDuration;
-
-  /// The app version, as a string.
+  /// The current version of the application.
   final String appVersion;
 
   /// The persisted member list filter.
   final MemberFilterOption memberListFilter;
 
+  /// The duration of a device scan, in seconds.
+  /// Defaults to 20 seconds.
+  final int scanDuration;
+
   /// Convert this object to a Map.
   Map<String, dynamic> toMap() {
     return {
-      'scanDuration': scanDuration,
       'memberListFilter': memberListFilter.index,
+      'scanDuration': scanDuration,
     };
   }
 
+  /// Convert the given [values] into a settings object.
   static Settings of(Map<String, dynamic> values) {
     int? memberListFilter = values['memberListFilter'];
 
     return Settings(
-      scanDuration: values['scanDuration'] ?? 20,
       memberListFilter: memberListFilter == null
           ? MemberFilterOption.all
           : MemberFilterOption.values[memberListFilter],
+      scanDuration: values['scanDuration'] ?? 20,
     );
   }
 }

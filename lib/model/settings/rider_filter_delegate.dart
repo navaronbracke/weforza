9244859bf27a7379feb0dfddfe2e57b765b7e01b
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:22:32 +0200

    accept a settings delegate in the rider filter delegate & scan duration delegate

--- lib/model/settings/rider_filter_delegate.dart
+++ lib/model/settings/rider_filter_delegate.dart
@@ -1,10 +1,16 @@
 import 'package:weforza/model/deferred_save_delegate.dart';
 import 'package:weforza/model/member_filter_option.dart';
+import 'package:weforza/riverpod/settings_provider.dart';
 
 /// This class represents the delegate that manages
 /// the rider list filter option for the settings page.
 class RiderFilterDelegate extends DeferredSaveDelegate<MemberFilterOption> {
-  RiderFilterDelegate({required super.initialValue});
+  RiderFilterDelegate({
+    required SettingsNotifier settingsDelegate,
+    required super.initialValue,
+  }) : _settingsDelegate = settingsDelegate;
+
+  final SettingsNotifier _settingsDelegate;
 
   @override
   void saveValue(MemberFilterOption value) {
--- lib/model/settings/scan_duration_delegate.dart
+++ lib/model/settings/scan_duration_delegate.dart
@@ -1,9 +1,15 @@
 import 'package:weforza/model/deferred_save_delegate.dart';
+import 'package:weforza/riverpod/settings_provider.dart';
 
 /// This class represents the delegate that manages
 /// the scan duration option for the settings page.
 class ScanDurationDelegate extends DeferredSaveDelegate<double> {
-  ScanDurationDelegate({required super.initialValue});
+  ScanDurationDelegate({
+    required SettingsNotifier settingsDelegate,
+    required super.initialValue,
+  }) : _settingsDelegate = settingsDelegate;
+
+  final SettingsNotifier _settingsDelegate;
 
   @override
   void saveValue(double value) {

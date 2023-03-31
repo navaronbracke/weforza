1e8007b5648e80b9b0ae2c33fdfa857670ccfa0d
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:11:56 +0200

    fix a name collision

--- lib/model/save_settings_delegate.dart
+++ lib/model/save_settings_delegate.dart
@@ -1,7 +1,7 @@
 /// This interface represents a delegate for saving the application settings.
 abstract class SaveSettingsDelegate {
   /// Get the [Future] that represents the save computation.
-  Future<void>? get future;
+  Future<void>? get saveSettingsFuture;
 
   /// Save the current settings.
   void saveSettings();

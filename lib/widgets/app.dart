ef2f6368033a1a8bc8ac9bfcd48e993f6eb05d78
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:22:39 +0200

    remove the page view from the home page; use an enum to manage the selected tab

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:weforza/generated/l10n.dart';
-import 'package:weforza/widgets/pages/home_page.dart';
+import 'package:weforza/widgets/pages/home_page/home_page.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 import 'package:weforza/widgets/theme.dart';
 

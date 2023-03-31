4ac262f1be3d15b9f81b57e943b2efa76173adf5
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:35 +0200

    fix bug with pop()

--- lib/widgets/pages/device_form.dart
+++ lib/widgets/pages/device_form.dart
@@ -77,7 +77,8 @@ class DeviceFormState extends ConsumerState<DeviceForm> with DeviceValidator {
       // Notify the animated list that a device was inserted.
       _future = _delegate.addDevice(model).then((_) => navigator.pop(true));
     } else {
-      _future = _delegate.editDevice(model);
+      // The animated list does not need to insert an item when it was edited.
+      _future = _delegate.editDevice(model).then((_) => navigator.pop());
     }
 
     setState(() {});

45af9988cc29dcb3a96c6658459d6a49bc6c8c75
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:23:42 +0200

    preload export directory on iOS

--- lib/riverpod/file_handler_provider.dart
+++ lib/riverpod/file_handler_provider.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/file/file_handler.dart';
 
 /// This provider provides the file handler.
 final fileHandlerProvider = Provider<FileHandler>((_) => IoFileHandler());
+
+/// This provider provides the default [Directory] for export files.
+final exportDataDefaultDirectoryProvider = Provider<Directory?>((_) => null);

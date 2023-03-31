e44e9591d4b2d08330dae83d1f7e24ab2155d4e0
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:03:17 +0200

    rename the member blocs

--- lib/widgets/pages/memberList/memberListPage.dart
+++ lib/widgets/pages/memberList/memberListPage.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:weforza/blocs/memberListBloc.dart';
+import 'package:weforza/blocs/member_list_bloc.dart';
 import 'package:weforza/file/fileHandler.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/injection/injectionContainer.dart';

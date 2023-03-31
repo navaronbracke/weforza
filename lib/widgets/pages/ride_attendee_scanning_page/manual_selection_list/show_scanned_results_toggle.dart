6567e05774671672ef366ee245075e3d197ef9af
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:10:40 +0200

    implement the show scanned results toggle as a new widget

---
+++ lib/widgets/pages/ride_attendee_scanning_page/manual_selection_list/show_scanned_results_toggle.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weforza/theme/app_theme.dart';
+import 'package:weforza/widgets/platform/platform_aware_widget.dart';
+
+/// This widget represents the toggle for showing or hiding
+/// the scanned results on the manual selection page.
+class ShowScannedResultsToggle extends StatelessWidget {
+  const ShowScannedResultsToggle({
+    super.key,
+    required this.initialValue,
+    required this.onChanged,
+    required this.stream,
+  });
+
+  /// The initial value for the toggle switch.
+  final bool initialValue;
+
+  /// The handler for changes to the toggle value.
+  final void Function(bool) onChanged;
+
+  /// The stream that provides updates for the state of the toggle switch.
+  final Stream<bool> stream;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<bool>(
+      initialData: initialValue,
+      stream: stream,
+      builder: (context, snapshot) {
+        final showScannedResults = snapshot.data!;
+
+        return PlatformAwareWidget(
+          android: () => Switch(
+            activeColor: Colors.white,
+            activeTrackColor:
+                ApplicationTheme.androidManualSelectionSwitchActiveTrackColor,
+            onChanged: onChanged,
+            value: showScannedResults,
+          ),
+          ios: () => CupertinoSwitch(
+            onChanged: onChanged,
+            value: showScannedResults,
+          ),
+        );
+      },
+    );
+  }
+}

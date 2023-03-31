ff9830cc727d2b6b2739385b65c5f0d178f7dfbd
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:13:36 +0200

    move the device type enum; clean up the device class

--- lib/model/device.dart
+++ lib/model/device.dart
@@ -1,112 +1,56 @@
-import 'package:flutter/material.dart';
-import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/model/device_type.dart';
 
-///A device is a piece of hardware.
-///It has a [name], an [ownerId] and a [type].
+/// A device is a piece of hardware that is owned by a person.
+/// Each device has a [creationDate], a [name], an [ownerId] and a [type].
 class Device {
   Device({
-    required this.ownerId,
-    required this.name,
     required this.creationDate,
+    required this.name,
+    required this.ownerId,
     this.type = DeviceType.unknown,
   }) : assert(ownerId.isNotEmpty && name.isNotEmpty);
 
-  final String ownerId;
+  /// The creation date of this device.
+  /// This creation date is not indicative of the actual creation date
+  /// of the physical hardware item.
+  /// It is only used internally as the key for the device record.
   final DateTime creationDate;
-  String name;
-  DeviceType type;
+
+  /// The human-readable name of the device.
+  final String name;
+
+  /// The UUID of the owner of this device.
+  final String ownerId;
+
+  /// The type of this device.
+  final DeviceType type;
 
   static final RegExp deviceNameRegex = RegExp(r'^[^,]{1,40}$');
+
   static const int nameMaxLength = 40;
 
-  ///Convert this object to a Map.
+  /// Convert this object to a Map.
   Map<String, dynamic> toMap() {
-    return {'deviceName': name, 'owner': ownerId, 'type': type.index};
+    return {'deviceName': name, 'owner': ownerId, 'type': type.typeIndex};
   }
 
-  ///Create a device from a Map and a given key.
+  /// Create a device from the given [key] and [values].
   static Device of(String key, Map<String, dynamic> values) {
     assert(key.isNotEmpty);
 
-    DeviceType type = DeviceType.unknown;
-
-    if (values['type'] < DeviceType.values.length) {
-      type = DeviceType.values[values['type']];
-    }
-
     return Device(
-        creationDate: DateTime.parse(key),
-        name: values['deviceName'],
-        ownerId: values['owner'],
-        type: type);
+      creationDate: DateTime.parse(key),
+      name: values['deviceName'] as String,
+      ownerId: values['owner'] as String,
+      type: DeviceType.fromTypeIndex(values['type'] as int),
+    );
   }
 
   @override
-  bool operator ==(Object other) =>
-      other is Device && name == other.name && ownerId == other.ownerId;
+  bool operator ==(Object other) {
+    return other is Device && name == other.name && ownerId == other.ownerId;
+  }
 
   @override
-  int get hashCode => hashValues(name, ownerId);
-}
-
-/// This enum declares the different device types.
-/// [DeviceType.phone] The device is a phone.
-/// [DeviceType.powerMeter] The device is a power meter.
-/// [DeviceType.cadenceMeter] The device is a cadence meter.
-/// [DeviceType.watch] The device is a (smart)watch that supports bluetooth.
-/// [DeviceType.headset] The device is a wireless headset /
-/// a pair of wireless earbuds.
-/// [DeviceType.gps] The device is a GPS.
-/// [DeviceType.pulseMonitor] The device is a heart rate monitor.
-enum DeviceType {
-  unknown,
-  pulseMonitor,
-  powerMeter,
-  cadenceMeter,
-  watch,
-  gps,
-  headset,
-  phone;
-
-  IconData get icon {
-    switch (this) {
-      case DeviceType.headset:
-        return Icons.headset;
-      case DeviceType.watch:
-        return Icons.watch;
-      case DeviceType.powerMeter:
-        return Icons.flash_on;
-      case DeviceType.cadenceMeter:
-        return Icons.rotate_left;
-      case DeviceType.phone:
-        return Icons.smartphone;
-      case DeviceType.gps:
-        return Icons.gps_fixed;
-      case DeviceType.pulseMonitor:
-        return Icons.favorite_border;
-      default:
-        return Icons.device_unknown;
-    }
-  }
-
-  String getLabel(S translator) {
-    switch (this) {
-      case DeviceType.headset:
-        return translator.DeviceHeadset;
-      case DeviceType.watch:
-        return translator.DeviceWatch;
-      case DeviceType.powerMeter:
-        return translator.DevicePowerMeter;
-      case DeviceType.cadenceMeter:
-        return translator.DeviceCadenceMeter;
-      case DeviceType.phone:
-        return translator.DevicePhone;
-      case DeviceType.gps:
-        return translator.DeviceGPS;
-      case DeviceType.pulseMonitor:
-        return translator.DevicePulseMonitor;
-      default:
-        return translator.DeviceUnknown;
-    }
-  }
+  int get hashCode => Object.hash(name, ownerId);
 }

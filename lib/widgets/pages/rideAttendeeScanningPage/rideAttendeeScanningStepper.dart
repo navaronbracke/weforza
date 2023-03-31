ce539b09037aeca6eb1623903db183d891f2c424
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:03:17 +0200

    fix lints

--- lib/widgets/pages/rideAttendeeScanningPage/rideAttendeeScanningStepper.dart
+++ lib/widgets/pages/rideAttendeeScanningPage/rideAttendeeScanningStepper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/theme/appTheme.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
@@ -7,72 +6,85 @@ import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 ///This Widget represents the stepper at the top of the scanning page.
 ///It shows if the user is in the scanning step or the manual assignment step.
 class RideAttendeeScanningStepper extends StatelessWidget {
-  RideAttendeeScanningStepper({
+  const RideAttendeeScanningStepper({
+    Key? key,
     required this.stream,
-  });
+  }) : super(key: key);
 
   final Stream<bool> stream;
 
   @override
   Widget build(BuildContext context) => StreamBuilder<bool>(
-    initialData: true,
-    stream: stream,
-    builder: (context, snapshot) => PlatformAwareWidget(
-      android: () => _buildAndroidWidget(context, snapshot.data!),
-      ios: () => _buildIosWidget(context, snapshot.data!),
-    ),
-  );
+        initialData: true,
+        stream: stream,
+        builder: (context, snapshot) => PlatformAwareWidget(
+          android: () => _buildAndroidWidget(context, snapshot.data!),
+          ios: () => _buildIosWidget(context, snapshot.data!),
+        ),
+      );
 
-  Widget _buildAndroidWidget(BuildContext context, bool isScanningStep){
+  Widget _buildAndroidWidget(BuildContext context, bool isScanningStep) {
     return Row(
       children: <Widget>[
         Text(
-            S.of(context).RideAttendeeScanningProcessScanLabel.toUpperCase(),
-            style: TextStyle(color: isScanningStep ? ApplicationTheme.androidRideAttendeeScanProcessCurrentStepColor:
-              ApplicationTheme.androidRideAttendeeScanProcessOtherStepColor
-            ),
+          S.of(context).RideAttendeeScanningProcessScanLabel.toUpperCase(),
+          style: TextStyle(
+              color: isScanningStep
+                  ? ApplicationTheme
+                      .androidRideAttendeeScanProcessCurrentStepColor
+                  : ApplicationTheme
+                      .androidRideAttendeeScanProcessOtherStepColor),
         ),
         SizedBox(
           width: 50,
           child: Center(
-              child: Icon(
-                  Icons.arrow_forward_ios,
-                  color: ApplicationTheme.androidRideAttendeeScanProcessArrowColor
-              )
-          ),
+              child: Icon(Icons.arrow_forward_ios,
+                  color: ApplicationTheme
+                      .androidRideAttendeeScanProcessArrowColor)),
         ),
         Text(
-            S.of(context).RideAttendeeScanningProcessAddMembersLabel.toUpperCase(),
-            style: TextStyle(color: isScanningStep ? ApplicationTheme.androidRideAttendeeScanProcessOtherStepColor:
-              ApplicationTheme.androidRideAttendeeScanProcessCurrentStepColor
-          ),
+          S
+              .of(context)
+              .RideAttendeeScanningProcessAddMembersLabel
+              .toUpperCase(),
+          style: TextStyle(
+              color: isScanningStep
+                  ? ApplicationTheme
+                      .androidRideAttendeeScanProcessOtherStepColor
+                  : ApplicationTheme
+                      .androidRideAttendeeScanProcessCurrentStepColor),
         ),
       ],
     );
   }
 
-  Widget _buildIosWidget(BuildContext context, bool isScanningStep){
+  Widget _buildIosWidget(BuildContext context, bool isScanningStep) {
     return Row(
       children: <Widget>[
-        Text(
-            S.of(context).RideAttendeeScanningProcessScanLabel.toUpperCase(),
-            style: TextStyle(color: isScanningStep ? ApplicationTheme.iosRideAttendeeScanProcessCurrentStepColor:
-            ApplicationTheme.iosRideAttendeeScanProcessOtherStepColor)
-        ),
-        SizedBox(
+        Text(S.of(context).RideAttendeeScanningProcessScanLabel.toUpperCase(),
+            style: TextStyle(
+                color: isScanningStep
+                    ? ApplicationTheme
+                        .iosRideAttendeeScanProcessCurrentStepColor
+                    : ApplicationTheme
+                        .iosRideAttendeeScanProcessOtherStepColor)),
+        const SizedBox(
           width: 50,
           child: Center(
-              child: Icon(
-                  Icons.arrow_forward_ios,
-                  color: ApplicationTheme.iosRideAttendeeScanProcessArrowColor
-              )
+            child: Icon(Icons.arrow_forward_ios,
+                color: ApplicationTheme.iosRideAttendeeScanProcessArrowColor),
           ),
         ),
         Text(
-          S.of(context).RideAttendeeScanningProcessAddMembersLabel.toUpperCase(),
-          style: TextStyle(color: isScanningStep ? ApplicationTheme.iosRideAttendeeScanProcessOtherStepColor:
-            ApplicationTheme.iosRideAttendeeScanProcessCurrentStepColor
-          ),
+          S
+              .of(context)
+              .RideAttendeeScanningProcessAddMembersLabel
+              .toUpperCase(),
+          style: TextStyle(
+              color: isScanningStep
+                  ? ApplicationTheme.iosRideAttendeeScanProcessOtherStepColor
+                  : ApplicationTheme
+                      .iosRideAttendeeScanProcessCurrentStepColor),
         ),
       ],
     );

defca61ee1837894e311465667327851ee798095
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:39 +0200

    implement adaptive animated checkmark

--- lib/widgets/custom/animated_checkmark.dart
+++ lib/widgets/custom/animated_checkmark.dart
@@ -113,3 +113,68 @@ class _AnimatedCheckmarkState extends State<AnimatedCheckmark>
     super.dispose();
   }
 }
+
+/// This widget represents an [AnimatedCheckmark]
+/// that scales itself to a [fraction] of the available layout.
+class AdaptiveAnimatedCheckmark extends StatelessWidget {
+  const AdaptiveAnimatedCheckmark({
+    super.key,
+    this.color,
+    this.duration = const Duration(milliseconds: 500),
+    this.fraction = 0.3,
+    this.strokeCap = StrokeCap.round,
+    this.strokeJoin = StrokeJoin.round,
+    this.strokeWidth = 4.0,
+  });
+
+  /// The color for the stroke.
+  ///
+  /// If this is null, the Theme's primary color is used.
+  final Color? color;
+
+  /// The duration of the animation.
+  ///
+  /// Defaults to 500 milliseconds.
+  final Duration duration;
+
+  /// The percent of the shortest available constraint that this
+  /// animated checkmark should occupy.
+  ///
+  /// Defaults to 30 percent.
+  final double fraction;
+
+  /// The stroke cap for the painter.
+  ///
+  /// Defaults to [StrokeCap.round].
+  final StrokeCap strokeCap;
+
+  /// The width of the painter's stroke.
+  ///
+  /// Defaults to 4.
+  final double strokeWidth;
+
+  /// The stroke join of the painter.
+  ///
+  /// Defaults to [StrokeJoin.round].
+  final StrokeJoin strokeJoin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final paintSize = constraints.biggest.shortestSide * fraction;
+
+          return AnimatedCheckmark(
+            color: color,
+            duration: duration,
+            size: Size.square(paintSize),
+            strokeCap: strokeCap,
+            strokeJoin: strokeJoin,
+            strokeWidth: strokeWidth,
+          );
+        },
+      ),
+    );
+  }
+}

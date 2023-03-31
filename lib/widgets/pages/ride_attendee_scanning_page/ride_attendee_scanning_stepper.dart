9535c6b0ed2d1b2d2876b00f266e086aa0a49349
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:53 +0200

    refactor scan stepper labels colors; add alignment attribute; inline the chevron icon

--- lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_stepper.dart
+++ lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_stepper.dart
@@ -1,66 +1,89 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/ride_attendee_scanning/ride_attendee_scanning_state.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
-import 'package:weforza/widgets/theme.dart';
 
 /// This widget represents the stepper at the top of the scanning page.
 /// It displays the two labels of the scanning process,
 /// `Scan` and `Manual`, separated by an arrow.
-/// The currently applicable label is highlighted.
 class RideAttendeeScanningStepper extends StatelessWidget {
-  const RideAttendeeScanningStepper({super.key, required this.stream});
+  const RideAttendeeScanningStepper({
+    super.key,
+    required this.alignment,
+    required this.stream,
+  });
+
+  /// The alignment for the children of the stepper.
+  final MainAxisAlignment alignment;
 
   /// The stream that indicates the current step in the scanning process.
   final Stream<RideAttendeeScanningState> stream;
-
-  Widget _buildStepper(
-    BuildContext context,
-    ScanStepperTheme theme,
-    bool isScanningStep,
-  ) {
-    final translator = S.of(context);
-
-    return Row(
-      children: [
-        Text(
-          translator.Scan.toUpperCase(),
-          style: TextStyle(
-            color: isScanningStep ? theme.active : theme.inactive,
-          ),
-        ),
-        SizedBox(width: 40, child: Center(child: theme.arrow)),
-        Text(
-          translator.ByHand.toUpperCase(),
-          style: TextStyle(
-            color: !isScanningStep ? theme.active : theme.inactive,
-          ),
-        ),
-      ],
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<RideAttendeeScanningState>(
       stream: stream,
       builder: (context, snapshot) {
-        // Every step before the manual selection is regarded as part of the
-        // `Scan` label.
-        final isScanningStep =
+        // Every step before the manual selection should highlight the `Scan` label.
+        final isScanStep =
             snapshot.data != RideAttendeeScanningState.manualSelection;
 
+        final translator = S.of(context);
+        final scanLabel = translator.Scan.toUpperCase();
+        final manualLabel = translator.Manual.toUpperCase();
+
         return PlatformAwareWidget(
-          android: (context) => _buildStepper(
-            context,
-            AppTheme.scanStepper.android,
-            isScanningStep,
-          ),
-          ios: (context) => _buildStepper(
-            context,
-            AppTheme.scanStepper.ios,
-            isScanningStep,
-          ),
+          android: (context) {
+            final activeColor = Colors.lime.shade300;
+            const inactiveColor = Colors.white;
+
+            return Row(
+              mainAxisAlignment: alignment,
+              children: [
+                Text(
+                  scanLabel,
+                  style: TextStyle(
+                    color: isScanStep ? activeColor : inactiveColor,
+                  ),
+                ),
+                const Icon(Icons.chevron_right_rounded, size: 48),
+                Text(
+                  manualLabel,
+                  style: TextStyle(
+                    color: isScanStep ? inactiveColor : activeColor,
+                  ),
+                ),
+              ],
+            );
+          },
+          ios: (context) {
+            const activeColor = CupertinoColors.activeGreen;
+            const inactiveColor = CupertinoColors.inactiveGray;
+
+            return Row(
+              mainAxisAlignment: alignment,
+              children: [
+                Text(
+                  scanLabel,
+                  style: TextStyle(
+                    color: isScanStep ? activeColor : inactiveColor,
+                  ),
+                ),
+                const Icon(
+                  CupertinoIcons.chevron_forward,
+                  color: CupertinoColors.inactiveGray,
+                  size: 32,
+                ),
+                Text(
+                  manualLabel,
+                  style: TextStyle(
+                    color: isScanStep ? inactiveColor : activeColor,
+                  ),
+                ),
+              ],
+            );
+          },
         );
       },
     );

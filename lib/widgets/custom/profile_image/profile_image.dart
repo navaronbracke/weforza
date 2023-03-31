14aa97d4e2dab7672b3c351b267a7d411d30121b
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:32 +0200

    refactor profile image widget

--- lib/widgets/custom/profile_image/profile_image.dart
+++ lib/widgets/custom/profile_image/profile_image.dart
@@ -3,41 +3,80 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:weforza/theme/app_theme.dart';
 
-/// This widget represents a profile image of a person.
-/// If the profile image could not be loaded,
-/// the [personInitials] are displayed.
-/// If those are null or empty, [icon] is used as a fallback.
+/// This widget represents a profile image.
+///
+/// The [image] indicates which file should be loaded.
+/// If the image could not be loaded, the [personInitials] are displayed instead.
+/// If the [personInitials] have not been provided, a placeholder [icon]
+/// is displayed instead.
 class ProfileImage extends StatelessWidget {
+  /// The default constructor.
   const ProfileImage({
     super.key,
-    this.image,
-    this.size = 75,
+    this.backgroundColor,
     required this.icon,
     this.iconColor,
-    this.backgroundColor,
+    this.image,
     this.personInitials,
-  }) : assert(size > 0);
-
-  /// The profile image to show.
-  final File? image;
-
-  /// The icon to use as placeholder if [personInitials] is null or empty.
-  final IconData icon;
-
-  /// The size of this widget.
-  final double size;
+    this.size = 64,
+  });
 
   /// The background color for [icon].
   final Color? backgroundColor;
 
+  /// The icon to use as placeholder if [personInitials] is null or empty.
+  final IconData icon;
+
   /// The color for [icon].
   final Color? iconColor;
+
+  /// The profile image to show.
+  final File? image;
 
   /// The initials of the person.
   final String? personInitials;
 
+  /// The size of this widget.
+  ///
+  /// Defaults to 64.
+  final double size;
+
+  Widget _buildPlaceholder() {
+    final initials = personInitials;
+
+    if (initials == null || initials.isEmpty) {
+      return Container(
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(size / 2)),
+          color: backgroundColor,
+        ),
+        child: Center(child: Icon(icon, color: iconColor, size: .7 * size)),
+      );
+    }
+
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(size / 2)),
+        color: _getBackgroundColor(initials),
+      ),
+      child: Center(
+        child: Text(
+          personInitials!.toUpperCase(),
+          style: ApplicationTheme.personInitialsTextStyle.copyWith(
+            fontSize: .5 * size,
+          ),
+        ),
+      ),
+    );
+  }
+
   Color _getBackgroundColor(String initials) {
     int index = initials.codeUnitAt(0);
+
     if (initials.length == 2) {
       index += initials.codeUnitAt(1);
     }
@@ -47,39 +86,21 @@ class ProfileImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (image == null) {
-      if (personInitials == null || personInitials!.isEmpty) {
-        return Container(
-          height: size,
-          width: size,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(size / 2)),
-            color: backgroundColor,
-          ),
-          child: Center(child: Icon(icon, color: iconColor, size: .7 * size)),
-        );
-      }
+    final placeholder = _buildPlaceholder();
 
-      return Container(
-        height: size,
-        width: size,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(size / 2)),
-          color: _getBackgroundColor(personInitials!),
-        ),
-        child: Center(
-          child: Text(
-            personInitials!.toUpperCase(),
-            style: ApplicationTheme.personInitialsTextStyle.copyWith(
-              fontSize: .5 * size,
-            ),
-          ),
-        ),
-      );
+    if (image == null) {
+      return placeholder;
     }
 
-    return ClipOval(
-      child: Image.file(image!, width: size, height: size, fit: BoxFit.cover),
+    return Image.file(
+      image!,
+      width: size,
+      height: size,
+      fit: BoxFit.cover,
+      errorBuilder: (context, error, stackTrace) => placeholder,
+      frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+        return frame == null ? placeholder : ClipOval(child: child);
+      },
     );
   }
 }

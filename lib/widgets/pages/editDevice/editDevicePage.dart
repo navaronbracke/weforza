bc136faf02e8d5d15141f541938257d800b8e418
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:03:17 +0200

    rename the edit blocs

--- lib/widgets/pages/editDevice/editDevicePage.dart
+++ lib/widgets/pages/editDevice/editDevicePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:weforza/blocs/editDeviceBloc.dart';
+import 'package:weforza/blocs/edit_device_bloc.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/injection/injectionContainer.dart';
 import 'package:weforza/repository/deviceRepository.dart';

2c20c0aaa7c85c33b161080b5b3c5dcde8cecad0
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:36 +0200

    refactor CupertinoIconButton

--- lib/widgets/platform/cupertino_icon_button.dart
+++ lib/widgets/platform/cupertino_icon_button.dart
@@ -1,66 +1,37 @@
-import 'package:flutter/widgets.dart';
-import 'package:weforza/theme/app_theme.dart';
+import 'package:flutter/cupertino.dart';
 
-/// This widget represents an iOS variant of the Material `IconButton`.
-class CupertinoIconButton extends StatefulWidget {
+/// This widget represents a [CupertinoButton] that displays an [Icon] as child.
+class CupertinoIconButton extends StatelessWidget {
   const CupertinoIconButton({
     super.key,
+    this.color = CupertinoColors.activeBlue,
     required this.icon,
-    this.idleColor = ApplicationTheme.primaryColor,
     required this.onPressed,
-    this.onPressedColor = ApplicationTheme.secondaryColor,
-    this.size = 28,
-  });
+    this.size = kMinInteractiveDimensionCupertino,
+  }) : assert(size >= kMinInteractiveDimensionCupertino);
+
+  /// The color for the icon.
+  ///
+  /// Defaults to [CupertinoColors.activeBlue].
+  final CupertinoDynamicColor color;
 
   /// The icon for the button.
   final IconData icon;
 
-  /// The color for the icon when it is not pressed.
-  ///
-  /// Defaults to [ApplicationTheme.primaryColor].
-  final Color idleColor;
-
   /// The onTap handler for the button.
   final void Function() onPressed;
 
-  /// The color for the icon when it is pressed.
-  ///
-  /// Defaults to [ApplicationTheme.secondaryColor].
-  final Color onPressedColor;
-
   /// The size for the button.
-  ///
-  /// Defaults to 28.
   final double size;
 
   @override
-  State<CupertinoIconButton> createState() => _CupertinoIconButtonState();
-}
-
-class _CupertinoIconButtonState extends State<CupertinoIconButton> {
-  late Color color;
-
-  @override
-  void initState() {
-    super.initState();
-    color = widget.idleColor;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: widget.onPressed,
-      onTapUp: (tapUpDetails) {
-        setState(() {
-          color = widget.idleColor;
-        });
-      },
-      onTapDown: (tapUpDetails) {
-        setState(() {
-          color = widget.onPressedColor;
-        });
-      },
-      child: Icon(widget.icon, color: color, size: widget.size),
+    return CupertinoButton(
+      borderRadius: BorderRadius.zero,
+      minSize: size,
+      onPressed: onPressed,
+      padding: EdgeInsets.zero,
+      child: Icon(icon, color: color),
     );
   }
 }

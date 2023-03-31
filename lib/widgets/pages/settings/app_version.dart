06c7dbca4816e9a98b9ddfb3280c888023c9df25
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:41 +0200

    require a builder function for the app version widget

--- lib/widgets/pages/settings/app_version.dart
+++ lib/widgets/pages/settings/app_version.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/riverpod/package_info_provider.dart';
-import 'package:weforza/widgets/platform/platform_aware_widget.dart';
-import 'package:weforza/widgets/theme.dart';
 
 /// This widget represents the app version display.
 class AppVersion extends ConsumerWidget {
-  const AppVersion({super.key});
+  const AppVersion({super.key, required this.builder});
+
+  final Widget Function(String version) builder;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final translator = S.of(context);
     final packageInfo = ref.watch(packageInfoProvider);
 
-    const theme = AppTheme.settings;
-
-    return PlatformAwareWidget(
-      android: () => Text(
-        translator.AppVersionNumber(packageInfo.version),
-        style: theme.optionDescriptionStyle,
-      ),
-      ios: () => Text(
-        translator.AppVersionNumber(packageInfo.version),
-        style: theme.optionDescriptionStyle.copyWith(fontSize: 14),
-      ),
-    );
+    return builder(packageInfo.version);
   }
 }

c204ac7da515d1d5bd5a78003927a344d1839902
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:07:08 +0200

    refactor Settings class

--- lib/model/settings.dart
+++ lib/model/settings.dart
@@ -5,28 +5,24 @@ class Settings {
   Settings({
     this.scanDuration = 20,
     this.memberListFilter = MemberFilterOption.all,
+    this.appVersion = '',
   }) : assert(scanDuration > 0);
 
-  ///The duration of a device scan, in seconds.
-  ///Defaults to 20 seconds
+  /// The duration of a device scan, in seconds.
+  /// Defaults to 20 seconds
   final int scanDuration;
 
-  //This variable stores the app version string.
-  late String appVersion;
+  /// The app version, as a string.
+  final String appVersion;
 
-  // This variable denotes if there is a ride calendar.
-  // It is set separately from the constructor invocation.
-  bool hasRideCalendar = false;
-
-  // This variable stores the filter value for the member list.
+  /// The persisted member list filter.
   final MemberFilterOption memberListFilter;
 
-  ///Convert this object to a Map.
+  /// Convert this object to a Map.
   Map<String, dynamic> toMap() {
     return {
       'scanDuration': scanDuration,
       'memberListFilter': memberListFilter.index,
-      // We don't write hasRideCalendar, as this is fetched at runtime.
     };
   }
 

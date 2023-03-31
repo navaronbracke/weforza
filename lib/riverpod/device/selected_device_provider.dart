3cb721abd841d28d337061aabd3f7bc71e0d2e98
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:09:43 +0200

    remove the selected device provider

--- lib/riverpod/device/selected_device_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:weforza/model/device.dart';
-
-/// This provider provides the selected device.
-final selectedDeviceProvider = StateProvider<Device?>((_) => null);

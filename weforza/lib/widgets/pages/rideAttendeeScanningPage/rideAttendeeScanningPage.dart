aba3d79cf6c690827771c6fe1e46b43aeab0e30b
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:13:42 +0200

    Moved settings.dart out of settings dir. Removed deprecated showAllDevices option.

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/rideAttendeeScanningPage.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/rideAttendeeScanningPage.dart
@@ -6,7 +6,7 @@ import 'package:weforza/bluetooth/bluetoothDeviceScanner.dart';
 import 'package:weforza/injection/injector.dart';
 import 'package:weforza/model/member.dart';
 import 'package:weforza/model/scanResultItem.dart';
-import 'package:weforza/model/settings/settings.dart';
+import 'package:weforza/model/settings.dart';
 import 'package:weforza/repository/deviceRepository.dart';
 import 'package:weforza/repository/memberRepository.dart';
 import 'package:weforza/repository/rideRepository.dart';

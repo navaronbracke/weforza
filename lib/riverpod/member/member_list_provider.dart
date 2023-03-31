24b710dd3a7060af081c44291b502578859e37a2
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:10:37 +0200

    fix usage

--- lib/riverpod/member/member_list_provider.dart
+++ lib/riverpod/member/member_list_provider.dart
@@ -5,12 +5,12 @@ import 'package:weforza/riverpod/repository/member_repository_provider.dart';
 import 'package:weforza/riverpod/settings_provider.dart';
 
 /// This provider provides the list of members.
-final memberListProvider = FutureProvider<List<Member>>((ref) async {
+final memberListProvider = FutureProvider<List<Member>>((ref) {
   final repository = ref.read(memberRepositoryProvider);
 
   // Watch the member list filter for changes.
-  final filter = await ref.watch(
-    settingsProvider.selectAsync((settings) => settings.memberListFilter),
+  final filter = ref.watch(
+    settingsProvider.select((settings) => settings.memberListFilter),
   );
 
   return repository.getMembers(filter);

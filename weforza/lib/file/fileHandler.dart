b2ab64fe9319d37b8a68380b51598196384ad4fa
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:13:45 +0200

    Implement write ride to file.

--- weforza/lib/file/fileHandler.dart
+++ weforza/lib/file/fileHandler.dart
@@ -1,7 +1,25 @@
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:weforza/model/member.dart';
+import 'package:weforza/model/ride.dart';
+
+enum FileExtension {
+  JSON, CSV
+}
+
+extension ToFileTypeExtension on FileExtension {
+  String toFileExtension(){
+    switch(this){
+      case FileExtension.JSON: return ".json";
+      case FileExtension.CSV: return ".csv";
+    }
+    return null;
+  }
+}
 
 ///This class provides a contract to work with [File].
 abstract class IFileHandler {
@@ -15,6 +33,8 @@ abstract class IFileHandler {
   Future<File> loadProfileImageFromDisk(String path);
 
   Future<File> chooseImportMemberDatasourceFile(List<String> allowedFileExtensions);
+
+  Future<void> saveRideAndAttendeesToFile(String fileName, FileExtension extension, Ride ride, List<Member> attendees);
 }
 
 ///This class is an implementation of [IFileHandler].
@@ -37,5 +57,52 @@ class FileHandler implements IFileHandler {
   @override
   Future<File> chooseImportMemberDatasourceFile(List<String> allowedFileExtensions)
     => FilePicker.getFile(type: FileType.custom, allowedExtensions: allowedFileExtensions);
+
+  @override
+  Future<void> saveRideAndAttendeesToFile(String fileName, FileExtension extension, Ride ride, List<Member> attendees) async {
+    String filePath;
+
+    if(Platform.isAndroid){
+      filePath = "${await getExternalStorageDirectory()}/$fileName${extension.toFileExtension()}";
+    }else if(Platform.isIOS){
+      filePath = "${await getApplicationDocumentsDirectory()}/$fileName${extension.toFileExtension()}";
+    }else{
+      throw Exception("Only Android and IOS are supported");
+    }
+
+    if(filePath == null){
+      throw Exception("Could not create file path");
+    }
+
+    await _writeRideToFile(ride, attendees, File(filePath), extension);
+  }
+
+  Future<void> _writeRideToFile(Ride ride, List<Member> attendees, File file, FileExtension fileExtension) async {
+    switch(fileExtension){
+      case FileExtension.JSON: {
+        //Ride details as JSON
+        //Attendees as array of JSOn objects
+        final data = {
+          "details": ride.exportToJson(),
+          "attendees": attendees.map((a) => a.exportToJson()).toList()
+        };
+        await file.writeAsString(jsonEncode(data));
+      }
+      break;
+      case FileExtension.CSV: {
+        //Ride details on single line
+        //Attendee 1
+        //Attendee 2
+        //.....
+        final buffer = StringBuffer();
+        buffer.writeln(ride.exportToCsv());
+        for(Member m in attendees){
+          buffer.writeln(m.exportToCsv());
+        }
+        await file.writeAsString(buffer.toString());
+      }
+      break;
+    }
+  }
 
 }

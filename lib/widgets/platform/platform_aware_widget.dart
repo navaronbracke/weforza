702c1b10854c0ade198383481ac9accc4a360d59
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:10:39 +0200

    clean up the PlatformAwareWidget class; use TargetPlatform instead of Platform

--- lib/widgets/platform/platform_aware_widget.dart
+++ lib/widgets/platform/platform_aware_widget.dart
@@ -1,69 +1,39 @@
-import 'dart:io';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
-
-///This [Widget] checks the [Platform] and returns an appropriate [Widget].
-///The functions for [android] and [ios] must not be null.
-///The [platformHandler] must not be null.
+/// This class represents a widget that adapts its layout
+/// to the current [TargetPlatform].
 ///
-///Only supports Android and IOS for now.
-///This may change when support for the web and or desktop enters stable.
+/// This widget currently only supports [TargetPlatform.android] and [TargetPlatform.iOS].
 class PlatformAwareWidget extends StatelessWidget {
+  /// The default constructor.
   const PlatformAwareWidget({
+    super.key,
     required this.android,
     required this.ios,
-    this.platformHandler = const PlatformAwareWidgetPlatformChecker(),
-    Key? key,
-  }) : super(key: key);
+  });
 
-  ///The [Widget] builder for Android.
+  /// The builder that is invoked for [TargetPlatform.android].
   final Widget Function() android;
 
-  ///The [Widget] builder for IOS.
+  /// The builder that is invoked for [TargetPlatform.iOS].
   final Widget Function() ios;
 
-  ///The [PlatformAwareWidgetPlatformChecker] that will handle the [Platform] checks.
-  final PlatformAwareWidgetPlatformChecker platformHandler;
-
-  ///Build the [Widget].
-  ///Returns the result of calling [android] when [platformHandler.isAndroid] is true.
-  ///Returns the result of calling [ios] when [platformHandler.isIOS] is true.
-  ///
-  ///Using this method on unsupported [Platform]s results in a [FlutterError].
   @override
   Widget build(BuildContext context) {
-    if (platformHandler.isAndroid) {
-      return android();
-    }
-    if (platformHandler.isIOS) {
-      return ios();
-    }
+    final targetPlatform = Theme.of(context).platform;
 
-    throw FlutterError('Cannot build for an unsupported platform.');
+    switch (targetPlatform) {
+      case TargetPlatform.android:
+        return android();
+      case TargetPlatform.iOS:
+        return ios();
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+        throw FlutterError(
+          'The current target platform: $targetPlatform is unsupported.',
+        );
+    }
   }
-}
-
-///This class will perform the [Platform] checks for [PlatformAwareWidget].
-///If it is desired to mock the [Platform],
-///this class can be subclassed and injected into [PlatformAwareWidget].
-class PlatformAwareWidgetPlatformChecker {
-  const PlatformAwareWidgetPlatformChecker();
-
-  ///Check if the [Platform] is Android.
-  bool get isAndroid => Platform.isAndroid;
-
-  ///Check if the [Platform] is IOS.
-  bool get isIOS => Platform.isIOS;
-
-  ///Check if the [Platform] is Windows.
-  bool get isWindows => Platform.isWindows;
-
-  ///Check if the [Platform] is Mac OSX.
-  bool get isMacOS => Platform.isMacOS;
-
-  ///Check if the [Platform] is Linux.
-  bool get isLinux => Platform.isLinux;
-
-  ///Check if the [Platform] is Fuchsia.
-  bool get isFuchsia => Platform.isFuchsia;
 }

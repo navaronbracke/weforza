52f130acce79bcc07c2dc1f8430230910b639f80
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:39:43 +0200

    fix the counter styles for iOS

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionItemCounter.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionItemCounter.dart
@@ -17,79 +17,105 @@ class ManualSelectionItemCounter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textColor = Colors.white;
-    final width = 95.0;
-    final height = 40.0;
 
-    return SizedBox(
-      width: width,
-      height: height,
-      child: Stack(
-        children: [
-          RectangleAndCircleQuarter(
-            color: backgroundColor,
-            size: Size(width, height),
-          ),
-          Positioned(
-            top: 5,
-            left: 10,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                PlatformAwareWidget(
-                  android: () => Icon(
-                    Icons.people,
-                    color: textColor,
-                    size: 30,
-                  ),
-                  ios: () => Icon(
-                    CupertinoIcons.person_2_fill,
-                    color: textColor,
-                    size: 30,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 5),
-                  child: StreamBuilder<int>(
-                    initialData: initialData,
-                    stream: countStream,
-                    builder: (context, snapshot){
-                      final count = snapshot.data;
-                      if(snapshot.hasError || count == null){
-                        return Text(
-                          "-",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: textColor,
-                          ),
-                        );
-                      }
-
-                      // Limit the maximum shown number.
-                      // This is an edge case for overflow.
-                      if(count > 999){
-                        return Text(
-                          "999+",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: textColor,
-                          ),
-                        );
-                      }
-
+    final child = Stack(
+      children: [
+        _buildBackground(),
+        Positioned(
+          top: 5,
+          left: 10,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildIcon(),
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: StreamBuilder<int>(
+                  initialData: initialData,
+                  stream: countStream,
+                  builder: (context, snapshot){
+                    final count = snapshot.data;
+                    if(snapshot.hasError || count == null){
                       return Text(
-                        "${snapshot.data}",
+                        "-",
                         style: TextStyle(
                           fontSize: 16,
                           color: textColor,
                         ),
                       );
-                    },
-                  ),
+                    }
+
+                    // Limit the maximum shown number.
+                    // This is an edge case for overflow.
+                    if(count > 999){
+                      return Text(
+                        "999+",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: textColor,
+                        ),
+                      );
+                    }
+
+                    return Text(
+                      "${snapshot.data}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: textColor,
+                      ),
+                    );
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
+      ],
+    );
+
+    return _buildSizedBox(child);
+  }
+
+  Widget _buildBackground(){
+    return PlatformAwareWidget(
+      android: () => RectangleAndCircleQuarter(
+        color: backgroundColor,
+        size: Size(95, 40),
+      ),
+      ios: () => RectangleAndCircleQuarter(
+        color: backgroundColor,
+        size: Size(110, 40),
+        drawHalfCircle: true,
+      ),
+    );
+  }
+
+  Widget _buildIcon(){
+    return  PlatformAwareWidget(
+      android: () => Icon(
+        Icons.people,
+        color: Colors.white,
+        size: 30,
+      ),
+      ios: () => Icon(
+        CupertinoIcons.person_2_fill,
+          color: Colors.white,
+          size: 30,
+        ),
+      );
+  }
+
+  Widget _buildSizedBox(Widget child){
+    return PlatformAwareWidget(
+      android: () => SizedBox(
+        width: 95,
+        height: 40,
+        child: child,
+      ),
+      ios: () => SizedBox(
+        width: 110,
+        height: 40,
+        child: child,
       ),
     );
   }

ea1dc266b31446fedeef9e79c89ced1366a5fae1
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:11:55 +0200

    use the version string instead of the full settings

--- lib/widgets/pages/settings/app_version.dart
+++ lib/widgets/pages/settings/app_version.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/generated/l10n.dart';
-import 'package:weforza/riverpod/settings_provider.dart';
 import 'package:weforza/theme/app_theme.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
 /// This widget represents the app version display.
-class AppVersion extends ConsumerWidget {
-  const AppVersion({Key? key}) : super(key: key);
+class AppVersion extends StatelessWidget {
+  const AppVersion({Key? key, required this.version}) : super(key: key);
+
+  final String version;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final translator = S.of(context);
-    final settings = ref.watch(settingsProvider);
 
     return PlatformAwareWidget(
       android: () => Text(
-        translator.AppVersionNumber(settings.appVersion),
+        translator.AppVersionNumber(version),
         style: ApplicationTheme.appVersionTextStyle,
       ),
       ios: () => Text(
-        translator.AppVersionNumber(settings.appVersion),
+        translator.AppVersionNumber(version),
         style: ApplicationTheme.appVersionTextStyle.copyWith(
           fontSize: 14,
         ),

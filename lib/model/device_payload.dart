c2fdda4a1c5087a359665b2fdb4ff65da9db92c3
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:13:36 +0200

    impl device payload class

---
+++ lib/model/device_payload.dart
@@ -0,0 +1,24 @@
+import 'package:weforza/model/device_type.dart';
+
+/// This class represents the model to add or edit a device.
+class DevicePayload {
+  /// The default constructor.
+  const DevicePayload({
+    required this.creationDate,
+    required this.name,
+    required this.ownerId,
+    required this.type,
+  });
+
+  /// The local creation date of this device.
+  final DateTime creationDate;
+
+  /// The name of this device.
+  final String name;
+
+  /// The UUID of the owner of the device.
+  final String ownerId;
+
+  /// The type of the device.
+  final DeviceType type;
+}

b01d4a22028f97bb28293e78b915459275af6723
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:40 +0200

    make the device icon use the theme colors

--- lib/widgets/common/device_icon.dart
+++ lib/widgets/common/device_icon.dart
@@ -3,12 +3,31 @@ import 'package:weforza/model/device_type.dart';
 import 'package:weforza/widgets/theme.dart';
 
 class DeviceIcon extends StatelessWidget {
-  const DeviceIcon({super.key, required this.type});
+  const DeviceIcon({super.key, this.size, required this.type});
+
+  final double? size;
 
   final DeviceType type;
 
   @override
   Widget build(BuildContext context) {
-    return Icon(type.icon, color: AppTheme.deviceTypePicker.selectedColor);
+    switch (Theme.of(context).platform) {
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return Icon(
+          type.icon,
+          color: AppTheme.deviceTypePicker.android.selectedColor,
+          size: size,
+        );
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        return Icon(
+          type.icon,
+          color: AppTheme.deviceTypePicker.ios.selectedColor,
+          size: size,
+        );
+    }
   }
 }

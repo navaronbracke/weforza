e019d42b4fcf634438395be181116e9dd08a1fb0
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:09:45 +0200

    fix args for readApplicationSettings(); optimize settings record ref

--- lib/database/settings_dao.dart
+++ lib/database/settings_dao.dart
@@ -4,7 +4,10 @@ import 'package:weforza/model/settings.dart';
 ///This class defines a contract for managing application settings.
 abstract class ISettingsDao {
   /// Read the [Settings] from the database.
-  Future<Settings> readApplicationSettings();
+  Future<Settings> readApplicationSettings(
+    String appVersion,
+    bool hasRideCalendar,
+  );
 
   /// Write the given settings to the database.
   Future<void> writeApplicationSettings(Settings newSettings);
@@ -13,37 +16,35 @@ abstract class ISettingsDao {
 class SettingsDao implements ISettingsDao {
   SettingsDao(this._database, this._settingsStore);
 
-  ///The key for the settings record.
+  /// The key for the settings record.
   static const _settingsKey = 'APPLICATION_SETTINGS';
 
-  ///A reference to the database.
+  /// A reference to the database.
   final Database _database;
 
-  ///A reference to the [Settings] store.
+  /// A  reference to the [Settings] store.
   final StoreRef<String, Map<String, dynamic>> _settingsStore;
 
   @override
-  Future<Settings> readApplicationSettings() async {
-    final settingsRecord = await _settingsStore.findFirst(_database);
-
-    return settingsRecord == null
-        ? Settings()
-        : Settings.of(settingsRecord.value);
+  Future<Settings> readApplicationSettings(
+    String appVersion,
+    bool hasRideCalendar,
+  ) async {
+    return Settings.of(
+      await _settingsStore.record(_settingsKey).get(_database) ?? {},
+      appVersion,
+      hasRideCalendar,
+    );
   }
 
   @override
   Future<void> writeApplicationSettings(Settings newSettings) async {
-    //Find the existing settings
-    final existingSettings = await _settingsStore.findFirst(_database);
-    //Get a reference to the record with the settings key
     final recordRef = _settingsStore.record(_settingsKey);
 
-    if (existingSettings == null) {
-      //Add a new settings record under the key
-      await recordRef.add(_database, newSettings.toMap());
-    } else {
-      //Update the record under the key
+    if (await recordRef.exists(_database)) {
       await recordRef.update(_database, newSettings.toMap());
+    } else {
+      await recordRef.add(_database, newSettings.toMap());
     }
   }
 }

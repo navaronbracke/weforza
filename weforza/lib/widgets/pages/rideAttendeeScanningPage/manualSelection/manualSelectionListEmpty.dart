de91889af1b22374e98ac195628a1d27d246d4f1
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:13:40 +0200

    Added empty manual selection list widget.

---
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionListEmpty.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/theme/appTheme.dart';
+import 'package:weforza/widgets/platform/platformAwareWidget.dart';
+
+class ManualSelectionListEmpty extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          Icons.people,
+          color: ApplicationTheme.listInformationalIconColor,
+          size: MediaQuery.of(context).size.shortestSide * .1,
+        ),
+        SizedBox(height: 5),
+        Text(S.of(context).RideAttendeeScanningManualSelectionEmptyList),
+        SizedBox(height: 20),
+        PlatformAwareWidget(
+          android: () => FlatButton(
+            child: Text(
+              S.of(context).RideAttendeeScanningGoBack,
+              style: TextStyle(color: ApplicationTheme.primaryColor),
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          ios: () => CupertinoButton(
+            child: Text(
+              S.of(context).RideAttendeeScanningGoBack,
+              style: TextStyle(color: ApplicationTheme.primaryColor),
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
+      ],
+    );
+  }
+}

3f3db94edc33c2825db875b01907033e0ef9db0a
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:55:47 +0200

    Update database location test.

--- weforza/integration_test/database_location_test.dart
+++ weforza/integration_test/database_location_test.dart
@@ -1,92 +1,149 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:file/memory.dart';
+import 'package:file_testing/file_testing.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
-import 'package:sembast/sembast_io.dart';
+import 'package:sembast/sembast.dart';
 import 'package:weforza/database/database.dart';
-import 'package:weforza/database/memberDao.dart';
-import 'package:weforza/model/member.dart';
-import 'package:weforza/model/memberFilterOption.dart';
+import 'package:weforza/database/databaseFactory.dart';
+
+/// This fake database provides an implementation for testing database creation.
+/// It does not support actual transactions.
+/// For testing transactions, an actual in-memory database can be used.
+class FakeDatabase implements Database {
+  FakeDatabase({
+    required this.path,
+    required this.version,
+  });
+
+  @override
+  int version;
+
+  @override
+  final String path;
+
+  @override
+  Future close() => Future.value();
+
+  @override
+  Future<T> transaction<T>(FutureOr<T> Function(Transaction transaction) action) {
+    throw UnsupportedError("FakeDatabase does not support transactions. Use an in-memory database instead of a fake one.");
+  }
+}
+
+/// This fake database factory provides an implementation
+/// for testing database creation.
+/// It does not support incrementing database versions,
+/// codecs and database opening modes.
+class FakeDatabaseFactory implements DatabaseFactory {
+  final MemoryFileSystem fileSystem = MemoryFileSystem();
+
+  /// Uses an in memory database.
+  @override
+  bool get hasStorage => false;
+
+  @override
+  Future<void> deleteDatabase(String path) async {
+    final File database = fileSystem.file(path);
+
+    if(await database.exists()){
+      await database.delete();
+    }
+  }
+
+  @override
+  Future<Database> openDatabase(String path, {int? version, OnVersionChangedFunction? onVersionChanged, DatabaseMode? mode, SembastCodec? codec}) async {
+    final File storage = fileSystem.file(path);
+
+    if(await storage.exists()){
+      return FakeDatabase(path: storage.path, version: version ?? 1);
+    }
+
+    // Use recursive to avoid having to create empty directories separately.
+    // It's just for testing anyway.
+    final File newStorage = await storage.create(recursive: true);
+    return FakeDatabase(path: newStorage.path, version: version ?? 1);
+  }
+}
 
 void main(){
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  /// Integration/regression test for the database location.
-  /// Older versions of the app used to store the database
-  /// in the Documents folder. Newer versions correctly store the database
-  /// in the Application Support directory.
   /*
   Run this test:
   flutter drive --driver=test_driver/integration_test.dart --target=integration_test/database_location_test.dart
   */
-  testWidgets("Database in documents directory is moved to application support directory", (WidgetTester tester) async {
-    final testDbName = "test_database.db";
-    final Member testRecord = Member(
-      uuid: "1",
-      firstname: "John",
-      lastname: "Doe",
-      alias: "Test",
-      isActiveMember: true,
-      profileImageFilePath: null,
-      lastUpdated: DateTime.now(),
-    );
+  group("Database creation tests", (){
+    /// Integration / regression test for new database creation.
+    /// New databases should be created in
+    /// the Application Support directory provided by the platform.
+    ///
+    /// New databases should also be created with the correct version number.
+    testWidgets("New databases are created in the Application Support Directory", (WidgetTester tester) async {
+      final testDbName = "test_database.db";
+      final applicationDatabase = ApplicationDatabase(databaseName: testDbName);
 
-    /*
-    * Arrange:
-    * Create a new empty db in the old location.
-    * Then insert a test record.
-    * */
-    final db = ApplicationDatabase(databaseName: testDbName);
+      // Setup the fake file system.
+      // Only provide the application support folder.
+      final newDirectory = await getApplicationSupportDirectory();
+      final dbFactory = ApplicationDatabaseFactory(
+        fileSystem: MemoryFileSystem(),
+        factory: FakeDatabaseFactory(),
+      );
 
-    final oldDirectory = await getApplicationDocumentsDirectory();
-    final oldDatabasePath = path.join(oldDirectory.path, testDbName);
+      // Create the actual database.
+      await applicationDatabase.openDatabase(dbFactory);
 
-    IMemberDao dao = MemberDao(
-      await databaseFactoryIo.openDatabase(oldDatabasePath),
-      db.memberStore,
-      db.rideAttendeeStore,
-      db.deviceStore,
-    );
-    await dao.addMember(testRecord);
+      final newDatabasePath = path.join(newDirectory.path, testDbName);
+      final realDb = applicationDatabase.getDatabase();
 
-    // Act: Create the 'new' database.
-    await db.createDatabase();
+      // There is currently no database migration in use.
+      // Thus new databases should start with version 1.
+      expect(realDb.path, newDatabasePath);
+      expect(realDb.version, 1);
+    });
 
-    final newDirectory = await getApplicationSupportDirectory();
-    final newDatabasePath = path.join(newDirectory.path, testDbName);
+    /// Integration / regression test for old databases that need to be moved
+    /// to the Application Support directory provided by the platform.
+    ///
+    /// Older versions of the application used to store their database
+    /// in the Documents directory.
+    /// Newer version correctly store the database
+    /// in the Application Support directory.
+    testWidgets("Database in documents directory is moved to application support directory", (WidgetTester tester) async {
+      final testDbName = "test_database.db";
+      final db = ApplicationDatabase(databaseName: testDbName);
+      final dbFactory = ApplicationDatabaseFactory(
+        fileSystem: MemoryFileSystem(),
+        factory: FakeDatabaseFactory(),
+      );
 
-    final newDatabase = await databaseFactoryIo.openDatabase(newDatabasePath);
-    final newDatabaseVersion = newDatabase.version;
-    // Get a new DAO for the new database.
-    dao = MemberDao(
-      newDatabase,
-      db.memberStore,
-      db.rideAttendeeStore,
-      db.deviceStore,
-    );
+      // Setup the fake file system.
+      // Provide the old and new folders where the database files exist.
+      final oldDirectory = await getApplicationDocumentsDirectory();
+      final newDirectory = await getApplicationSupportDirectory();
 
-    final records = await dao.getMembers(MemberFilterOption.ALL);
-    final oldDbFile = File(oldDatabasePath);
-    final newDbFile = File(newDatabasePath);
-    /*
-    * Assert:
-    * Has the database moved to the new directory?
-    * Is the version unchanged?
-    * Is the data unchanged?
-    * */
-    expect(await oldDbFile.exists(), false);
-    expect(await newDbFile.exists(), true);
-    expect(records.first, testRecord);
-    expect(newDatabaseVersion, 1);
+      // Create a database in the old directory.
+      final oldDatabasePath = path.join(oldDirectory.path, testDbName);
+      await dbFactory.factory.openDatabase(oldDatabasePath);
 
-    // Clean up.
-    try {
-      await oldDbFile.delete();
-    }catch (_){}
-    try {
-      await newDbFile.delete();
-    }catch (_){}
+      // Try to open the application database.
+      // Since the 'old' database is in the wrong directory,
+      // we expect it to be moved.
+      await db.openDatabase(dbFactory);
+
+      final newDatabasePath = path.join(newDirectory.path, testDbName);
+      final newDatabase = db.getDatabase();
+
+      // There is currently no database migration in use.
+      // Thus databases should start with version 1.
+      expect(newDatabase.version, 1);
+      expect(newDatabase.path, newDatabasePath);
+      expect(dbFactory.fileSystem.file(oldDatabasePath), isNot(exists));
+    });
   });
 }

97b1efcfd92021af522e9aa75bc6a7076ad2bdf5
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:13:34 +0200

    convert device type to enhanced enum

--- lib/model/device.dart
+++ lib/model/device.dart
@@ -1,14 +1,15 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:weforza/generated/l10n.dart';
 
 ///A device is a piece of hardware.
 ///It has a [name], an [ownerId] and a [type].
 class Device {
-  Device(
-      {required this.ownerId,
-      required this.name,
-      required this.creationDate,
-      this.type = DeviceType.unknown})
-      : assert(ownerId.isNotEmpty && name.isNotEmpty);
+  Device({
+    required this.ownerId,
+    required this.name,
+    required this.creationDate,
+    this.type = DeviceType.unknown,
+  }) : assert(ownerId.isNotEmpty && name.isNotEmpty);
 
   final String ownerId;
   final DateTime creationDate;
@@ -25,9 +26,9 @@ class Device {
   ///Create a device from a Map and a given key.
   static Device of(String key, Map<String, dynamic> values) {
     assert(key.isNotEmpty);
-    //Start with unknown
+
     DeviceType type = DeviceType.unknown;
-    //if the index fits within the values, use it
+
     if (values['type'] < DeviceType.values.length) {
       type = DeviceType.values[values['type']];
     }
@@ -64,5 +65,47 @@ enum DeviceType {
   watch,
   gps,
   headset,
-  phone,
+  phone;
+
+  IconData get icon {
+    switch (this) {
+      case DeviceType.headset:
+        return Icons.headset;
+      case DeviceType.watch:
+        return Icons.watch;
+      case DeviceType.powerMeter:
+        return Icons.flash_on;
+      case DeviceType.cadenceMeter:
+        return Icons.rotate_left;
+      case DeviceType.phone:
+        return Icons.smartphone;
+      case DeviceType.gps:
+        return Icons.gps_fixed;
+      case DeviceType.pulseMonitor:
+        return Icons.favorite_border;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  String getLabel(S translator) {
+    switch (this) {
+      case DeviceType.headset:
+        return translator.DeviceHeadset;
+      case DeviceType.watch:
+        return translator.DeviceWatch;
+      case DeviceType.powerMeter:
+        return translator.DevicePowerMeter;
+      case DeviceType.cadenceMeter:
+        return translator.DeviceCadenceMeter;
+      case DeviceType.phone:
+        return translator.DevicePhone;
+      case DeviceType.gps:
+        return translator.DeviceGPS;
+      case DeviceType.pulseMonitor:
+        return translator.DevicePulseMonitor;
+      default:
+        return translator.DeviceUnknown;
+    }
+  }
 }

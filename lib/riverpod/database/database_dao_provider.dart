e91a2a5bd9846d88753fd428df2f8946827c587d
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:10:45 +0200

    refactor the dao provider to use the new database tables provider

--- lib/riverpod/database/database_dao_provider.dart
+++ lib/riverpod/database/database_dao_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weforza/database/database_tables.dart';
 import 'package:weforza/database/device_dao.dart';
 import 'package:weforza/database/export_rides_dao.dart';
 import 'package:weforza/database/import_members_dao.dart';
@@ -6,73 +7,78 @@ import 'package:weforza/database/member_dao.dart';
 import 'package:weforza/database/ride_dao.dart';
 import 'package:weforza/database/settings_dao.dart';
 import 'package:weforza/riverpod/database/database_provider.dart';
-import 'package:weforza/riverpod/database/database_store_provider.dart';
 
 /// This provider provides the device dao.
 final deviceDaoProvider = Provider<IDeviceDao>((ref) {
   final database = ref.read(databaseProvider);
+  final databaseTables = ref.read(databaseTableProvider);
 
   return DeviceDao(
     database.getDatabase(),
-    ref.read(deviceStoreProvider),
-    ref.read(memberStoreProvider),
+    databaseTables.device,
+    databaseTables.member,
   );
 });
 
 /// This provider provides the export rides dao.
 final exportRidesDaoProvider = Provider<IExportRidesDao>((ref) {
   final database = ref.read(databaseProvider);
+  final databaseTables = ref.read(databaseTableProvider);
 
   return ExportRidesDao(
     database.getDatabase(),
-    ref.read(memberStoreProvider),
-    ref.read(rideStoreProvider),
-    ref.read(rideAttendeeStoreProvider),
+    databaseTables.member,
+    databaseTables.ride,
+    databaseTables.rideAttendee,
   );
 });
 
 /// This provider provides the import members dao.
 final importMembersDaoProvider = Provider<IImportMembersDao>((ref) {
   final database = ref.read(databaseProvider);
+  final databaseTables = ref.read(databaseTableProvider);
 
   return ImportMembersDao(
     database.getDatabase(),
-    ref.read(memberStoreProvider),
-    ref.read(deviceStoreProvider),
+    databaseTables.member,
+    databaseTables.device,
   );
 });
 
 /// This provider provides the member dao.
 final memberDaoProvider = Provider<IMemberDao>((ref) {
   final database = ref.read(databaseProvider);
+  final databaseTables = ref.read(databaseTableProvider);
 
   return MemberDao(
     database.getDatabase(),
-    ref.read(memberStoreProvider),
-    ref.read(rideAttendeeStoreProvider),
-    ref.read(deviceStoreProvider),
+    databaseTables.member,
+    databaseTables.rideAttendee,
+    databaseTables.device,
   );
 });
 
 /// This provider provides the ride dao.
 final rideDaoProvider = Provider<IRideDao>((ref) {
   final database = ref.read(databaseProvider);
+  final databaseTables = ref.read(databaseTableProvider);
 
   return RideDao(
     database.getDatabase(),
-    ref.read(memberStoreProvider),
-    ref.read(rideStoreProvider),
-    ref.read(rideAttendeeStoreProvider),
+    databaseTables.member,
+    databaseTables.ride,
+    databaseTables.rideAttendee,
   );
 });
 
 /// This provider provides the application settings dao.
 final settingsDaoProvider = Provider<ISettingsDao>((ref) {
   final database = ref.read(databaseProvider);
+  final databaseTables = ref.read(databaseTableProvider);
 
   return SettingsDao(
     database.getDatabase(),
-    ref.read(settingsStoreProvider),
-    ref.read(rideStoreProvider),
+    databaseTables.settings,
+    databaseTables.ride,
   );
 });

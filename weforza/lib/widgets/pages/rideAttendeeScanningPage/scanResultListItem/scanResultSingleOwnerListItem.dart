0186e655a238f2763dea9d60d6ccae1c95e547cb
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:21:44 +0200

    Refactored ScanResultSingleOwnerListItem.

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/scanResultListItem/scanResultSingleOwnerListItem.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/scanResultListItem/scanResultSingleOwnerListItem.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/member.dart';
-import 'package:weforza/theme/appTheme.dart';
 
 class ScanResultSingleOwnerListItem extends StatelessWidget {
   ScanResultSingleOwnerListItem({
-    @required this.deviceName,
-    @required this.owner,
-  }): assert(owner != null && deviceName != null && deviceName.isNotEmpty);
+    @required this.owner
+  }): assert(owner != null);
 
-  final String deviceName;
   final Member owner;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SelectableText(deviceName, scrollPhysics: ClampingScrollPhysics()),
-          Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: Text(
-              _formatDeviceOwnerNameLabel(context,owner.firstname,owner.lastname,owner.alias),
-              style: ApplicationTheme.rideAttendeeScanResultOwnerLabelTextStyle,
-              overflow: TextOverflow.ellipsis,
-            ),
-          )
-        ],
+      child: Text(
+        _formatDeviceOwnerNameLabel(context,owner.firstname,owner.lastname,owner.alias),
+        overflow: TextOverflow.ellipsis,
       ),
     );
   }

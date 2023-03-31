ecec89d53c09e380aa56a4d355f6ec8c66d46eba
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:13:41 +0200

    reimplement ride scan results list

---
+++ lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scan_results_list.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weforza/generated/l10n.dart';
+import 'package:weforza/model/ride_attendee_scanning/ride_attendee_scanning_delegate.dart';
+import 'package:weforza/model/ride_attendee_scanning/scanned_device.dart';
+import 'package:weforza/theme/app_theme.dart';
+import 'package:weforza/widgets/common/member_name_and_alias.dart';
+import 'package:weforza/widgets/pages/ride_attendee_scanning_page/scan_progress_indicator.dart';
+import 'package:weforza/widgets/pages/ride_attendee_scanning_page/stop_scan_button.dart';
+import 'package:weforza/widgets/platform/platform_aware_widget.dart';
+
+/// This widget represents the scan results list
+/// for the ride attendee scanning page.
+class RideAttendeeScanResultsList extends StatelessWidget {
+  const RideAttendeeScanResultsList({
+    super.key,
+    required this.delegate,
+    required this.scanResultsListKey,
+  });
+
+  /// The delegate that manages the device scan.
+  final RideAttendeeScanningDelegate delegate;
+
+  /// The key for the scan results list.
+  final GlobalKey<AnimatedListState> scanResultsListKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: delegate.stopScan,
+      child: Column(
+        children: [
+          ScanProgressIndicator(
+            isScanning: delegate.scanner.isScanning,
+            scanDuration: Duration(seconds: delegate.settings.scanDuration),
+          ),
+          Expanded(
+            child: AnimatedList(
+              key: scanResultsListKey,
+              itemBuilder: (context, index, animation) {
+                return SizeTransition(
+                  sizeFactor: animation,
+                  child: _RideAttendeeScanResultsListItem(
+                    device: delegate.getScanResult(index),
+                  ),
+                );
+              },
+            ),
+          ),
+          StopScanButton(delegate: delegate),
+        ],
+      ),
+    );
+  }
+}
+
+/// This widget represents a single item for the [RideAttendeeScanResultsList].
+class _RideAttendeeScanResultsListItem extends StatelessWidget {
+  const _RideAttendeeScanResultsListItem({required this.device});
+
+  final ScannedDevice device;
+
+  Widget _buildMultipleOwnersListItem(String amountOfOwnersLabel) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 4),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 4),
+                child: PlatformAwareWidget(
+                  android: () => const Icon(
+                    Icons.people,
+                    color: ApplicationTheme.multipleOwnerColor,
+                  ),
+                  ios: () => const Icon(
+                    CupertinoIcons.person_2_fill,
+                    color: ApplicationTheme.multipleOwnerColor,
+                  ),
+                ),
+              ),
+              SelectableText(
+                device.name,
+                scrollPhysics: const ClampingScrollPhysics(),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 4),
+          child: Text(
+            amountOfOwnersLabel,
+            style: ApplicationTheme.multipleOwnersLabelStyle,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSingleOwnerListItem() {
+    final owner = device.owners.first;
+    const textStyle = TextStyle(
+      color: ApplicationTheme.rideAttendeeScanResultSingleOwnerColor,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 4),
+          child: PlatformAwareWidget(
+            android: () => const Icon(
+              Icons.person,
+              color: ApplicationTheme.rideAttendeeScanResultSingleOwnerColor,
+            ),
+            ios: () => const Icon(
+              CupertinoIcons.person_fill,
+              color: ApplicationTheme.rideAttendeeScanResultSingleOwnerColor,
+            ),
+          ),
+        ),
+        MemberNameAndAlias(
+          alias: owner.alias,
+          firstName: owner.firstname,
+          firstLineStyle: textStyle,
+          lastName: owner.lastname,
+          secondLineStyle: textStyle,
+          isTwoLine: false,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child;
+
+    // Build a list item for an unknown device.
+    if (device.owners.isEmpty) {
+      child = Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4),
+            child: Icon(Icons.device_unknown, color: Colors.black),
+          ),
+          SelectableText(
+            device.name,
+            scrollPhysics: const ClampingScrollPhysics(),
+          ),
+        ],
+      );
+    }
+
+    if (device.owners.length == 1) {
+      child = _buildSingleOwnerListItem();
+    }
+
+    child = _buildMultipleOwnersListItem(
+      S.of(context).AmountOfRidersWithDeviceName(device.owners.length),
+    );
+
+    return Padding(padding: const EdgeInsets.all(4), child: child);
+  }
+}

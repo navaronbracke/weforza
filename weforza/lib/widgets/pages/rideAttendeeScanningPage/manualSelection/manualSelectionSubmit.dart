028d78d89948ad20855412c94311bfdbfa4f9af0
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:39:43 +0200

    optimize manual selection submit builder function

--- weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionSubmit.dart
+++ weforza/lib/widgets/pages/rideAttendeeScanningPage/manualSelection/manualSelectionSubmit.dart
@@ -19,80 +19,72 @@ class ManualSelectionSubmit extends StatelessWidget {
   final int Function() getAttendeeCount;
 
   @override
-  Widget build(BuildContext context) => PlatformAwareWidget(
-    android: _buildAndroidWidget,
-    ios: _buildIosWidget,
-  );
+  Widget build(BuildContext context) => Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ManualSelectionItemCounter(
+            backgroundColor: ApplicationTheme.primaryColor,
+            countStream: attendeeCountStream,
+            initialData: getAttendeeCount(),
+          ),
+          Expanded(child: Center()),
+          PlatformAwareWidget(
+            android: _buildAndroidWidget,
+            ios: _buildIosWidget,
+          ),
+        ],
+      );
 
-  //TODO if both widgets work similarly, rewrite the stream builder
-
-  Widget _buildAndroidWidget(){
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        ManualSelectionItemCounter(
-          backgroundColor: ApplicationTheme.primaryColor,
-          countStream: attendeeCountStream,
-          initialData: getAttendeeCount(),
-        ),
-        Expanded(child: Center()),
-        StreamBuilder<bool>(
-          stream: isSaving,
-          initialData: false,
-          builder: (context, snapshot){
-            return snapshot.data! ? Padding(
-              padding: const EdgeInsets.only(right: 15, top: 15, bottom: 15),
-              child: SizedBox(
-                width: 25, height: 25,
-                child: CircularProgressIndicator(strokeWidth: 2),
-              ),
-            ): Padding(
-              padding: const EdgeInsets.only(right: 10, top: 10, bottom: 10),
-              child: ElevatedButton(
-                child: Text(S.of(context).Save),
-                onPressed: onSave,
-              ),
-            );
-          },
-        ),
-      ],
+  Widget _buildAndroidWidget() {
+    return StreamBuilder<bool>(
+      stream: isSaving,
+      initialData: false,
+      builder: (context, snapshot) {
+        return snapshot.data!
+            ? Padding(
+                padding: const EdgeInsets.only(right: 15, top: 15, bottom: 15),
+                child: SizedBox(
+                  width: 25,
+                  height: 25,
+                  child: CircularProgressIndicator(strokeWidth: 2),
+                ),
+              )
+            : Padding(
+                padding: const EdgeInsets.only(right: 10, top: 10, bottom: 10),
+                child: ElevatedButton(
+                  child: Text(S.of(context).Save),
+                  onPressed: onSave,
+                ),
+              );
+      },
     );
   }
 
-  Widget _buildIosWidget(){
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        ManualSelectionItemCounter(
-          backgroundColor: ApplicationTheme.primaryColor,
-          countStream: attendeeCountStream,
-          initialData: getAttendeeCount(),
-        ),
-        Expanded(child: Center()),
-        StreamBuilder<bool>(
-          stream: isSaving,
-          initialData: false,
-          builder: (context, snapshot){
-            return snapshot.data! ? Padding(
-              padding: const EdgeInsets.only(right: 20, bottom: 14),
-              child: CupertinoActivityIndicator(radius: 12.5),
-            ): Padding(
-              padding: const EdgeInsets.only(top: 10, right: 10),
-              child: CupertinoButton.filled(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 14.0,
-                  horizontal: 56,
+  Widget _buildIosWidget() {
+    return StreamBuilder<bool>(
+      stream: isSaving,
+      initialData: false,
+      builder: (context, snapshot) {
+        return snapshot.data!
+            ? Padding(
+                padding: const EdgeInsets.only(right: 20, bottom: 14),
+                child: CupertinoActivityIndicator(radius: 12.5),
+              )
+            : Padding(
+                padding: const EdgeInsets.only(top: 10, right: 10),
+                child: CupertinoButton.filled(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 14.0,
+                    horizontal: 56,
+                  ),
+                  child: Text(
+                    S.of(context).Save,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: onSave,
                 ),
-                child: Text(
-                  S.of(context).Save,
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: onSave,
-              ),
-            );
-          },
-        ),
-      ],
+              );
+      },
     );
   }
 }

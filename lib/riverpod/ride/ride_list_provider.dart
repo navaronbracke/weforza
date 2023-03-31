55647c35f0fa5236898d91c331011d995679b0b1
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:09:45 +0200

    impl ride list count provider

--- lib/riverpod/ride/ride_list_provider.dart
+++ lib/riverpod/ride/ride_list_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/model/ride.dart';
 import 'package:weforza/riverpod/repository/ride_repository_provider.dart';
@@ -7,4 +9,26 @@ final rideListProvider = FutureProvider<List<Ride>>((ref) {
   final repository = ref.read(rideRepositoryProvider);
 
   return repository.getRides();
+});
+
+/// This provider provides a stream of changes to the number of rides.
+final rideListCountProvider = StreamProvider<int>((ref) {
+  final repository = ref.read(rideRepositoryProvider);
+
+  final controller = StreamController<int>();
+
+  final subscription = repository.getRideCount().listen((event) {
+    if (controller.isClosed) {
+      return;
+    }
+
+    controller.add(event);
+  });
+
+  ref.onDispose(() {
+    subscription.cancel();
+    controller.close();
+  });
+
+  return controller.stream;
 });

77f7f4b06aa88a121c101b9f3cee2fa4a05207c2
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:03:17 +0200

    rename the import / export bloc

--- lib/widgets/pages/exportRide/exportRidePage.dart
+++ lib/widgets/pages/exportRide/exportRidePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:weforza/blocs/exportRideBloc.dart';
+import 'package:weforza/blocs/export_ride_bloc.dart';
 import 'package:weforza/file/fileHandler.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/exportDataOrError.dart';
--- lib/widgets/pages/importMembers/importMembersPage.dart
+++ lib/widgets/pages/importMembers/importMembersPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:weforza/blocs/importMembersBloc.dart';
+import 'package:weforza/blocs/import_members_bloc.dart';
 import 'package:weforza/exceptions/exceptions.dart';
 import 'package:weforza/file/fileHandler.dart';
 import 'package:weforza/generated/l10n.dart';

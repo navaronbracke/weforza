2b409a75def88a47681b8058b16b67dd9eecce36
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:43 +0200

    implement settings page scrollview wrapper

--- lib/widgets/pages/settings/settings_page.dart
+++ lib/widgets/pages/settings/settings_page.dart
@@ -276,3 +276,59 @@ class SettingsPageState extends ConsumerState<SettingsPage>
     super.dispose();
   }
 }
+
+class _SettingsPageScrollView extends StatelessWidget {
+  const _SettingsPageScrollView({
+    required this.addExcludedTermInputField,
+    required this.excludedTermsList,
+    required this.memberListFilter,
+    this.navigationBar,
+    required this.resetRideCalendarButton,
+    required this.scanDurationOption,
+    required this.version,
+  });
+
+  /// The widget that represents the input field for adding a new excluded term.
+  final Widget addExcludedTermInputField;
+
+  /// The widget that represents the list of excluded terms.
+  final Widget excludedTermsList;
+
+  /// The widget that represents the member list filter.
+  final Widget memberListFilter;
+
+  /// The sliver navigation bar that is placed first in the scrollview.
+  final Widget? navigationBar;
+
+  /// The widget that displays the reset ride calendar button.
+  final Widget resetRideCalendarButton;
+
+  /// The widget that displays the scan duration option.
+  final Widget scanDurationOption;
+
+  /// The widget that displays the app version information.
+  final Widget version;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        if (navigationBar != null) navigationBar!,
+        SliverList(
+          delegate: SliverChildListDelegate.fixed([
+            memberListFilter,
+            scanDurationOption,
+          ]),
+        ),
+        addExcludedTermInputField,
+        excludedTermsList,
+        SliverList(
+          delegate: SliverChildListDelegate.fixed([
+            resetRideCalendarButton,
+            version,
+          ]),
+        ),
+      ],
+    );
+  }
+}

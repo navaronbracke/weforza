9d9d2aa814294e21207c0e0cd509c99f73642029
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:10:40 +0200

    split the selected ride attendees from the selected ride

--- lib/riverpod/ride/selected_ride_provider.dart
+++ lib/riverpod/ride/selected_ride_provider.dart
@@ -1,38 +1,47 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/model/member.dart';
 import 'package:weforza/model/ride.dart';
-import 'package:weforza/model/selected_ride.dart';
 import 'package:weforza/riverpod/repository/ride_repository_provider.dart';
 import 'package:weforza/riverpod/ride/ride_list_provider.dart';
 
 /// This provider manages the selected ride.
-final selectedRideProvider =
-    StateNotifierProvider<SelectedRideNotifier, SelectedRide?>(
+final selectedRideProvider = StateNotifierProvider<SelectedRideNotifier, Ride?>(
   SelectedRideNotifier.new,
 );
 
-class SelectedRideNotifier extends StateNotifier<SelectedRide?> {
+/// This provider provides the list of attendees for the currently selected ride.
+final selectedRideAttendeesProvider = FutureProvider<List<Member>>((ref) {
+  final repository = ref.watch(rideRepositoryProvider);
+  final selectedRide = ref.watch(selectedRideProvider);
+
+  if (selectedRide == null) {
+    return Future.error(ArgumentError.notNull('selectedRide'));
+  }
+
+  return repository.getRideAttendees(selectedRide.date);
+});
+
+class SelectedRideNotifier extends StateNotifier<Ride?> {
   SelectedRideNotifier(this.ref) : super(null);
 
   final Ref ref;
-
-  void setSelectedRide({
-    required Future<List<Member>> attendees,
-    required Ride ride,
-  }) {
-    state = SelectedRide(attendees, ride);
-  }
 
   Future<void> deleteRide() async {
     final ride = state;
 
     if (ride == null) {
-      return Future.error(ArgumentError.notNull('selected ride'));
+      return Future.error(ArgumentError.notNull('ride'));
     }
 
-    await ref.read(rideRepositoryProvider).deleteRide(ride.value.date);
+    await ref.read(rideRepositoryProvider).deleteRide(ride.date);
 
     // Refresh the ride list.
     ref.refresh(rideListProvider);
+  }
+
+  void setSelectedRide(Ride ride) {
+    if (state != ride) {
+      state = ride;
+    }
   }
 }

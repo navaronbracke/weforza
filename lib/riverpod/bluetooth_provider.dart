e9483a09367d8ec0df7515c1dfcff015046fd943
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:22:38 +0200

    don't use an auto dispose provider

--- lib/riverpod/bluetooth_provider.dart
+++ lib/riverpod/bluetooth_provider.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/bluetooth/bluetooth_device_scanner.dart';
 
-/// This provider provides the bluetooth scanner.
-final bluetoothProvider = Provider.autoDispose<BluetoothDeviceScanner>((ref) {
+/// This provider provides the Bluetooth scanner.
+///
+/// This provider is not an `autoDispose` provider,
+/// as the Bluetooth scanner can be reused multiple times.
+/// It is disposed using [ProviderRef.onDispose] when the [ProviderContainer] is disposed.
+final bluetoothProvider = Provider<BluetoothDeviceScanner>((ref) {
   final scanner = BluetoothDeviceScannerImpl();
 
   ref.onDispose(scanner.dispose);

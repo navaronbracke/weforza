9a1878389c7dc0cb5e6e8bbc36681aa6608a37d5
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:21:30 +0200

    clean up exportable member

--- lib/model/exportable_member.dart
+++ lib/model/exportable_member.dart
@@ -1,38 +1,58 @@
 import 'package:weforza/extensions/date_extension.dart';
 
-/// This class is used as format for exporting members.
+/// This class represents an exportable member.
+///
+/// It is used for exporting members as well as importing members,
+/// since the format should be interchangeable.
 class ExportableMember {
+  /// The default constructor.
   ExportableMember({
+    required this.active,
+    required this.alias,
+    required this.devices,
     required this.firstName,
     required this.lastName,
-    required this.alias,
-    required this.isActiveMember,
-    required this.devices,
     required this.lastUpdated,
   }) : assert(firstName.isNotEmpty && lastName.isNotEmpty);
 
-  final bool isActiveMember;
-  final String firstName;
-  final String lastName;
+  /// Whether this member is an active member.
+  final bool active;
+
+  /// The alias of the member.
   final String alias;
+
+  /// The names of the devices that this member owns.
   final Set<String> devices;
+
+  /// The first name of the member.
+  final String firstName;
+
+  /// The last name of the member.
+  final String lastName;
+
+  /// The timestamp that indicates the last update of this member.
   final DateTime lastUpdated;
 
+  /// Convert this object to a comma separated string.
   String toCsv() {
-    final String devicesString = devices.isEmpty ? '' : devices.join(',');
+    final sb = StringBuffer('$firstName,$lastName,$alias,');
 
-    return '$firstName,$lastName,$alias,${isActiveMember ? 1 : 0},'
-        '${lastUpdated.toStringWithoutMilliseconds()},$devicesString';
+    sb.write('${active ? 1 : 0},');
+    sb.write('${lastUpdated.toStringWithoutMilliseconds()},');
+    sb.write(devices.isEmpty ? '' : devices.join(','));
+
+    return sb.toString();
   }
 
+  /// Convert this object to JSON.
   Map<String, Object?> toJson() {
     return {
+      'active': active,
+      'alias': alias,
+      'devices': devices.toList(),
       'firstName': firstName,
       'lastName': lastName,
-      'alias': alias,
-      'active': isActiveMember,
       'lastUpdated': lastUpdated.toStringWithoutMilliseconds(),
-      'devices': devices.toList(),
     };
   }
 }

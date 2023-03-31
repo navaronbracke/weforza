d701f78a5cb8560b4f18c3e1288cac578afe40e4
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:30:13 +0200

    Added isActiveMember flag to exportableMember.

--- weforza/lib/model/exportableMember.dart
+++ weforza/lib/model/exportableMember.dart
@@ -6,12 +6,15 @@ class ExportableMember {
     @required this.firstName,
     @required this.lastName,
     @required this.alias,
+    @required this.isActiveMember,
     @required this.devices,
   }): assert(
-    firstName != null && firstName.isNotEmpty && lastName != null &&
-        lastName.isNotEmpty && alias != null && devices != null
+    firstName != null && firstName.isNotEmpty
+        && lastName != null && lastName.isNotEmpty && alias != null
+        && devices != null && isActiveMember != null
   );
 
+  final bool isActiveMember;
   final String firstName;
   final String lastName;
   final String alias;
@@ -19,7 +22,7 @@ class ExportableMember {
 
   String toCsv(){
     final String devicesString = devices.isEmpty ? "" : ",${devices.join(",")}";
-    return "$firstName,$lastName,$alias$devicesString";
+    return "$firstName,$lastName,$alias,${isActiveMember ? 1: 0},$devicesString";
   }
 
   Map<String,dynamic> toJson(){
@@ -27,6 +30,7 @@ class ExportableMember {
       "firstName": firstName,
       "lastName": lastName,
       "alias": alias,
+      "active": isActiveMember,
       "devices": List<String>.of(devices)
     };
   }

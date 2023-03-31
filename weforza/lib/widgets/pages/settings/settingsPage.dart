93674da5d072731fc94aea32e31d0725ba0d53a2
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 13:36:05 +0200

    Update the settings page to use the reste calendar button widget.

--- weforza/lib/widgets/pages/settings/settingsPage.dart
+++ weforza/lib/widgets/pages/settings/settingsPage.dart
@@ -4,73 +4,75 @@ import 'package:flutter/cupertino.dart';
 import 'package:weforza/blocs/settingsBloc.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/injection/injector.dart';
+import 'package:weforza/model/settings.dart';
 import 'package:weforza/repository/settingsRepository.dart';
 import 'package:weforza/theme/appTheme.dart';
 import 'package:weforza/widgets/pages/settings/loadingSettings.dart';
+import 'package:weforza/widgets/pages/settings/resetRideCalendarButton.dart';
 import 'package:weforza/widgets/pages/settings/scanDurationOption.dart';
 import 'package:weforza/widgets/pages/settings/settingsPageGenericError.dart';
 import 'package:weforza/widgets/pages/settings/settingsSubmit.dart';
 import 'package:weforza/widgets/platform/platformAwareWidget.dart';
 
-
 class SettingsPage extends StatefulWidget {
   @override
   _SettingsPageState createState() => _SettingsPageState(
-      SettingsBloc(InjectionContainer.get<SettingsRepository>())
-  );
+        SettingsBloc(
+          InjectionContainer.get<SettingsRepository>(),
+        ),
+      );
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  _SettingsPageState(this.bloc): assert(bloc != null);
+  _SettingsPageState(this.bloc) : assert(bloc != null);
 
   final SettingsBloc bloc;
 
   @override
-  void initState() {
-    super.initState();
-    bloc.loadSettings();
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return FutureBuilder<void>(
-      future: bloc.settingsFuture,
-      builder: (context, snapshot){
-        if(snapshot.connectionState == ConnectionState.done){
-          if(snapshot.hasError){
+  Widget build(BuildContext context) {
+    return FutureBuilder<Settings>(
+      // We have to reload the future on each build.
+      // Sadly enough we don't have any other options.
+      // There is no global store tat could have helped :/
+      // But this widget doesn't call it's setState method.
+      // Thus we don't get any unnecessary build calls for the FutureBuilder.
+      //
+      // We put an artificial delay here to decrease the feeling of popping in.
+      // See https://www.youtube.com/watch?v=O6ZQ9r8a3iw
+      future: Future.delayed(Duration(seconds: 1), () => bloc.loadSettings()),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
             return SettingsPageGenericError();
-          }else{
+          } else {
             return PlatformAwareWidget(
-              android: () => _buildAndroidWidget(context),
-              ios: () => _buildIosWidget(context),
+              android: () => _buildAndroidWidget(context, snapshot.data),
+              ios: () => _buildIosWidget(context, snapshot.data),
             );
           }
-        }else{
+        } else {
           return LoadingSettings();
         }
       },
     );
   }
 
-  Widget _buildAndroidWidget(BuildContext context){
+  Widget _buildAndroidWidget(BuildContext context, Settings settings) {
     return Scaffold(
       appBar: AppBar(
         title: Text(S.of(context).Settings),
         actions: <Widget>[
           SettingsSubmit(
             submitStream: bloc.submitStream,
-            onSubmit: () async {
-              await bloc.saveSettings();
-              setState(() {});
-            },
+            onSubmit: bloc.saveSettings,
           ),
         ],
       ),
-      body: _buildBody(context),
+      body: _buildBody(context, settings),
     );
   }
 
-  Widget _buildIosWidget(BuildContext context){
+  Widget _buildIosWidget(BuildContext context, Settings settings) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Row(
@@ -86,10 +88,7 @@ class _SettingsPageState extends State<SettingsPage> {
               child: Center(
                 child: SettingsSubmit(
                   submitStream: bloc.submitStream,
-                  onSubmit: () async {
-                    await bloc.saveSettings();
-                    setState(() {});
-                  },
+                  onSubmit: bloc.saveSettings,
                 ),
               ),
             ),
@@ -98,12 +97,12 @@ class _SettingsPageState extends State<SettingsPage> {
         transitionBetweenRoutes: false,
       ),
       child: SafeArea(
-        child: _buildBody(context),
+        child: _buildBody(context, settings),
       ),
     );
   }
 
-  Widget _buildBody(BuildContext context){
+  Widget _buildBody(BuildContext context, Settings settings) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
@@ -112,35 +111,41 @@ class _SettingsPageState extends State<SettingsPage> {
             child: SingleChildScrollView(
               child: Column(
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: ScanDurationOption(
-                      getValue: () => bloc.scanDuration,
-                      maxScanValue: bloc.maxScanValue,
-                      minScanValue: bloc.minScanValue,
-                      onChanged: bloc.onScanDurationChanged,
-                    ),
+                  ScanDurationOption(
+                    getValue: () => bloc.scanDuration,
+                    maxScanValue: bloc.maxScanValue,
+                    minScanValue: bloc.minScanValue,
+                    onChanged: bloc.onScanDurationChanged,
                   ),
                 ],
               ),
             ),
           ),
-          _buildApplicationPackageInfo()
+          if (settings.hasRideCalendar) ResetRideCalendarButton(),
+          _buildApplicationPackageInfo(
+            context,
+            settings.packageInfo.version,
+            settings.packageInfo.buildNumber,
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildApplicationPackageInfo(){
+  Widget _buildApplicationPackageInfo(
+    BuildContext context,
+    String version,
+    String buildNumber,
+  ) {
     return Column(
       children: [
         Text(
-          S.of(context).AppVersionNumber(bloc.appVersion),
-          style:  ApplicationTheme.appVersionTextStyle,
+          S.of(context).AppVersionNumber(version),
+          style: ApplicationTheme.appVersionTextStyle,
         ),
         Text(
-          S.of(context).AppVersionBuildNumber(bloc.appBuildNumber),
-          style:  ApplicationTheme.appVersionTextStyle,
+          S.of(context).AppVersionBuildNumber(buildNumber),
+          style: ApplicationTheme.appVersionTextStyle,
         ),
       ],
     );
@@ -152,5 +157,3 @@ class _SettingsPageState extends State<SettingsPage> {
     super.dispose();
   }
 }
-
-

6248787eaabf9d3f8d8764c8704d41be7a0e051a
Navaron Bracke <brackenavaron@gmail.com>
2023-03-31 14:10:40 +0200

    refactor the ride attendee scanning page

--- lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_page.dart
+++ lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_page.dart
@@ -1,67 +1,149 @@
-import 'package:app_settings/app_settings.dart';
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:weforza/blocs/attendee_scanning_bloc.dart';
-import 'package:weforza/bluetooth/bluetooth_device_scanner.dart';
-import 'package:weforza/file/file_handler.dart';
-import 'package:weforza/model/member.dart';
-import 'package:weforza/model/ride_attendee_scan_result.dart';
-import 'package:weforza/model/scan_process_step.dart';
-import 'package:weforza/repository/device_repository.dart';
-import 'package:weforza/repository/member_repository.dart';
-import 'package:weforza/repository/ride_repository.dart';
-import 'package:weforza/repository/settings_repository.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/bluetooth_disabled.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weforza/model/ride_attendee_scanning/ride_attendee_scanning_delegate.dart';
+import 'package:weforza/model/ride_attendee_scanning/ride_attendee_scanning_state.dart';
+import 'package:weforza/riverpod/bluetooth_provider.dart';
+import 'package:weforza/riverpod/repository/device_repository_provider.dart';
+import 'package:weforza/riverpod/repository/member_repository_provider.dart';
+import 'package:weforza/riverpod/repository/ride_repository_provider.dart';
+import 'package:weforza/riverpod/settings_provider.dart';
 import 'package:weforza/widgets/pages/ride_attendee_scanning_page/generic_scan_error.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/manual_selection/manual_selection.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/manual_selection/manual_selection_list_item.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/manual_selection/manual_selection_submit.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/preparing_scan.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_progress_indicator.dart';
+import 'package:weforza/widgets/pages/ride_attendee_scanning_page/manual_selection_list/manual_selection_list.dart';
 import 'package:weforza/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_stepper.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/scan_permission_denied.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/scan_result_list_item/scan_result_multiple_possible_owners_list_item.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/scan_result_list_item/scan_result_single_owner_list_item.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/scan_result_list_item/scan_result_unknown_device_list_item.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/skip_scan_button.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/unresolved_owners_list/unresolved_owners_list.dart';
-import 'package:weforza/widgets/pages/ride_attendee_scanning_page/unresolved_owners_list/unresolved_owners_list_item.dart';
+import 'package:weforza/widgets/pages/ride_attendee_scanning_page/scan_progress_indicator.dart';
+import 'package:weforza/widgets/pages/ride_attendee_scanning_page/scan_results_list.dart';
+import 'package:weforza/widgets/pages/ride_attendee_scanning_page/unresolved_owners_list.dart';
 import 'package:weforza/widgets/platform/platform_aware_loading_indicator.dart';
 import 'package:weforza/widgets/platform/platform_aware_widget.dart';
-import 'package:weforza/widgets/providers/selectedItemProvider.dart';
 
-class RideAttendeeScanningPage extends StatefulWidget {
-  const RideAttendeeScanningPage({
-    Key? key,
-    required this.onRefreshAttendees,
-    required this.fileHandler,
-  }) : super(key: key);
-
-  final void Function() onRefreshAttendees;
-  final IFileHandler fileHandler;
+class RideAttendeeScanningPage extends ConsumerStatefulWidget {
+  const RideAttendeeScanningPage({super.key});
 
   @override
-  _RideAttendeeScanningPageState createState() =>
-      _RideAttendeeScanningPageState();
+  RideAttendeeScanningPageState createState() =>
+      RideAttendeeScanningPageState();
 }
 
-class _RideAttendeeScanningPageState extends State<RideAttendeeScanningPage> {
-  late AttendeeScanningBloc bloc;
+class RideAttendeeScanningPageState
+    extends ConsumerState<RideAttendeeScanningPage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _scanProgressBarController;
 
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
+  final GlobalKey<AnimatedListState> _scanResultsKey = GlobalKey();
+
+  StreamSubscription<bool>? _startScanningSubscription;
+
+  late final RideAttendeeScanningDelegate delegate;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    bloc = AttendeeScanningBloc(
-      ride: SelectedItemProvider.of(context).selectedRide.value!,
-      settingsRepo: InjectionContainer.get<SettingsRepository>(),
-      deviceRepo: InjectionContainer.get<DeviceRepository>(),
-      memberRepo: InjectionContainer.get<MemberRepository>(),
-      scanner: InjectionContainer.get<BluetoothDeviceScanner>(),
-      ridesRepo: InjectionContainer.get<RideRepository>(),
+  void initState() {
+    super.initState();
+
+    delegate = RideAttendeeScanningDelegate(
+      deviceRepository: ref.read(deviceRepositoryProvider),
+      memberRepository: ref.read(memberRepositoryProvider),
+      // The delegate will have added the device to the scan results,
+      // the only thing left to do is to inset it into the animated list.
+      onDeviceFound: (device) => _scanResultsKey.currentState?.insertItem(0),
+      ref: ref,
+      rideRepository: ref.read(rideRepositoryProvider),
+      scanner: ref.read(bluetoothProvider),
+      settings: ref.read(settingsProvider),
     );
-    bloc.scanFuture = bloc.startDeviceScan(_onDeviceFound);
+
+    _scanProgressBarController = AnimationController(
+      duration: Duration(seconds: delegate.settings.scanDuration),
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 0.0,
+    );
+
+    // Setup a subscription that listens to the start of the device scan
+    // and starts the progress animation.
+    _startScanningSubscription = delegate.scanner.isScanning.listen((value) {
+      // If the subscription is null, the stream is or will be disposed.
+      if (_startScanningSubscription == null || !value) {
+        return;
+      }
+
+      // Start the animation when the scan stream emits the event
+      // that indicates that the scan started.
+      if (_scanProgressBarController.status == AnimationStatus.dismissed) {
+        _scanProgressBarController.forward();
+      }
+    });
+
+    delegate.startDeviceScan();
+  }
+
+  Widget _buildAndroidLayout(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: RideAttendeeScanningStepper(stream: delegate.stateStream),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return StreamBuilder<RideAttendeeScanningState>(
+      stream: delegate.stateStream,
+      builder: (context, snapshot) {
+        final state = snapshot.data;
+
+        if (snapshot.hasError || state == null) {
+          return const Center(child: GenericScanError());
+        }
+
+        switch (state) {
+          case RideAttendeeScanningState.bluetoothDisabled:
+            return Center(
+              child: BluetoothDisabledError(onRetry: delegate.startDeviceScan),
+            );
+          case RideAttendeeScanningState.manualSelection:
+            return ManualSelectionList(delegate: delegate);
+          case RideAttendeeScanningState.permissionDenied:
+            return const Center(child: PermissionDeniedError());
+          case RideAttendeeScanningState.requestPermissions:
+          case RideAttendeeScanningState.startingScan:
+          case RideAttendeeScanningState.stoppingScan:
+            return const Center(child: PlatformAwareLoadingIndicator());
+          case RideAttendeeScanningState.scanning:
+            return ScanResultsList(
+              delegate: delegate,
+              progressBar: ScanProgressIndicator(
+                animationController: _scanProgressBarController,
+                isScanning: delegate.scanner.isScanning,
+              ),
+              scanResultsListKey: _scanResultsKey,
+            );
+          case RideAttendeeScanningState.unresolvedOwnersSelection:
+            return UnresolvedOwnersList(delegate: delegate);
+        }
+      },
+    );
+  }
+
+  Widget _buildIOSLayout(BuildContext context) {
+    return CupertinoPageScaffold(
+      resizeToAvoidBottomInset: false,
+      navigationBar: CupertinoNavigationBar(
+        transitionBetweenRoutes: false,
+        automaticallyImplyLeading: false,
+        middle: RideAttendeeScanningStepper(stream: delegate.stateStream),
+      ),
+      child: SafeArea(
+        // The bottom view padding should be preserved
+        // for the manual selection save widget,
+        bottom: false,
+        child: _buildBody(),
+      ),
+    );
   }
 
   @override
@@ -72,202 +154,12 @@ class _RideAttendeeScanningPageState extends State<RideAttendeeScanningPage> {
     );
   }
 
-  Widget _buildAndroidLayout(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: RideAttendeeScanningStepper(
-            stream: bloc.isScanStep,
-          ),
-        ),
-        body: _buildBody());
-  }
-
-  Widget _buildIOSLayout(BuildContext context) {
-    return CupertinoPageScaffold(
-      resizeToAvoidBottomInset: false,
-      navigationBar: CupertinoNavigationBar(
-        transitionBetweenRoutes: false,
-        automaticallyImplyLeading: false,
-        middle: RideAttendeeScanningStepper(
-          stream: bloc.isScanStep,
-        ),
-      ),
-      child: SafeArea(
-        child: _buildBody(),
-        // We need the bottom view padding for the manual selection save widget.
-        bottom: false,
-      ),
-    );
-  }
-
-  Widget _buildBody() {
-    return StreamBuilder<ScanProcessStep>(
-      initialData: ScanProcessStep.init,
-      stream: bloc.scanStepStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(child: GenericScanErrorWidget());
-        } else {
-          switch (snapshot.data) {
-            case ScanProcessStep.init:
-              return const Center(child: PreparingScanWidget());
-            case ScanProcessStep.bluetoothDisabled:
-              return Center(
-                child: BluetoothDisabledWidget(
-                  onGoToSettings: () async =>
-                      await AppSettings.openBluetoothSettings(),
-                  onRetryScan: () =>
-                      bloc.scanFuture = bloc.startDeviceScan(_onDeviceFound),
-                ),
-              );
-            case ScanProcessStep.scan:
-              return WillPopScope(
-                onWillPop: () => bloc.stopScan(),
-                child: Column(
-                  children: <Widget>[
-                    RideAttendeeScanningProgressIndicator(
-                      isScanning: bloc.scanning,
-                      getDuration: () => bloc.scanDuration,
-                    ),
-                    Expanded(
-                      child: AnimatedList(
-                        key: _listKey,
-                        itemBuilder: (context, index, animation) {
-                          final String deviceName = bloc.getScanResultAt(index);
-                          final List<Member> owners =
-                              bloc.getDeviceOwners(deviceName);
-
-                          return SizeTransition(
-                            sizeFactor: animation,
-                            child: _buildScanResultListItem(deviceName, owners),
-                          );
-                        },
-                      ),
-                    ),
-                    SkipScanButton(
-                      isScanning: bloc.scanning,
-                      onSkip: () => bloc.skipScan(),
-                      onContinue: () => bloc.continueToUnresolvedOwnersList(),
-                    ),
-                  ],
-                ),
-              );
-            case ScanProcessStep.manual:
-              return RideAttendeeManualSelection(
-                isMemberScanned: bloc.isMemberScanned,
-                activeMembersFuture: bloc.loadActiveMembers(),
-                itemBuilder: _buildManualSelectionListItem,
-                saveButtonBuilder: _buildSaveButton,
-                query: bloc.searchQuery,
-                showScanned: bloc.showScanned,
-                onShowScannedChanged: bloc.onShowScannedChanged,
-                onQueryChanged: bloc.onQueryChanged,
-              );
-            case ScanProcessStep.stoppingScan:
-              return const Center(child: PlatformAwareLoadingIndicator());
-            case ScanProcessStep.permissionDenied:
-              return const Center(child: ScanPermissionDenied());
-            case ScanProcessStep.resolveMultipleOwners:
-              return Center(
-                child: UnresolvedOwnersList(
-                  future: bloc.filterAndSortMultipleOwnersList(),
-                  itemBuilder: (Member member) {
-                    // For UnresolvedOwnersListItem's isScanned is false anyway.
-                    final attendeeScanResult = RideAttendeeScanResult(
-                        uuid: member.uuid, isScanned: false);
-
-                    return UnresolvedOwnersListItem(
-                      firstName: member.firstname,
-                      lastName: member.lastname,
-                      alias: member.alias,
-                      isSelected: () => bloc.isItemSelected(attendeeScanResult),
-                      // We should not show dialogs on this screen.
-                      // We have to select manually anyway.
-                      onTap: () =>
-                          bloc.onMemberSelectedWithoutConfirmationDialog(
-                              attendeeScanResult),
-                    );
-                  },
-                  onButtonPressed: () => bloc.continueToManualSelection(),
-                ),
-              );
-            default:
-              return const Center(child: GenericScanErrorWidget());
-          }
-        }
-      },
-    );
-  }
-
-  ///Trigger an insertion for a new item in the AnimatedList.
-  void _onDeviceFound(String deviceName) {
-    if (_listKey.currentState != null) {
-      bloc.addAutomaticScanResult(deviceName);
-      _listKey.currentState!.insertItem(0);
-    }
-  }
-
-  Future<void> _onSaveScanResults(BuildContext context) async {
-    // Errors from this future are caught
-    // by the FutureBuilder that consumes this method.
-    return await bloc.saveRideAttendees().then((_) {
-      widget.onRefreshAttendees();
-      Navigator.of(context).pop();
-    });
-  }
-
-  Widget _buildSaveButton(BuildContext context) {
-    return ManualSelectionSubmit(
-      attendeeCount: bloc.attendeeCount,
-      initialAttendeeCount: bloc.rideAttendeeCount,
-      showScanned: bloc.showScanned,
-      onShowScannedChanged: bloc.onShowScannedChanged,
-      save: () => _onSaveScanResults(context),
-    );
-  }
-
-  Widget _buildManualSelectionListItem(Member item, bool isScanned) {
-    return ManualSelectionListItem(
-      profileImageFuture: widget.fileHandler
-          .loadProfileImageFromDisk(item.profileImageFilePath),
-      firstName: item.firstname,
-      lastName: item.lastname,
-      alias: item.alias,
-      personInitials: item.initials,
-      isScanned: () => bloc.isMemberScanned(item.uuid),
-      isSelected: () => bloc.isItemSelected(
-        RideAttendeeScanResult(
-          uuid: item.uuid,
-          isScanned: isScanned,
-        ),
-      ),
-      uuid: item.uuid,
-      canTap: () => bloc.canSelectMember,
-      addScanResult: bloc.addManualScanResult,
-      removeScanResult: bloc.removeScanResult,
-      scanResultExists: bloc.scanResultExists,
-    );
-  }
-
-  Widget _buildScanResultListItem(String deviceName, List<Member> owners) {
-    if (owners.isEmpty) {
-      return ScanResultUnknownDeviceListItem(deviceName: deviceName);
-    }
-    if (owners.length == 1) {
-      return ScanResultSingleOwnerListItem(owner: owners.first);
-    }
-
-    return ScanResultMultiplePossibleOwnersListItem(
-      deviceName: deviceName,
-      amountOfPossibleOwners: owners.length,
-    );
-  }
-
   @override
   void dispose() {
-    bloc.dispose();
+    _startScanningSubscription?.cancel();
+    _startScanningSubscription = null;
+    _scanProgressBarController.dispose();
+    delegate.dispose();
     super.dispose();
   }
 }

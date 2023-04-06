61101e5d5ffdef6bd656131ac4eda7f1bd280257
Navaron Bracke <brackenavaron@gmail.com>
2023-04-06 12:02:28 +0200

    show just the current label

--- lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_stepper.dart
+++ lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_stepper.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/ride_attendee_scanning/ride_attendee_scanning_state.dart';
-import 'package:weforza/widgets/platform/platform_aware_widget.dart';
 
 /// This widget represents the stepper at the top of the scanning page.
 /// It displays the two labels of the scanning process,
@@ -25,17 +23,6 @@ class RideAttendeeScanningStepper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final translator = S.of(context);
-    final scanLabel = translator.scan.toUpperCase();
-    final manualLabel = translator.manual.toUpperCase();
-
-    final separator = PlatformAwareWidget(
-      android: (_) => const Icon(Icons.chevron_right_rounded, size: 48),
-      ios: (_) => const Icon(
-        CupertinoIcons.chevron_forward,
-        color: CupertinoColors.inactiveGray,
-        size: 32,
-      ),
-    );
 
     return StreamBuilder<RideAttendeeScanningState>(
       initialData: initialData,
@@ -44,60 +31,8 @@ class RideAttendeeScanningStepper extends StatelessWidget {
         // Every step before the manual selection should highlight the `Scan` label.
         final isScanStep = snapshot.data != RideAttendeeScanningState.manualSelection;
 
-        return Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _RideAttendeeScanningStepperLabel(
-              isSelected: isScanStep,
-              text: scanLabel,
-            ),
-            separator,
-            _RideAttendeeScanningStepperLabel(
-              isSelected: !isScanStep,
-              text: manualLabel,
-            ),
-          ],
-        );
+        return Text(isScanStep ? translator.scan : translator.manual);
       },
-    );
-  }
-}
-
-class _RideAttendeeScanningStepperLabel extends StatelessWidget {
-  const _RideAttendeeScanningStepperLabel({
-    required this.isSelected,
-    required this.text,
-  });
-
-  /// Whether this label is selected.
-  final bool isSelected;
-
-  /// The text for the label.
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    Color selectedColor;
-    Color? unselectedColor;
-
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-      case TargetPlatform.fuchsia:
-      case TargetPlatform.linux:
-      case TargetPlatform.windows:
-        selectedColor = Theme.of(context).colorScheme.primary;
-        unselectedColor = Theme.of(context).colorScheme.onSurface.withOpacity(0.5);
-        break;
-      case TargetPlatform.iOS:
-      case TargetPlatform.macOS:
-        selectedColor = CupertinoColors.activeGreen;
-        unselectedColor = CupertinoColors.inactiveGray;
-        break;
-    }
-
-    return Text(
-      text,
-      style: TextStyle(color: isSelected ? selectedColor : unselectedColor),
     );
   }
 }

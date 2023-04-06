9430dd77298640cfc371df16f249536d2267b4d4
Navaron Bracke <brackenavaron@gmail.com>
2023-04-06 12:05:31 +0200

    fix doc

--- lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_stepper.dart
+++ lib/widgets/pages/ride_attendee_scanning_page/ride_attendee_scanning_stepper.dart
@@ -4,8 +4,6 @@ import 'package:weforza/generated/l10n.dart';
 import 'package:weforza/model/ride_attendee_scanning/ride_attendee_scanning_state.dart';
 
 /// This widget represents the stepper at the top of the scanning page.
-/// It displays the two labels of the scanning process,
-/// `Scan` and `Manual`, separated by an arrow.
 class RideAttendeeScanningStepper extends StatelessWidget {
   /// The default constructor.
   const RideAttendeeScanningStepper({
@@ -28,7 +26,7 @@ class RideAttendeeScanningStepper extends StatelessWidget {
       initialData: initialData,
       stream: stream,
       builder: (context, snapshot) {
-        // Every step before the manual selection should highlight the `Scan` label.
+        // Every step before the manual selection should show the `Scan` label.
         final isScanStep = snapshot.data != RideAttendeeScanningState.manualSelection;
 
         return Text(isScanStep ? translator.scan : translator.manual);

d0f66207efa860481d7a765d65f429c4d8cbae83
Navaron Bracke <brackenavaron@gmail.com>
2023-05-18 12:10:48 +0200

    remove the export directory provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
-import 'dart:io';
-
 import 'package:file/local.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:weforza/database/database.dart';
 import 'package:weforza/database/database_factory.dart';
 import 'package:weforza/database/settings_dao.dart';
 import 'package:weforza/repository/settings_repository.dart';
 import 'package:weforza/riverpod/database/database_provider.dart';
-import 'package:weforza/riverpod/file_handler_provider.dart';
 import 'package:weforza/riverpod/package_info_provider.dart';
 import 'package:weforza/riverpod/settings_provider.dart';
 import 'package:weforza/widgets/app.dart';
@@ -37,14 +33,6 @@ void main() async {
   // Preload the package info.
   final packageInfo = await PackageInfo.fromPlatform();
 
-  Directory? defaultExportDirectory;
-
-  // On iOS, the Documents directory within the application
-  // is a good default for the export directory.
-  if (Platform.isIOS) {
-    defaultExportDirectory = await getApplicationDocumentsDirectory();
-  }
-
   runApp(
     ProviderScope(
       overrides: [
@@ -55,10 +43,6 @@ void main() async {
         // Inject the preloaded settings.
         settingsProvider.overrideWith(
           (ref) => SettingsNotifier(settings, settingsRepository),
-        ),
-        // Inject the default directory for file exports.
-        exportDataDefaultDirectoryProvider.overrideWithValue(
-          defaultExportDirectory,
         ),
       ],
       child: const WeForzaApp(),
--- lib/riverpod/file_handler_provider.dart
+++ lib/riverpod/file_handler_provider.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/file/file_handler.dart';
 
 /// This provider provides the file handler.
-final fileHandlerProvider = Provider<FileHandler>((_) => IoFileHandler());
-
-/// This provider provides the default [Directory] for export files.
-final exportDataDefaultDirectoryProvider = Provider<Directory?>((_) => null);
+final fileHandlerProvider = Provider<FileHandler>((_) => const IoFileHandler());

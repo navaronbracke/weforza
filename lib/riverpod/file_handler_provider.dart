00e0867f97fab8054d6d41cdf354785b5fb964e8
Navaron Bracke <brackenavaron@gmail.com>
2023-05-18 14:05:20 +0200

    let the file handler consume the directories

--- lib/riverpod/file_handler_provider.dart
+++ lib/riverpod/file_handler_provider.dart
@@ -2,4 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weforza/file/file_handler.dart';
 
 /// This provider provides the file handler.
-final fileHandlerProvider = Provider<FileHandler>((_) => const IoFileHandler());
+final fileHandlerProvider = Provider<FileHandler>((ref) => IoFileHandler(ref.read(fileHandlerDirectoriesProvider)));
+
+/// This provider provides the [FileHandlerDirectories] for the [FileHandler].
+final fileHandlerDirectoriesProvider = Provider<FileHandlerDirectories>(
+  (_) => throw UnimplementedError('This implementation is a stub. Instead, preload the directories on startup.'),
+);

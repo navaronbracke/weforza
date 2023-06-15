fb3c5b97bfdb5910274e8679fb4a45b85cb683bd
Navaron Bracke <brackenavaron@gmail.com>
2023-06-15 11:44:46 +0200

    update dao provider to use the getters

--- lib/riverpod/database/database_dao_provider.dart
+++ lib/riverpod/database/database_dao_provider.dart
@@ -8,43 +8,19 @@ import 'package:weforza/database/settings_dao.dart';
 import 'package:weforza/riverpod/database/database_provider.dart';
 
 /// This provider provides the device dao.
-final deviceDaoProvider = Provider<DeviceDao>((ref) {
-  final database = ref.read(databaseProvider);
-
-  return DeviceDaoImpl(database.getDatabase());
-});
+final deviceDaoProvider = Provider<DeviceDao>((ref) => ref.read(databaseProvider).deviceDao);
 
 /// This provider provides the export rides dao.
-final exportRidesDaoProvider = Provider<ExportRidesDao>((ref) {
-  final database = ref.read(databaseProvider);
-
-  return ExportRidesDaoImpl(database.getDatabase());
-});
+final exportRidesDaoProvider = Provider<ExportRidesDao>((ref) => ref.read(databaseProvider).exportRidesDao);
 
 /// This provider provides the import riders dao.
-final importRidersDaoProvider = Provider<ImportRidersDao>((ref) {
-  final database = ref.read(databaseProvider);
-
-  return ImportRidersDaoImpl(database.getDatabase());
-});
+final importRidersDaoProvider = Provider<ImportRidersDao>((ref) => ref.read(databaseProvider).importRidersDao);
 
 /// This provider provides the rider dao.
-final riderDaoProvider = Provider<RiderDao>((ref) {
-  final database = ref.read(databaseProvider);
-
-  return RiderDaoImpl(database.getDatabase());
-});
+final riderDaoProvider = Provider<RiderDao>((ref) => ref.read(databaseProvider).riderDao);
 
 /// This provider provides the ride dao.
-final rideDaoProvider = Provider<RideDao>((ref) {
-  final database = ref.read(databaseProvider);
-
-  return RideDaoImpl(database.getDatabase());
-});
+final rideDaoProvider = Provider<RideDao>((ref) => ref.read(databaseProvider).rideDao);
 
 /// This provider provides the application settings dao.
-final settingsDaoProvider = Provider<SettingsDao>((ref) {
-  final database = ref.read(databaseProvider);
-
-  return SettingsDaoImpl(database.getDatabase());
-});
+final settingsDaoProvider = Provider<SettingsDao>((ref) => ref.read(databaseProvider).settingsDao);
--- lib/riverpod/database/database_provider.dart
+++ lib/riverpod/database/database_provider.dart
@@ -4,6 +4,6 @@ import 'package:weforza/database/database.dart';
 /// This provider provides the application database.
 ///
 /// This provider should be overridden with the opened database at application startup.
-final databaseProvider = Provider<ApplicationDatabase>((ref) {
+final databaseProvider = Provider<Database>((ref) {
   throw UnsupportedError('The application database should be preloaded at startup.');
 });

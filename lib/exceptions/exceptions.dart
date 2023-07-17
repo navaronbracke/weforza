a572914279ac48a2deca26c48a826cf2d60e04cd
Navaron Bracke <brackenavaron@gmail.com>
2023-07-17 18:58:12 +0200

    add camera denied exception; sort exceptions

--- lib/exceptions/exceptions.dart
+++ lib/exceptions/exceptions.dart
@@ -1,8 +1,19 @@
+/// An exception that is thrown when access to the camera is denied.
+class CameraPermissionDeniedException implements Exception {}
+
 /// An exception that is thrown when a device already exists.
 class DeviceExistsException implements Exception {}
 
+/// An exception that is thrown when permission to read or write to external storage is denied.
+///
+/// This permission is typically only used on Android 9 and lower.
+class ExternalStoragePermissionDeniedException implements Exception {}
+
 /// An exception that is thrown when a file is required, but it was not provided.
 class FileRequiredException implements Exception {}
+
+/// An exception that is thrown when access to the Photo library is denied.
+class PhotoLibraryPermissionDeniedException implements Exception {}
 
 /// An exception that is thrown when a rider already exists.
 class RiderExistsException implements Exception {}
@@ -20,11 +31,3 @@ class StopScanException implements Exception {}
 /// An exception that is thrown when a [File] was provided,
 /// but the given file format is not supported.
 class UnsupportedFileFormatException implements Exception {}
-
-/// An exception that is thrown when permission to read or write to external storage is denied.
-///
-/// This permission is typically only used on Android 9 and lower.
-class ExternalStoragePermissionDeniedException implements Exception {}
-
-/// An exception that is thrown when access to the Photo library is denied.
-class PhotoLibraryPermissionDeniedException implements Exception {}

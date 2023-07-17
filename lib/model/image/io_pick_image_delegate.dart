fae729f557a8e652f6688c592610a4f92b1607a4
Navaron Bracke <brackenavaron@gmail.com>
2023-07-17 17:44:03 +0200

    implement requesting permissions before taking a photo

--- lib/model/image/io_pick_image_delegate.dart
+++ lib/model/image/io_pick_image_delegate.dart
@@ -2,15 +2,19 @@ import 'dart:io' show Platform;
 
 import 'package:file/file.dart' as fs;
 import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
 import 'package:weforza/exceptions/exceptions.dart';
 import 'package:weforza/file/file_system.dart';
 import 'package:weforza/model/image/pick_image_delegate.dart';
 import 'package:weforza/native_service/file_storage_delegate.dart';
+import 'package:weforza/native_service/media_permissions_service.dart';
 
 /// The [FileSystem] based implementation of [PickImageDelegate].
 class IoPickImageDelegate implements PickImageDelegate {
-  IoPickImageDelegate({required this.fileStorageDelegate, required this.fileSystem});
+  IoPickImageDelegate({
+    required this.fileStorageDelegate,
+    required this.fileSystem,
+    required this.mediaPermissionsDelegate,
+  });
 
   final FileStorageDelegate fileStorageDelegate;
 
@@ -18,14 +22,36 @@ class IoPickImageDelegate implements PickImageDelegate {
 
   final ImagePicker imagePicker = ImagePicker();
 
+  final MediaPermissionsService mediaPermissionsDelegate;
+
   /// Request permission to use the camera and the relevant photo library related permissions.
   ///
-  /// On Android 9 and lower this requests permission to write to external storage.
+  /// On Android 9 and lower this requests permission to read/write to external storage.
   /// On iOS this requests permission to add a photo to the Photo library.
   ///
   /// If any permission is not granted, this returns a [Future.error].
   Future<void> _requestCameraAndPhotoLibraryPermissions() async {
-    // TODO
+    if (Platform.isAndroid || Platform.isIOS) {
+      await mediaPermissionsDelegate.requestCameraPermission();
+    }
+
+    if (Platform.isAndroid && !fileSystem.hasScopedStorage) {
+      // Request both read and write permission of the external storage.
+      // The write permission is required to save the image, while the read permission is required
+      // to view the image later, after it was saved.
+      final bool hasExternalStoragePermission = await fileStorageDelegate.requestExternalStoragePermission(
+        read: true,
+        write: true,
+      );
+
+      if (!hasExternalStoragePermission) {
+        throw ExternalStoragePermissionDeniedException();
+      }
+    }
+
+    if (Platform.isIOS) {
+      await mediaPermissionsDelegate.requestAddToPhotoLibraryPermission();
+    }
   }
 
   /// Take a new photo with the camera of the device.

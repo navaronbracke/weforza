e866c8e75cdd593d27aee14f3f8ebf16ff9eb745
Navaron Bracke <brackenavaron@gmail.com>
2023-07-18 12:55:19 +0200

    move requestCameraPermission up in file; fix compilation issues

--- ios/Runner/MediaDelegate.swift
+++ ios/Runner/MediaDelegate.swift
@@ -11,42 +11,46 @@ import Foundation
 import Photos
 
 class MediaDelegate : NSObject {
-    /// Request permission to access the Photo library.
-    func requestPhotosLibraryPermission(result: @escaping FlutterResult) {
-        let photosAuthorizationStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
-        
-        switch(photosAuthorizationStatus) {
-        case .authorized:
-            result(true)
-        case .denied, .limited, .restricted:
-            result(false)
-        case .notDetermined:
-            PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
-                switch(status) {
-                case .authorized:
-                    result(true)
-                case .denied, .limited, .restricted:
-                    result(false)
-                case .notDetermined:
-                    result(nil)
-                }
-            }
-        }
-    }
-    
     /// Request permission to use the camera.
     func requestCameraPermission(result: @escaping FlutterResult) {
         let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
         
         switch(cameraAuthorizationStatus) {
         case .notDetermined:
-            AVCaptureDevice.requestAccess(for: .video) { let granted
+            AVCaptureDevice.requestAccess(for: .video) { granted in
                 result(granted)
             }
         case .authorized:
             result(true)
-        case .denied .restricted:
+        case .denied, .restricted:
             result(false)
+        }
+    }
+    
+    /// Request permission to access the Photo library.
+    func requestPhotosLibraryPermission(result: @escaping FlutterResult) {
+        if #available(iOS 14.0, *) {
+            let photosAuthorizationStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+            
+            switch(photosAuthorizationStatus) {
+            case .authorized:
+                result(true)
+            case .denied, .limited, .restricted:
+                result(false)
+            case .notDetermined:
+                PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+                    switch(status) {
+                    case .authorized:
+                        result(true)
+                    case .denied, .limited, .restricted:
+                        result(false)
+                    case .notDetermined:
+                        result(nil)
+                    }
+                }
+            }
+        } else {
+            result(true)
         }
     }
 }

e9760632e92538a9e4ccdbd53bb1d8f7b4df1bc5
Navaron Bracke <brackenavaron@gmail.com>
2023-07-23 15:44:00 +0200

    implement content uri image provider

---
+++ lib/file/content_uri_image_provider.dart
@@ -0,0 +1,72 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:weforza/native_service/file_storage_delegate.dart';
+
+/// Decodes the given `content://` [Uri] as an image,
+/// associating it with the given scale.
+@immutable
+final class ContentUriImage extends ImageProvider<ContentUriImage> {
+  /// Creates a new [ContentUriImage] from the given [uri].
+  ContentUriImage({
+    required this.fileStorageDelegate,
+    required this.uri,
+    this.scale = 1.0,
+  }) : assert(uri.isScheme('content'), "Only 'content://' Uris are supported.");
+
+  /// The delegate that will load the image.
+  final FileStorageDelegate fileStorageDelegate;
+
+  /// The scale to place in the [ImageInfo] object of the image.
+  final double scale;
+
+  /// The content [Uri] to load.
+  final Uri uri;
+
+  Future<Codec> _loadAsync(ContentUriImage key, {required ImageDecoderCallback decode}) async {
+    assert(key == this, 'ContentUriImage key is not equal to the caller object.');
+
+    final Uint8List bytes = await fileStorageDelegate.getBytesFromContentUri(key.uri);
+
+    if (bytes.isEmpty) {
+      PaintingBinding.instance.imageCache.evict(key);
+
+      throw StateError('The image at the given Uri "$uri" is empty and cannot be loaded as an image.');
+    }
+
+    return decode(await ImmutableBuffer.fromUint8List(bytes));
+  }
+
+  @override
+  ImageStreamCompleter loadImage(ContentUriImage key, ImageDecoderCallback decode) {
+    return MultiFrameImageStreamCompleter(
+      codec: _loadAsync(key, decode: decode),
+      scale: key.scale,
+      debugLabel: key.uri.toString(),
+      informationCollector: () => <DiagnosticsNode>[
+        ErrorDescription('Uri: ${key.uri}'),
+      ],
+    );
+  }
+
+  @override
+  Future<ContentUriImage> obtainKey(ImageConfiguration configuration) {
+    return SynchronousFuture<ContentUriImage>(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+
+    return other is ContentUriImage && other.uri == uri && other.scale == scale;
+  }
+
+  @override
+  int get hashCode => Object.hash(uri, scale);
+
+  @override
+  String toString() => 'ContentUriImage("$uri", scale: $scale)';
+}

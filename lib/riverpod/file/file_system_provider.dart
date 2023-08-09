f71c4b0008bee611e2d28f2533b81d3a98b6a72b
Navaron Bracke <brackenavaron@gmail.com>
2023-08-09 21:16:13 +0200

    fix comment

--- lib/riverpod/file/file_system_provider.dart
+++ lib/riverpod/file/file_system_provider.dart
@@ -4,8 +4,6 @@ import 'package:weforza/file/io_file_system.dart';
 import 'package:weforza/riverpod/file/file_storage_delegate_provider.dart';
 
 /// This provider provides the file system.
-///
-/// This provider should be preloaded with the file system at application startup.
 final fileSystemProvider = Provider<FileSystem>(
   (ref) => IoFileSystem(
     fileStorageDelegate: ref.read(fileStorageDelegateProvider),

08eae3d07bc41f112b9a5d38b3005923ad9b8a1e
Navaron Bracke <brackenavaron@gmail.com>
2025-03-08 18:40:23 +0100

    add serializable stable index to rider list filter option

--- lib/model/rider/rider_filter_option.dart
+++ lib/model/rider/rider_filter_option.dart
@@ -1,5 +1,14 @@
 /// This enum declares the different filter options for filtering riders.
+///
 /// [RiderFilterOption.all] No filter.
 /// [RiderFilterOption.active] Only active riders.
 /// [RiderFilterOption.inactive] Only inactive riders.
-enum RiderFilterOption { all, active, inactive }
+enum RiderFilterOption {
+  all(0),
+  active(1),
+  inactive(2);
+
+  const RiderFilterOption(this.value);
+
+  final int value;
+}

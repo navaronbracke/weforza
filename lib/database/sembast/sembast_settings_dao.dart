fdb9149de33a23d058534fc881734d5636c78e16
Navaron Bracke <brackenavaron@gmail.com>
2026-03-15 16:47:45 +0100

    stub out the moving of the sembast settings

--- lib/database/sembast/sembast_settings_dao.dart
+++ lib/database/sembast/sembast_settings_dao.dart
@@ -1,11 +1,13 @@
 import 'package:sembast/sembast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weforza/database/sembast/database_tables.dart';
 import 'package:weforza/database/settings_dao.dart';
+import 'package:weforza/model/rider/rider_filter_option.dart';
 import 'package:weforza/model/settings/settings.dart';
 
 /// This class represents the sembast implementation of [SettingsDao].
 class SembastSettingsDao implements SettingsDao {
-  SembastSettingsDao(this._database) : _settingsRecordRef = DatabaseTables.settings.record(_settingsKey);
+  SembastSettingsDao(this._database) : _settingsRecordRef = DatabaseTables.settings.record(_sembastSettingsKey);
 
   /// A reference to the database.
   final Database _database;
@@ -13,12 +15,35 @@ class SembastSettingsDao implements SettingsDao {
   /// The record reference for the settings record.
   final RecordRef<String, Map<String, Object?>> _settingsRecordRef;
 
-  /// The key for the settings record.
-  static const _settingsKey = 'APPLICATION_SETTINGS';
+  static final SharedPreferencesAsync _sharedPreferences = SharedPreferencesAsync();
+
+  /// The key for the settings record in Sembast.
+  static const _sembastSettingsKey = 'APPLICATION_SETTINGS';
 
   @override
   Future<Settings> read() async {
-    return Settings.of(await _settingsRecordRef.get(_database) ?? {});
+    final Map<String, Object?>? sembastRecord = await _settingsRecordRef.get(_database);
+
+    // If the record exists in the database, move it to shared preferences and return it.
+    // Otherwise, read it from shared preferences.
+    if (sembastRecord case {
+      'excludedTermsFilter': final List<Object?>? excludedTermsFilter,
+      'memberListFilter': final int? riderListFilter,
+      'scanDuration': final int? scanDuration,
+    }) {
+      final settings = Settings(
+        excludedTermsFilter: Set.of(excludedTermsFilter?.whereType<String>() ?? const Iterable.empty()),
+        riderListFilter: riderListFilter == null ? RiderFilterOption.all : RiderFilterOption.values[riderListFilter],
+        scanDuration: scanDuration ?? 20,
+      );
+
+      await write(settings);
+      await _settingsRecordRef.delete(_database);
+
+      return settings;
+    }
+
+    // TODO: read from shared prefs
   }
 
   @override

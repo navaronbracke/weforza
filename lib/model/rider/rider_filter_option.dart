11bc623d89371c4efbd773653c1af0362fb27717
Navaron Bracke <brackenavaron@gmail.com>
2026-03-15 16:13:24 +0100

    add index to filter option

--- lib/model/rider/rider_filter_option.dart
+++ lib/model/rider/rider_filter_option.dart
@@ -2,4 +2,13 @@
 /// [RiderFilterOption.all] No filter.
 /// [RiderFilterOption.active] Only active riders.
 /// [RiderFilterOption.inactive] Only inactive riders.
-enum RiderFilterOption { all, active, inactive }
+enum RiderFilterOption {
+  all(1),
+  active(2),
+  inactive(3);
+
+  const RiderFilterOption(this.value);
+
+  /// The raw value of this filter option.
+  final int value;
+}
